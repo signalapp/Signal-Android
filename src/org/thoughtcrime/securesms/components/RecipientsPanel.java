1ac1cec634a9696cff025b43931e2b96c212b50a
Moxie Marlinspike <moxie@thoughtcrime.org>
2012-07-19 15:16:07 -0700

    Update look for RecipientsPanel.
    1) Make the "add contacts" graphic more holo-ific.
    2) Adjust the graphic position on holo-themed devices.

--- src/org/thoughtcrime/securesms/components/RecipientsPanel.java
+++ src/org/thoughtcrime/securesms/components/RecipientsPanel.java
@@ -1,6 +1,6 @@
-/** 
+/**
  * Copyright (C) 2011 Whisper Systems
- * 
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
@@ -10,15 +10,18 @@
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 package org.thoughtcrime.securesms.components;
 
-import java.util.Iterator;
-import java.util.LinkedList;
-import java.util.List;
+import android.content.Context;
+import android.os.Build;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.RelativeLayout;
 
 import org.thoughtcrime.securesms.R;
 import org.thoughtcrime.securesms.contacts.RecipientsAdapter;
@@ -28,27 +31,24 @@ import org.thoughtcrime.securesms.recipients.RecipientFactory;
 import org.thoughtcrime.securesms.recipients.RecipientFormattingException;
 import org.thoughtcrime.securesms.recipients.Recipients;
 
-import android.content.Context;
-import android.util.AttributeSet;
-import android.util.Log;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.widget.RelativeLayout;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
 
 /**
- * Panel component combining both an editable field with a button for 
+ * Panel component combining both an editable field with a button for
  * a list-based contact selector.
- * 
+ *
  * @author Moxie Marlinspike
  */
-public class RecipientsPanel extends RelativeLayout {	
-	
+public class RecipientsPanel extends RelativeLayout {
+
   private RecipientsPanelChangedListener panelChangeListener;
   private RecipientsEditor recipientsText;
   private View panel;
 
   private static final int RECIPIENTS_MAX_LENGTH = 312;
-    
+
   public RecipientsPanel(Context context) {
     super(context);
     initialize();
@@ -63,37 +63,37 @@ public class RecipientsPanel extends RelativeLayout {
     super(context, attrs, defStyle);
     initialize();
   }
-		
+
   public void addRecipient(String name, String number) {
     if (name != null) recipientsText.append(name + "< " + number + ">, ");
     else              recipientsText.append(number + ", ");
   }
-		
+
   public void addRecipients(Recipients recipients) {
     List<Recipient> recipientList = recipients.getRecipientsList();
     Iterator<Recipient> iterator  = recipientList.iterator();
-		
+
     while (iterator.hasNext()) {
       Recipient recipient = iterator.next();
       addRecipient(recipient.getName(), recipient.getNumber());
     }
   }
-	
+
   public Recipients getRecipients() throws RecipientFormattingException {
     String rawText        = recipientsText.getText().toString();
     Recipients recipients = RecipientFactory.getRecipientsFromString(getContext(), rawText);
-		
+
     if (recipients.isEmpty())
       throw new RecipientFormattingException("Recipient List Is Empty!");
-		
-    return recipients;		
+
+    return recipients;
   }
-	
+
   public void disable() {
     recipientsText.setText("");
-    panel.setVisibility(View.GONE);	
+    panel.setVisibility(View.GONE);
   }
-	
+
   public void setPanelChangeListener(RecipientsPanelChangedListener panelChangeListener) {
     this.panelChangeListener = panelChangeListener;
   }
@@ -101,15 +101,20 @@ public class RecipientsPanel extends RelativeLayout {
   private void initialize() {
     LayoutInflater inflater = (LayoutInflater)getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
     inflater.inflate(R.layout.recipients_panel, this, true);
-		
-    panel = findViewById(R.id.recipients_panel);		
-    initRecipientsEditor();        
+
+    View imageButton = findViewById(R.id.contacts_button);
+
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.ICE_CREAM_SANDWICH)
+      ((MarginLayoutParams) imageButton.getLayoutParams()).topMargin = 0;
+
+    panel = findViewById(R.id.recipients_panel);
+    initRecipientsEditor();
   }
-	
+
   private void initRecipientsEditor() {
     Recipients recipients = null;
     recipientsText        = (RecipientsEditor)findViewById(R.id.recipients_text);
-    	
+
     try {
       recipients = getRecipients();
     } catch (RecipientFormattingException e) {
@@ -120,7 +125,7 @@ public class RecipientsPanel extends RelativeLayout {
     recipientsText.populate(recipients);
     recipientsText.setOnFocusChangeListener(new FocusChangedListener());
   }
-	
+
   private class FocusChangedListener implements View.OnFocusChangeListener {
     public void onFocusChange(View v, boolean hasFocus) {
       if (!hasFocus && (panelChangeListener != null)) {
@@ -132,7 +137,7 @@ public class RecipientsPanel extends RelativeLayout {
       }
     }
   }
-    
+
   public interface RecipientsPanelChangedListener {
     public void onRecipientsPanelUpdate(Recipients recipients);
   }
