2b33154740c5c902017e607e76bc19c9037b2300
kris kechagia <kk@rndsec.net>
2012-07-19 11:22:37 +0200

    adds Android.mk
    Change-Id: I80967b24a11ad49111f18dc647580d185af79afd

---
+++ Android.mk
@@ -0,0 +1,11 @@
+LOCAL_PATH:= $(call my-dir)
+include $(CLEAR_VARS)
+
+LOCAL_MODULE_TAGS := optional
+
+LOCAL_SRC_FILES := $(call all-java-files-under, src)
+
+LOCAL_PACKAGE_NAME := TextSecure
+LOCAL_CERTIFICATE := platform
+
+include $(BUILD_PACKAGE)
