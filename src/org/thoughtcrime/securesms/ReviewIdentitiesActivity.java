a66cc636c6d820327e5e9d1820e29480958bb4b3
Moxie Marlinspike <moxie@thoughtcrime.org>
2012-07-23 21:43:55 -0700

    Fix review identity activity UI.

--- src/org/thoughtcrime/securesms/ReviewIdentitiesActivity.java
+++ src/org/thoughtcrime/securesms/ReviewIdentitiesActivity.java
@@ -1,6 +1,6 @@
-/** 
+/**
  * Copyright (C) 2011 Whisper Systems
- * 
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
@@ -10,13 +10,30 @@
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 package org.thoughtcrime.securesms;
 
-import java.io.IOException;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.DialogInterface.OnClickListener;
+import android.content.Intent;
+import android.database.Cursor;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.ContextMenu;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CursorAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.actionbarsherlock.app.SherlockListActivity;
+import com.actionbarsherlock.view.Menu;
+import com.actionbarsherlock.view.MenuItem;
 
 import org.thoughtcrime.securesms.crypto.IdentityKey;
 import org.thoughtcrime.securesms.crypto.InvalidKeyException;
@@ -27,45 +44,30 @@ import org.thoughtcrime.securesms.database.IdentityDatabase;
 import org.thoughtcrime.securesms.util.Base64;
 import org.thoughtcrime.securesms.util.MemoryCleaner;
 
-import android.app.AlertDialog;
-import android.app.ListActivity;
-import android.content.Context;
-import android.content.DialogInterface;
-import android.content.DialogInterface.OnClickListener;
-import android.content.Intent;
-import android.database.Cursor;
-import android.os.Bundle;
-import android.util.Log;
-import android.view.ContextMenu;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.CursorAdapter;
-import android.widget.Toast;
+import java.io.IOException;
 
 /**
  * Activity for reviewing/managing saved identity keys.
- * 
+ *
  * @author Moxie Marlinspike
  */
-public class ReviewIdentitiesActivity extends ListActivity {
+public class ReviewIdentitiesActivity extends SherlockListActivity {
 
-  private static final int MENU_OPTION_VIEW = 1;
   private static final int MENU_OPTION_DELETE = 2;
-	
+
   private MasterSecret masterSecret;
   private MasterCipher masterCipher;
-	
+
   @Override
   public void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);        
+    super.onCreate(savedInstanceState);
     this.setContentView(R.layout.review_identities);
-                
+    this.getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
     initializeResources();
     registerForContextMenu(this.getListView());
   }
-  
+
   @Override
   protected void onDestroy() {
     masterCipher = null;
@@ -73,36 +75,46 @@ public class ReviewIdentitiesActivity extends ListActivity {
     MemoryCleaner.clean(masterSecret);
     super.onDestroy();
   }
-	
+
+  @Override
+  public void onListItemClick(ListView listView, View view, int position, long id) {
+    viewIdentity(((IdentityKeyView)view).getIdentityKeyString());
+  }
+
   @Override
   public void onCreateContextMenu (ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
-    menu.add(0, MENU_OPTION_VIEW, Menu.NONE, "View Key");
     menu.add(0, MENU_OPTION_DELETE, Menu.NONE, "Delete");
   }
 
   @Override
-  public boolean onContextItemSelected(MenuItem item) {
+  public boolean onContextItemSelected(android.view.MenuItem item) {
     Cursor cursor            = ((CursorAdapter)this.getListAdapter()).getCursor();
     String identityKeyString = cursor.getString(cursor.getColumnIndexOrThrow(IdentityDatabase.IDENTITY_KEY));
     String identityName      = cursor.getString(cursor.getColumnIndexOrThrow(IdentityDatabase.IDENTITY_NAME));
-		
-		
+
+
     switch(item.getItemId()) {
     case MENU_OPTION_DELETE:
       deleteIdentity(identityName, identityKeyString);
-      return true;
-    case MENU_OPTION_VIEW:
-      viewIdentity(identityKeyString);
       return true;
     }
     return false;
   }
 
-	
+  @Override
+  public boolean onOptionsItemSelected(MenuItem item) {
+    switch (item.getItemId()) {
+    case android.R.id.home: finish(); return true;
+    }
+
+    return false;
+  }
+
+
   private void initializeResources() {
     this.masterSecret = (MasterSecret)getIntent().getParcelableExtra("master_secret");
     this.masterCipher = new MasterCipher(masterSecret);
-		
+
     Cursor cursor = DatabaseFactory.getIdentityDatabase(this).getIdentities();
     this.startManagingCursor(cursor);
     this.setListAdapter(new IdentitiesListAdapter(this, cursor));
@@ -121,7 +133,7 @@ public class ReviewIdentitiesActivity extends ListActivity {
       Toast.makeText(this, "Unable to view corrupted identity key!", Toast.LENGTH_LONG).show();
     }
   }
-	
+
   private void deleteIdentity(String name, String keyString) {
     AlertDialog.Builder alertDialog = new AlertDialog.Builder(this);
     alertDialog.setTitle("Delete identity?");
@@ -131,23 +143,24 @@ public class ReviewIdentitiesActivity extends ListActivity {
     alertDialog.setPositiveButton(R.string.yes, new DeleteIdentityListener(name, keyString));
     alertDialog.show();
   }
-	
+
   private class DeleteIdentityListener implements OnClickListener {
     private final String name;
     private final String keyString;
-		
+
     public DeleteIdentityListener(String name, String keyString) {
       this.name      = name;
       this.keyString = keyString;
     }
-		
+
     public void onClick(DialogInterface arg0, int arg1) {
-      DatabaseFactory.getIdentityDatabase(ReviewIdentitiesActivity.this).deleteIdentity(name, keyString);
+      DatabaseFactory.getIdentityDatabase(ReviewIdentitiesActivity.this)
+        .deleteIdentity(name, keyString);
     }
   }
-	
+
   private class IdentitiesListAdapter extends CursorAdapter {
-		
+
     public IdentitiesListAdapter(Context context, Cursor cursor) {
       super(context, cursor);
     }
@@ -160,13 +173,13 @@ public class ReviewIdentitiesActivity extends ListActivity {
     public void bindView(View view, Context context, Cursor cursor) {
       IdentityKey identityKey;
       boolean valid;
-			
+
       String identityKeyString = cursor.getString(cursor.getColumnIndexOrThrow(IdentityDatabase.IDENTITY_KEY));
       String identityName   	 = cursor.getString(cursor.getColumnIndexOrThrow(IdentityDatabase.IDENTITY_NAME));
 
-      try {	
+      try {
         String mac             = cursor.getString(cursor.getColumnIndexOrThrow(IdentityDatabase.MAC));
-        valid              	   = masterCipher.verifyMacFor(identityName + identityKeyString, Base64.decode(mac));			
+        valid              	   = masterCipher.verifyMacFor(identityName + identityKeyString, Base64.decode(mac));
         identityKey            = new IdentityKey(Base64.decode(identityKeyString), 0);
       } catch (InvalidKeyException ike) {
         Log.w("ReviewIdentitiesActivity",ike);
@@ -176,7 +189,10 @@ public class ReviewIdentitiesActivity extends ListActivity {
         valid = false;
       }
 
-      ((IdentityKeyView)view).set(identityName, valid);
+      if (!valid)
+        identityName = "Invalid Identity!";
+
+      ((IdentityKeyView)view).set(identityName, identityKeyString);
     }
 
     @Override
