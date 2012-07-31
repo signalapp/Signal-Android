853e12693d36f936074a8c216eb041b7e6808ec0
Moxie Marlinspike <moxie@thoughtcrime.org>
2012-07-31 15:48:14 -0700

    Fix Auto/Save activities for GB.

--- src/org/thoughtcrime/securesms/AutoInitiateActivity.java
+++ src/org/thoughtcrime/securesms/AutoInitiateActivity.java
@@ -1,6 +1,6 @@
-/** 
+/**
  * Copyright (C) 2011 Whisper Systems
- * 
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
@@ -10,20 +10,12 @@
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 package org.thoughtcrime.securesms;
 
-import org.thoughtcrime.securesms.crypto.KeyExchangeInitiator;
-import org.thoughtcrime.securesms.crypto.MasterSecret;
-import org.thoughtcrime.securesms.database.LocalKeyRecord;
-import org.thoughtcrime.securesms.database.RemoteKeyRecord;
-import org.thoughtcrime.securesms.recipients.Recipient;
-import org.thoughtcrime.securesms.util.MemoryCleaner;
-
-import android.app.Activity;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
@@ -32,28 +24,37 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
+import com.actionbarsherlock.app.SherlockActivity;
+
+import org.thoughtcrime.securesms.crypto.KeyExchangeInitiator;
+import org.thoughtcrime.securesms.crypto.MasterSecret;
+import org.thoughtcrime.securesms.database.LocalKeyRecord;
+import org.thoughtcrime.securesms.database.RemoteKeyRecord;
+import org.thoughtcrime.securesms.recipients.Recipient;
+import org.thoughtcrime.securesms.util.MemoryCleaner;
+
 /**
  * Activity which prompts the user to initiate a secure
  * session.  Initiated by whitespace tag detection from
  * the remote endpoint.
- * 
+ *
  * @author Moxie Marlinspike
  *
  */
-public class AutoInitiateActivity extends Activity {
+public class AutoInitiateActivity extends SherlockActivity {
 
   private long threadId;
   private Recipient recipient;
   private MasterSecret masterSecret;
-	
+
   @Override
   public void onCreate(Bundle icicle) {
     super.onCreate(icicle);
     setContentView(R.layout.auto_initiate_activity);
-		
+
     initializeResources();
   }
-  
+
   @Override
   public void onDestroy() {
     MemoryCleaner.clean(masterSecret);
@@ -64,22 +65,22 @@ public class AutoInitiateActivity extends Activity {
     this.threadId     = this.getIntent().getLongExtra("threadId", -1);
     this.recipient    = (Recipient)this.getIntent().getParcelableExtra("recipient");
     this.masterSecret = (MasterSecret)this.getIntent().getParcelableExtra("masterSecret");
-		
-    ((Button)findViewById(R.id.initiate_button)).setOnClickListener(new OkListener());		
+
+    ((Button)findViewById(R.id.initiate_button)).setOnClickListener(new OkListener());
     ((Button)findViewById(R.id.cancel_button)).setOnClickListener(new CancelListener());
   }
-	
+
   private void initiateKeyExchange() {
     KeyExchangeInitiator.initiate(this, masterSecret, recipient, true);
     finish();
   }
-	
+
   private class OkListener implements View.OnClickListener {
     public void onClick(View v) {
       initiateKeyExchange();
     }
   }
-	
+
   private class CancelListener implements View.OnClickListener {
     public void onClick(View v) {
       Log.w("AutoInitiateActivity", "Exempting threadID: " + threadId);
@@ -87,16 +88,16 @@ public class AutoInitiateActivity extends Activity {
       AutoInitiateActivity.this.finish();
     }
   }
-	
+
   public static void exemptThread(Context context, long threadId) {
     SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(context);
-    sp.edit().putBoolean("pref_thread_auto_init_exempt_" + threadId, true).commit();		
+    sp.edit().putBoolean("pref_thread_auto_init_exempt_" + threadId, true).commit();
   }
-	
-  public static boolean isValidAutoInitiateSituation(Context context, MasterSecret masterSecret, 
-						     Recipient recipient, String message, long threadId) 
+
+  public static boolean isValidAutoInitiateSituation(Context context, MasterSecret masterSecret,
+                 Recipient recipient, String message, long threadId)
   {
-    return 
+    return
       isMessageTagged(message)             &&
       isThreadQualified(context, threadId) &&
       isExchangeQualified(context, masterSecret, recipient);
@@ -110,11 +111,11 @@ public class AutoInitiateActivity extends Activity {
     SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(context);
     return !sp.getBoolean("pref_thread_auto_init_exempt_" + threadId, false);
   }
-	
+
   private static boolean isExchangeQualified(Context context, MasterSecret masterSecret, Recipient recipient) {
-    return 
+    return
       (new RemoteKeyRecord(context,recipient).getCurrentRemoteKey() == null) &&
       (new LocalKeyRecord(context, masterSecret, recipient).getCurrentKeyPair() == null);
   }
-	
+
 }
--- src/org/thoughtcrime/securesms/SaveIdentityActivity.java
+++ src/org/thoughtcrime/securesms/SaveIdentityActivity.java
@@ -1,6 +1,6 @@
-/** 
+/**
  * Copyright (C) 2011 Whisper Systems
- * 
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
@@ -10,19 +10,12 @@
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 package org.thoughtcrime.securesms;
 
-import org.thoughtcrime.securesms.crypto.IdentityKey;
-import org.thoughtcrime.securesms.crypto.InvalidKeyException;
-import org.thoughtcrime.securesms.crypto.MasterSecret;
-import org.thoughtcrime.securesms.database.DatabaseFactory;
-import org.thoughtcrime.securesms.util.MemoryCleaner;
-
-import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
@@ -32,35 +25,44 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.actionbarsherlock.app.SherlockActivity;
+
+import org.thoughtcrime.securesms.crypto.IdentityKey;
+import org.thoughtcrime.securesms.crypto.InvalidKeyException;
+import org.thoughtcrime.securesms.crypto.MasterSecret;
+import org.thoughtcrime.securesms.database.DatabaseFactory;
+import org.thoughtcrime.securesms.util.MemoryCleaner;
+
 /**
  * Activity that provides interface for users to save
  * identity keys they receive.
- * 
+ *
  * @author Moxie Marlinspike
  */
-public class SaveIdentityActivity extends Activity {
+public class SaveIdentityActivity extends SherlockActivity {
 
   private MasterSecret masterSecret;
   private IdentityKey identityKey;
-	
+
   private EditText identityName;
   private Button okButton;
   private Button cancelButton;
-	
+
   @Override
   public void onCreate(Bundle bundle) {
     super.onCreate(bundle);
     setContentView(R.layout.save_identity_activity);
-		
+
     initializeResources();
     initializeListeners();
   }
-	
+
+  @Override
   protected void onDestroy() {
     MemoryCleaner.clean(masterSecret);
     super.onDestroy();
   }
-  
+
   private void initializeResources() {
     String nameSuggestion = getIntent().getStringExtra("name_suggestion");
 
@@ -69,31 +71,31 @@ public class SaveIdentityActivity extends Activity {
     this.identityName = (EditText)findViewById(R.id.identity_name);
     this.okButton     = (Button)findViewById(R.id.ok_button);
     this.cancelButton = (Button)findViewById(R.id.cancel_button);
-		
+
     if ((nameSuggestion != null) && (nameSuggestion.trim().length() > 0)) {
       this.identityName.setText(nameSuggestion);
     }
   }
-	
+
   private void initializeListeners() {
     this.okButton.setOnClickListener(new OkListener());
     this.cancelButton.setOnClickListener(new CancelListener());
   }
-	
+
   private class OkListener implements View.OnClickListener {
     public void onClick(View v) {
       if (identityName.getText() == null || identityName.getText().toString().trim().length() == 0) {
         Toast.makeText(SaveIdentityActivity.this, "You must specify a name for this identity!", Toast.LENGTH_LONG).show();
         return;
       }
-			
+
       try {
         DatabaseFactory.getIdentityDatabase(SaveIdentityActivity.this).saveIdentity(masterSecret, identityKey, identityName.getText().toString());
       } catch (InvalidKeyException e) {
         AlertDialog.Builder builder = new AlertDialog.Builder(SaveIdentityActivity.this);
         builder.setTitle("Identity Name Exists!");
         builder.setMessage("An identity key with the specified name already exists.");
-        builder.setPositiveButton("Manage Identities", new DialogInterface.OnClickListener() {					
+        builder.setPositiveButton("Manage Identities", new DialogInterface.OnClickListener() {
           public void onClick(DialogInterface dialog, int which) {
             Intent intent = new Intent(SaveIdentityActivity.this, ReviewIdentitiesActivity.class);
             intent.putExtra("master_secret", masterSecret);
@@ -104,15 +106,15 @@ public class SaveIdentityActivity extends Activity {
         builder.show();
         return;
       }
-			
+
       finish();
-    }		
+    }
   }
-	
+
   private class CancelListener implements View.OnClickListener {
     public void onClick(View v) {
       finish();
     }
   }
-	
+
 }
