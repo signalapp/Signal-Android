2277dbd5724c6bfa984e3cad15afba36ecfddd85
Moxie Marlinspike <moxie@thoughtcrime.org>
2013-02-09 17:38:33 -0800

    Localize ok/cancel

--- src/org/thoughtcrime/securesms/preferences/PassphraseTimeoutPreference.java
+++ src/org/thoughtcrime/securesms/preferences/PassphraseTimeoutPreference.java
@@ -1,6 +1,6 @@
-/** 
+/**
  * Copyright (C) 2011 Whisper Systems
- * 
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
@@ -10,14 +10,11 @@
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 package org.thoughtcrime.securesms.preferences;
-
-import org.thoughtcrime.securesms.ApplicationPreferencesActivity;
-import org.thoughtcrime.securesms.R;
 
 import android.content.Context;
 import android.content.DialogInterface;
@@ -29,9 +26,12 @@ import android.widget.SeekBar;
 import android.widget.Spinner;
 import android.widget.TextView;
 
+import org.thoughtcrime.securesms.ApplicationPreferencesActivity;
+import org.thoughtcrime.securesms.R;
+
 /**
  * Dialog preference for encryption passphrase timeout.
- * 
+ *
  * @author Moxie Marlinspike
  */
 
@@ -40,14 +40,14 @@ public class PassphraseTimeoutPreference extends DialogPreference {
   private Spinner scaleSpinner;
   private SeekBar seekBar;
   private TextView timeoutText;
-	
+
   public PassphraseTimeoutPreference(Context context, AttributeSet attrs) {
     super(context, attrs);
     this.setDialogLayoutResource(R.layout.passphrase_timeout_dialog);
-    this.setPositiveButtonText("Ok");
-    this.setNegativeButtonText("Cancel");		
+    this.setPositiveButtonText(android.R.string.ok);
+    this.setNegativeButtonText(android.R.string.cancel);
   }
-	
+
   @Override
   protected View onCreateDialogView() {
     View dialog       = super.onCreateDialogView();
@@ -57,30 +57,30 @@ public class PassphraseTimeoutPreference extends DialogPreference {
 
     initializeDefaults();
     initializeListeners();
-		
+
     return dialog;
   }
-	
+
   @Override
   public void onClick(DialogInterface dialog, int which) {
     if (which == DialogInterface.BUTTON_POSITIVE) {
       int interval;
-			
+
       if (scaleSpinner.getSelectedItemPosition() == 0) {
         interval = Math.max(seekBar.getProgress(), 1);
       } else {
         interval = Math.max(seekBar.getProgress(), 1) * 60;
       }
-				
+
       this.getSharedPreferences().edit().putInt(ApplicationPreferencesActivity.PASSPHRASE_TIMEOUT_INTERVAL_PREF, interval).commit();
     }
-		
+
     super.onClick(dialog, which);
   }
-	
+
   private void initializeDefaults() {
     int timeout = this.getSharedPreferences().getInt(ApplicationPreferencesActivity.PASSPHRASE_TIMEOUT_INTERVAL_PREF, 60 * 5);
-		
+
     if (timeout > 60) {
       scaleSpinner.setSelection(1);
       seekBar.setMax(24);
@@ -96,10 +96,11 @@ public class PassphraseTimeoutPreference extends DialogPreference {
 
   private void initializeListeners() {
     this.seekBar.setOnSeekBarChangeListener(new SeekbarChangedListener());
-    this.scaleSpinner.setOnItemSelectedListener(new ScaleSelectedListener());		
+    this.scaleSpinner.setOnItemSelectedListener(new ScaleSelectedListener());
   }
-	
+
   private class ScaleSelectedListener implements AdapterView.OnItemSelectedListener {
+    @Override
     public void onItemSelected(AdapterView<?> arg0, View arg1, int arg2, long selected) {
       if (selected == 0) {
         seekBar.setMax(60);
@@ -108,25 +109,29 @@ public class PassphraseTimeoutPreference extends DialogPreference {
       }
     }
 
+    @Override
     public void onNothingSelected(AdapterView<?> arg0) {
-    }		
+    }
   }
 
   private class SeekbarChangedListener implements SeekBar.OnSeekBarChangeListener {
 
+    @Override
     public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-      if (progress < 1) 
+      if (progress < 1)
         progress = 1;
-			
+
       timeoutText.setText(progress +"");
     }
 
+    @Override
     public void onStartTrackingTouch(SeekBar seekBar) {
     }
 
+    @Override
     public void onStopTrackingTouch(SeekBar seekBar) {
     }
-		
+
   }
-	
+
 }
