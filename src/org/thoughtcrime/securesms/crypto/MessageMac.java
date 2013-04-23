f2475491fe7b26ee4e341a538d7ccf8f5415e8be
Moxie Marlinspike <moxie@thoughtcrime.org>
2013-04-23 14:49:32 -0700

    Fix for NPE on null encrypted MMS messages.

--- src/org/thoughtcrime/securesms/crypto/MessageMac.java
+++ src/org/thoughtcrime/securesms/crypto/MessageMac.java
@@ -61,7 +61,7 @@ public class MessageMac {
   }
 	
   public static byte[] getMessageWithoutMac(byte[] message) throws InvalidMacException {
-    if (message.length <= MAC_LENGTH)
+    if (message == null || message.length <= MAC_LENGTH)
       throw new InvalidMacException("Message shorter than MAC!");
 		
     byte[] strippedMessage = new byte[message.length - MAC_LENGTH];
