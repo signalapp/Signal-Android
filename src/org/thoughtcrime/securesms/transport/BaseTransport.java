53803630d4c38765b1b81eb9620a49e7a8638ddd
Moxie Marlinspike <moxie@thoughtcrime.org>
2013-07-12 17:40:41 -0700

    Enable TextSecure universal transport.

---
+++ src/org/thoughtcrime/securesms/transport/BaseTransport.java
@@ -0,0 +1,32 @@
+package org.thoughtcrime.securesms.transport;
+
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+
+import org.thoughtcrime.securesms.service.SendReceiveService;
+import org.thoughtcrime.securesms.service.SmsListener;
+
+public abstract class BaseTransport {
+
+  protected Intent constructSentIntent(Context context, long messageId, long type) {
+    Intent pending = new Intent(SendReceiveService.SENT_SMS_ACTION,
+                                Uri.parse("custom://" + messageId + System.currentTimeMillis()),
+                                context, SmsListener.class);
+
+    pending.putExtra("type", type);
+    pending.putExtra("message_id", messageId);
+
+    return pending;
+  }
+
+  protected Intent constructDeliveredIntent(Context context, long messageId, long type) {
+    Intent pending = new Intent(SendReceiveService.DELIVERED_SMS_ACTION,
+                                Uri.parse("custom://" + messageId + System.currentTimeMillis()),
+                                context, SmsListener.class);
+    pending.putExtra("type", type);
+    pending.putExtra("message_id", messageId);
+
+    return pending;
+  }
+}
