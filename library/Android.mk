dd30a2ee36e0643fee89a80880925f6928aa768b
Koushik Dutta <koushd@gmail.com>
2013-09-06 14:02:11 -0700

    add protobuf-java-2.4.1

--- library/Android.mk
+++ library/Android.mk
@@ -30,6 +30,15 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := TextSecureLibrary
 LOCAL_SDK_VERSION := 8
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
-LOCAL_STATIC_JAVA_LIBRARIES := libphonenumbergoogle thoughtcrimegson spongycastle-light-jdk15on libprotobuf-java-2.3.0-lite
+LOCAL_STATIC_JAVA_LIBRARIES := libphonenumbergoogle thoughtcrimegson spongycastle-light-jdk15on libprotobuf-java-2.3.0-lite protobuf-java-2.4.1
 
 include $(BUILD_STATIC_JAVA_LIBRARY)
+
+include $(CLEAR_VARS)
+LOCAL_SRC_FILES := libs/protobuf-java-2.4.1.jar
+LOCAL_MODULE := protobuf-java-2.4.1
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := JAVA_LIBRARIES
+LOCAL_MODULE_PATH := $(TARGET_OUT)/fake_packages/$(LOCAL_SRC_FILES)
+
+include $(BUILD_PREBUILT)
