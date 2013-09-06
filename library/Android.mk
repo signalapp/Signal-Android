54963e87ff8c1c13de3e020f0d1424a82387d906
Koushik Dutta <koushd@gmail.com>
2013-09-06 14:30:42 -0700

    fix

--- library/Android.mk
+++ library/Android.mk
@@ -30,7 +30,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := TextSecureLibrary
 LOCAL_SDK_VERSION := 8
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
-LOCAL_STATIC_JAVA_LIBRARIES := libphonenumbergoogle thoughtcrimegson spongycastle-light-jdk15on libprotobuf-java-2.3.0-lite protobuf-java-2.4.1
+LOCAL_STATIC_JAVA_LIBRARIES := libphonenumbergoogle thoughtcrimegson spongycastle-light-jdk15on protobuf-java-2.4.1
 
 include $(BUILD_STATIC_JAVA_LIBRARY)
 
