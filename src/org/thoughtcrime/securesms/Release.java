f002072f384a24685f08b8722d108b0bc3e5cc88
Moxie Marlinspike <moxie@thoughtcrime.org>
2013-12-01 17:21:41 -0800

    New attachment API

--- src/org/thoughtcrime/securesms/Release.java
+++ src/org/thoughtcrime/securesms/Release.java
@@ -3,6 +3,6 @@ package org.thoughtcrime.securesms;
 public class Release {
 
   public static final String PUSH_URL = "https://textsecure-service.whispersystems.org";
-//  public static final String PUSH_SERVICE_URL = "http://192.168.1.135:8080";
+//  public static final String PUSH_URL = "http://192.168.1.135:8080";
 
 }
