2f986229488215174094e742bc8fe796ad333a14
Moxie Marlinspike <moxie@thoughtcrime.org>
2014-02-14 16:18:18 -0800

    Fix group migration.

--- src/org/thoughtcrime/securesms/database/DatabaseFactory.java
+++ src/org/thoughtcrime/securesms/database/DatabaseFactory.java
@@ -648,6 +648,8 @@ public class DatabaseFactory {
         db.execSQL("ALTER TABLE sms ADD COLUMN address_device_id INTEGER DEFAULT 1;");
         db.execSQL("ALTER TABLE mms ADD COLUMN address_device_id INTEGER DEFAULT 1;");
         db.execSQL("ALTER TABLE sms ADD COLUMN group_action INTEGER DEFAULT -1;");
+        db.execSQL("ALTER TABLE sms ADD COLUMN group_action_arguments TEXT;");
+        db.execSQL("ALTER TABLE mms ADD COLuMN group_action INTEGER DEFAULT -1;");
         db.execSQL("ALTER TABLE mms ADD COLUMN group_action_arguments TEXT;");
         db.execSQL("ALTER TABLE thread ADD COLUMN group_action INTEGER DEFAULT -1;");
         db.execSQL("ALTER TABLE thread ADD COLUMN group_action_arguments TEXT;");
