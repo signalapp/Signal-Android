2dcbd8cfdbf05c13c1a95069958be2e9d6bbd712
Ondrej Hamak <ondrej.hamak@zentity.com>
2014-02-28 02:43:08 +0100

    locale sensitive country sort

--- src/org/thoughtcrime/securesms/database/loaders/CountryListLoader.java
+++ src/org/thoughtcrime/securesms/database/loaders/CountryListLoader.java
@@ -1,10 +1,12 @@
 package org.thoughtcrime.securesms.database.loaders;
 
 
+import java.text.Collator;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.HashMap;
+import java.util.Locale;
 import java.util.Map;
 import java.util.Set;
 
@@ -13,6 +15,8 @@ import android.support.v4.content.AsyncTaskLoader;
 
 
 import com.google.i18n.phonenumbers.PhoneNumberUtil;
+
+import org.thoughtcrime.securesms.util.TextSecurePreferences;
 import org.whispersystems.textsecure.util.PhoneNumberFormatter;
 
 public class CountryListLoader extends AsyncTaskLoader<ArrayList<Map<String, String>>> {
@@ -29,19 +33,38 @@ public class CountryListLoader extends AsyncTaskLoader<ArrayList<Map<String, Str
     for (String region : regions) {
       Map<String, String> data = new HashMap<String, String>(2);
       data.put("country_name", PhoneNumberFormatter.getRegionDisplayName(region));
-      data.put("country_code", "+" +PhoneNumberUtil.getInstance().getCountryCodeForRegion(region));
+      data.put("country_code", "+" + PhoneNumberUtil.getInstance().getCountryCodeForRegion(region));
       results.add(data);
     }
 
-    Collections.sort(results, new RegionComparator());
+    String language = TextSecurePreferences.getLanguage(getContext());
+    RegionComparator regionComparator;
+    if (language == null) {
+      regionComparator = new RegionComparator();
+    } else {
+      regionComparator = new RegionComparator(new Locale(language));
+    }
+
+    Collections.sort(results, regionComparator);
 
     return results;
   }
 
   private class RegionComparator implements Comparator<Map<String, String>> {
+
+    private final Collator collator;
+
+    public RegionComparator() {
+      collator = Collator.getInstance();
+    }
+
+    public RegionComparator(Locale locale) {
+      collator = Collator.getInstance(locale);
+    }
+
     @Override
     public int compare(Map<String, String> lhs, Map<String, String> rhs) {
-      return lhs.get("country_name").compareTo(rhs.get("country_name"));
+      return collator.compare(lhs.get("country_name"), rhs.get("country_name"));
     }
   }
 }
