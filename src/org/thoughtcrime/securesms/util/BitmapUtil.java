a438d4bf51b63635f807ceac81e0146a26b7109d
Lukas Barth <mail@tinloaf.de>
2014-03-05 21:07:09 +0100

    Fix changing files for no reason

--- src/org/thoughtcrime/securesms/util/BitmapUtil.java
+++ src/org/thoughtcrime/securesms/util/BitmapUtil.java
@@ -4,7 +4,6 @@ import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
-import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.PorterDuff;
 import android.graphics.PorterDuffXfermode;
