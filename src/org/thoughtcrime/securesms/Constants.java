4d2656ef38c950d62d358414c5a2d5a6960f8dad
Wikinaut <mail@wikinaut.de>
2014-04-15 11:12:51 +0200

    Introduction of a new "About application" page
    + version number from Manifest
    + urls for license, list of changes, support, bugreports
    + based on concepts as found in https://github.com/schildbach/bitcoin-wallet
    + theme-aware page (dark/light design)
    + returns correctly from web pages (like source code, support forum) to the About page
    + (C) 2014 Open Whisper Systems

--- src/org/thoughtcrime/securesms/Constants.java
+++ src/org/thoughtcrime/securesms/Constants.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2014 Open Whisper Systems and the original author or authors.
+ * Copyright (C) 2014 Open Whisper Systems
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
