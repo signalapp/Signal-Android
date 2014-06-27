2ffd23272eddf0637e12f8249162a5079a22e99c
Jabro <jabro91@gmx.de>
2014-06-27 18:15:29 +0200

    Add copyright of leolin and OpenWhisperSystems

--- src/org/thoughtcrime/securesms/badger/ShortcutBadger.java
+++ src/org/thoughtcrime/securesms/badger/ShortcutBadger.java
@@ -1,3 +1,34 @@
+/*
+ *Copyright 2014 Leo Lin
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Copyright (C) 2013 Open Whisper Systems
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package org.thoughtcrime.securesms.badger;
 
 import android.content.ComponentName;
@@ -6,13 +37,6 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.content.pm.ResolveInfo;import java.lang.String;
 
-/**
- * Created with IntelliJ IDEA.
- * User: leolin
- * Date: 2013/11/14
- * Time: 5:51
- * To change this template use File | Settings | File Templates.
- */
 public abstract class ShortcutBadger {
     private static final String HOME_PACKAGE_SONY = "com.sonyericsson.home";
     private static final String HOME_PACKAGE_SAMSUNG = "com.sec.android.app.launcher";
