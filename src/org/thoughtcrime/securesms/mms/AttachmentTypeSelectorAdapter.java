cf6e6b18bf54dff12ed15d18a14ff6d29ab16bc7
Bastian Köcher <git@kchr.de>
2014-07-20 01:41:22 +0200

    Replaced tabs with spaces

--- src/org/thoughtcrime/securesms/mms/AttachmentTypeSelectorAdapter.java
+++ src/org/thoughtcrime/securesms/mms/AttachmentTypeSelectorAdapter.java
@@ -32,89 +32,85 @@ import java.util.List;
 
 public class AttachmentTypeSelectorAdapter extends ArrayAdapter<AttachmentTypeSelectorAdapter.IconListItem> {
 
-	public static final int ADD_IMAGE     			= 1;
-//	public static final int TAKE_PICTURE  = 2;
-	public static final int ADD_VIDEO     			= 3;
-//	public static final int	RECORD_VIDEO  = 4;
-	public static final int	ADD_SOUND	    			= 5;
-//	public static final int	RECORD_SOUND  = 6;
-	public static final int	ADD_CONTACT_INFO	  = 7;
+  public static final int ADD_IMAGE         = 1;
+//  public static final int TAKE_PICTURE  = 2;
+  public static final int ADD_VIDEO         = 3;
+//  public static final int	RECORD_VIDEO  = 4;
+  public static final int ADD_SOUND         = 5;
+//  public static final int	RECORD_SOUND  = 6;
+  public static final int ADD_CONTACT_INFO  = 7;
 
-	private final Context context;
+  private final Context context;
 
-	public AttachmentTypeSelectorAdapter(Context context) {
-		super(context, R.layout.icon_list_item, getItemList(context));
-		this.context = context;
-	}
+  public AttachmentTypeSelectorAdapter(Context context) {
+    super(context, R.layout.icon_list_item, getItemList(context));
+    this.context = context;
+  }
 
-	public int buttonToCommand(int position) {
-		return getItem(position).getCommand();
-	}
+  public int buttonToCommand(int position) {
+    return getItem(position).getCommand();
+  }
 
-	@Override
-	public View getView(int position, View convertView, ViewGroup parent) {
-	  TextView text;
-	  ImageView image;
+  @Override
+  public View getView(int position, View convertView, ViewGroup parent) {
+    TextView text;
+    ImageView image;
 
-	  View view;
-	  if (convertView == null) {
-	    LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-	    view = inflater.inflate(R.layout.icon_list_item, parent, false);
-	  } else {
-	    view = convertView;
-	  }
+    View view;
+    if (convertView == null) {
+      LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+      view = inflater.inflate(R.layout.icon_list_item, parent, false);
+    } else {
+      view = convertView;
+    }
 
-	  text = (TextView) view.findViewById(R.id.text1);
-	  text.setText(getItem(position).getTitle());
+    text = (TextView) view.findViewById(R.id.text1);
+    text.setText(getItem(position).getTitle());
 
-	  image = (ImageView) view.findViewById(R.id.icon);
-	  image.setImageResource(getItem(position).getResource());
+    image = (ImageView) view.findViewById(R.id.icon);
+    image.setImageResource(getItem(position).getResource());
 
-	  return view;
-	}
+    return view;
+  }
 
-	private static List<IconListItem> getItemList(Context context) {
-	  List<IconListItem> data = new ArrayList<IconListItem>(7);
-	  addItem(data, context.getString(R.string.AttachmentTypeSelectorAdapter_picture),
-	          R.drawable.ic_attach_picture_holo_light, ADD_IMAGE);
-//        addItem(data, "Capture picture", R.drawable.ic_launcher_camera, TAKE_PICTURE);
-	  addItem(data, context.getString(R.string.AttachmentTypeSelectorAdapter_video),
-	          R.drawable.ic_attach_video_holo_light, ADD_VIDEO);
-//        addItem(data, "Capture video", R.drawable.ic_launcher_camera_record, RECORD_VIDEO);
-	  addItem(data, context.getString(R.string.AttachmentTypeSelectorAdapter_audio),
-	          R.drawable.ic_attach_audio_holo_light, ADD_SOUND);
-//        addItem(data, "Record audio", R.drawable.ic_launcher_record_audio, RECORD_SOUND);
-    addItem(data, context.getString(R.string.AttachmentTypeSelectorAdapter_contact),
-      R.drawable.ic_action_add_person, ADD_CONTACT_INFO);
+  private static List<IconListItem> getItemList(Context context) {
+    List<IconListItem> data = new ArrayList<IconListItem>(7);
+    addItem(data, context.getString(R.string.AttachmentTypeSelectorAdapter_picture), R.drawable.ic_attach_picture_holo_light, ADD_IMAGE);
+    //        addItem(data, "Capture picture", R.drawable.ic_launcher_camera, TAKE_PICTURE);
+    addItem(data, context.getString(R.string.AttachmentTypeSelectorAdapter_video), R.drawable.ic_attach_video_holo_light, ADD_VIDEO);
+    //        addItem(data, "Capture video", R.drawable.ic_launcher_camera_record, RECORD_VIDEO);
+    addItem(data, context.getString(R.string.AttachmentTypeSelectorAdapter_audio), R.drawable.ic_attach_audio_holo_light, ADD_SOUND);
+    //        addItem(data, "Record audio", R.drawable.ic_launcher_record_audio, RECORD_SOUND);
+    addItem(data, context.getString(R.string.AttachmentTypeSelectorAdapter_contact), R.drawable.ic_action_add_person, ADD_CONTACT_INFO);
 
-	  return data;
-	}
+    return data;
+  }
 
-	private static void addItem(List<IconListItem> list, String text, int resource, int id) {
-		list.add(new IconListItem(text, resource, id));
-	}
+  private static void addItem(List<IconListItem> list, String text, int resource, int id) {
+    list.add(new IconListItem(text, resource, id));
+  }
 
-	public static class IconListItem {
-	  private final String mTitle;
-	  private final int mResource;
-	  private final int id;
+  public static class IconListItem {
+    private final String mTitle;
+    private final int mResource;
+    private final int id;
 
-	  public IconListItem(String title, int resource, int id) {
-	    mResource = resource;
-	    mTitle = title;
-	    this.id = id;
-	  }
+    public IconListItem(String title, int resource, int id) {
+      mResource = resource;
+      mTitle = title;
+      this.id = id;
+    }
 
-	  public int getCommand() {
-	    return id;
-	  }
+    public int getCommand() {
+      return id;
+    }
 
-	  public String getTitle() {
-	    return mTitle;
-	  }
+    public String getTitle() {
+      return mTitle;
+    }
 
-	  public int getResource() {
-	    return mResource;
-	  }
-	}
+    public int getResource() {
+      return mResource;
+    }
+  }
 }
