4a2143d8441520e205d100179b15254eb3a4e864
Jake McGinty <me@jake.su>
2014-07-26 02:42:06 -1000

    refactor initializeReminders()

--- src/org/thoughtcrime/securesms/ConversationListFragment.java
+++ src/org/thoughtcrime/securesms/ConversationListFragment.java
@@ -16,6 +16,7 @@
  */
 package org.thoughtcrime.securesms;
 
+import android.annotation.TargetApi;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.ProgressDialog;
@@ -23,6 +24,8 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.database.Cursor;
 import android.os.AsyncTask;
+import android.os.Build;
+import android.os.Build.VERSION_CODES;
 import android.os.Bundle;
 import android.provider.Telephony;
 import android.support.v4.app.LoaderManager;
@@ -205,64 +208,12 @@ public class ConversationListFragment extends SherlockListFragment
       TextSecurePreferences.setPromptedDefaultSmsProvider(getActivity(), false);
     }
 
-    final ViewGroup   container = (ViewGroup  ) reminderView.findViewById(R.id.container     );
-    final ImageButton cancel    = (ImageButton) reminderView.findViewById(R.id.cancel        );
-    final Button      ok        = (Button     ) reminderView.findViewById(R.id.ok            );
-    final TextView    title     = (TextView   ) reminderView.findViewById(R.id.reminder_title);
-    final TextView    text      = (TextView   ) reminderView.findViewById(R.id.reminder_text );
-    final ImageView   icon      = (ImageView  ) reminderView.findViewById(R.id.icon          );
     if (!isDefault && !TextSecurePreferences.hasPromptedDefaultSmsProvider(getActivity())) {
-      icon.setImageResource(R.drawable.sms_selection_icon);
-      title.setText(R.string.reminder_header_sms_default_title);
-      text.setText(R.string.reminder_header_sms_default_text);
-      cancel.setOnClickListener(new OnClickListener() {
-        @Override
-        public void onClick(View v) {
-          TextSecurePreferences.setPromptedDefaultSmsProvider(getActivity(), true);
-          container.setVisibility(View.GONE);
-        }
-      });
-      ok.setOnClickListener(new OnClickListener() {
-        @Override
-        public void onClick(View v) {
-          TextSecurePreferences.setPromptedDefaultSmsProvider(getActivity(), true);
-          Intent intent = new Intent(Telephony.Sms.Intents.ACTION_CHANGE_DEFAULT);
-          intent.putExtra(Telephony.Sms.Intents.EXTRA_PACKAGE_NAME, getActivity().getPackageName());
-          startActivity(intent);
-        }
-      });
-      container.setVisibility(View.VISIBLE);
+      showDefaultSmsReminder();
     } else if (isDefault && !ApplicationMigrationService.isDatabaseImported(getActivity())) {
-      icon.setImageResource(R.drawable.sms_system_import_icon);
-      title.setText(R.string.reminder_header_sms_import_title);
-      text.setText(R.string.reminder_header_sms_import_text);
-      cancel.setOnClickListener(new OnClickListener() {
-        @Override
-        public void onClick(View v) {
-          ApplicationMigrationService.setDatabaseImported(getActivity());
-          container.setVisibility(View.GONE);
-        }
-      });
-      ok.setOnClickListener(new OnClickListener() {
-        @Override
-        public void onClick(View v) {
-          Intent intent = new Intent(getActivity(), ApplicationMigrationService.class);
-          intent.setAction(ApplicationMigrationService.MIGRATE_DATABASE);
-          intent.putExtra("master_secret", masterSecret);
-          getActivity().startService(intent);
-
-          Intent nextIntent = new Intent(getActivity(), ConversationListActivity.class);
-          intent.putExtra("master_secret", masterSecret);
-
-          Intent activityIntent = new Intent(getActivity(), DatabaseMigrationActivity.class);
-          activityIntent.putExtra("master_secret", masterSecret);
-          activityIntent.putExtra("next_intent", nextIntent);
-          getActivity().startActivity(activityIntent);
-        }
-      });
-      container.setVisibility(View.VISIBLE);
+      showSystemSmsImportReminder();
     } else {
-      container.setVisibility(View.GONE);
+      reminderView.findViewById(R.id.container).setVisibility(View.GONE);
     }
   }
 
@@ -386,6 +337,79 @@ public class ConversationListFragment extends SherlockListFragment
   public void onDestroyActionMode(ActionMode mode) {
     ((ConversationListAdapter)getListAdapter()).initializeBatchMode(false);
     actionMode = null;
+  }
+
+  @TargetApi(VERSION_CODES.KITKAT)
+  private void showDefaultSmsReminder() {
+    final ViewGroup container = (ViewGroup) reminderView.findViewById(R.id.container);
+
+    setReminderData(R.drawable.sms_selection_icon,
+                    R.string.reminder_header_sms_default_title,
+                    R.string.reminder_header_sms_default_text,
+                    new OnClickListener() {
+                      @Override
+                      public void onClick(View v) {
+                        TextSecurePreferences.setPromptedDefaultSmsProvider(getActivity(), true);
+                        Intent intent = new Intent(Telephony.Sms.Intents.ACTION_CHANGE_DEFAULT);
+                        intent.putExtra(Telephony.Sms.Intents.EXTRA_PACKAGE_NAME, getActivity().getPackageName());
+                        startActivity(intent);
+                      }
+                    },
+                    new OnClickListener() {
+                      @Override
+                      public void onClick(View v) {
+                        TextSecurePreferences.setPromptedDefaultSmsProvider(getActivity(), true);
+                        container.setVisibility(View.GONE);
+                      }
+                    });
+    container.setVisibility(View.VISIBLE);
+  }
+
+  private void showSystemSmsImportReminder() {
+    final ViewGroup container = (ViewGroup) reminderView.findViewById(R.id.container);
+
+    setReminderData(R.drawable.sms_system_import_icon,
+                    R.string.reminder_header_sms_import_title,
+                    R.string.reminder_header_sms_import_text,
+                    new OnClickListener() {
+                      @Override
+                      public void onClick(View v) {
+                        Intent intent = new Intent(getActivity(), ApplicationMigrationService.class);
+                        intent.setAction(ApplicationMigrationService.MIGRATE_DATABASE);
+                        intent.putExtra("master_secret", masterSecret);
+                        getActivity().startService(intent);
+
+                        Intent nextIntent = new Intent(getActivity(), ConversationListActivity.class);
+                        intent.putExtra("master_secret", masterSecret);
+
+                        Intent activityIntent = new Intent(getActivity(), DatabaseMigrationActivity.class);
+                        activityIntent.putExtra("master_secret", masterSecret);
+                        activityIntent.putExtra("next_intent", nextIntent);
+                        getActivity().startActivity(activityIntent);
+                      }
+                    },
+                    new OnClickListener() {
+                      @Override
+                      public void onClick(View v) {
+                        ApplicationMigrationService.setDatabaseImported(getActivity());
+                        container.setVisibility(View.GONE);
+                      }
+                    });
+    container.setVisibility(View.VISIBLE);
+  }
+
+  private void setReminderData(int iconResId, int titleResId, int textResId, OnClickListener okListener, OnClickListener cancelListener) {
+    final ImageButton cancel = (ImageButton) reminderView.findViewById(R.id.cancel);
+    final Button      ok     = (Button     ) reminderView.findViewById(R.id.ok);
+    final TextView    title  = (TextView   ) reminderView.findViewById(R.id.reminder_title);
+    final TextView    text   = (TextView   ) reminderView.findViewById(R.id.reminder_text);
+    final ImageView   icon   = (ImageView  ) reminderView.findViewById(R.id.icon);
+
+    icon.setImageResource(iconResId);
+    title.setText(titleResId);
+    text.setText(textResId);
+    ok.setOnClickListener(okListener);
+    cancel.setOnClickListener(cancelListener);
   }
 
 }
