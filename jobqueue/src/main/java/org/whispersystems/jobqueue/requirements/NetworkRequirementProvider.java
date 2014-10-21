689b0e2961f4999e3b80478d3ffa6bd99920d171
Moxie Marlinspike <moxie@thoughtcrime.org>
2014-10-20 18:25:18 -0700

    Incorporate PR feedback. Add license and eliminate duplicate code.
    // FREEBIE

--- jobqueue/src/main/java/org/whispersystems/jobqueue/requirements/NetworkRequirementProvider.java
+++ jobqueue/src/main/java/org/whispersystems/jobqueue/requirements/NetworkRequirementProvider.java
@@ -1,3 +1,19 @@
+/**
+ * Copyright (C) 2014 Open Whisper Systems
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package org.whispersystems.jobqueue.requirements;
 
 import android.content.BroadcastReceiver;
@@ -11,7 +27,11 @@ public class NetworkRequirementProvider implements RequirementProvider {
 
   private RequirementListener listener;
 
+  private final NetworkRequirement requirement;
+
   public NetworkRequirementProvider(Context context) {
+    this.requirement = new NetworkRequirement(context);
+
     context.getApplicationContext().registerReceiver(new BroadcastReceiver() {
       @Override
       public void onReceive(Context context, Intent intent) {
@@ -19,11 +39,7 @@ public class NetworkRequirementProvider implements RequirementProvider {
           return;
         }
 
-        ConnectivityManager cm        = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
-        NetworkInfo         netInfo   = cm.getActiveNetworkInfo();
-        boolean             connected = netInfo != null && netInfo.isConnectedOrConnecting();
-
-        if (connected) {
+        if (requirement.isPresent()) {
           listener.onRequirementStatusChanged();
         }
       }
