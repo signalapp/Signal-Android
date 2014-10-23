e7b6a852c50fe2c72426f006c5e1f045969a06b8
Moxie Marlinspike <moxie@thoughtcrime.org>
2014-10-23 16:02:37 -0700

    Fix bug caused by 1mod8 compatibility issue.
    In the switch from v3, we bind identities in the message MAC
    instead of doing the 1mod8 trick.  Since identity keys were
    never set as 1mod8, it seemed like we could just remove it.
    
    However, PreKeys are durable.  If an old client upgrades to v3,
    it has a bunch of keys that *were* set to 1mod8 floating around.
    The Curve25519 donna code re-sets the private key bits on every
    operation, which results in a different key, and breaks the output
    of an agreement.
    
    So now we don't intentionally generate keys with 1mod8, but we
    have to remove the donna code to honor existing 1mod8 keys for
    the rest of time.  Trevor is squarely to blame.
    
    // FREEBIE

--- libaxolotl/jni/curve25519-donna.c
+++ libaxolotl/jni/curve25519-donna.c
@@ -857,9 +857,9 @@ curve25519_donna(u8 *mypublic, const u8 *secret, const u8 *basepoint) {
   int i;
 
   for (i = 0; i < 32; ++i) e[i] = secret[i];
-  e[0] &= 248;
-  e[31] &= 127;
-  e[31] |= 64;
+//  e[0] &= 248;
+//  e[31] &= 127;
+//  e[31] |= 64;
 
   fexpand(bp, basepoint);
   cmult(x, z, e, bp);
