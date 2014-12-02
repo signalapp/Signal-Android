0b587d43d6e2c0fd8b3cae588f2011e7ebab977d
Aaron M Brown <aaron@aaronmbrown.net>
2014-12-01 21:06:17 -0800

    Fix issue 2100: explain size limit for video attachments.
    Also fixed for audio attachments.
    
    // FREEBIE

--- src/org/thoughtcrime/securesms/ConversationActivity.java
+++ src/org/thoughtcrime/securesms/ConversationActivity.java
@@ -863,7 +863,10 @@ public class ConversationActivity extends PassphraseRequiredActionBarActivity
       Log.w("ComposeMessageActivity", e);
     } catch (MediaTooLargeException e) {
       attachmentManager.clear();
-      Toast.makeText(this, R.string.ConversationActivity_sorry_the_selected_video_exceeds_message_size_restrictions,
+
+      Toast.makeText(this, getResources().
+        getString(R.string.ConversationActivity_sorry_the_selected_video_exceeds_message_size_restrictions,
+                 (Slide.MAX_MESSAGE_SIZE/1024)),
                      Toast.LENGTH_LONG).show();
       Log.w("ComposeMessageActivity", e);
     }
@@ -879,7 +882,9 @@ public class ConversationActivity extends PassphraseRequiredActionBarActivity
       Log.w("ComposeMessageActivity", e);
     } catch (MediaTooLargeException e) {
       attachmentManager.clear();
-      Toast.makeText(this, R.string.ConversationActivity_sorry_the_selected_audio_exceeds_message_size_restrictions,
+      Toast.makeText(this, getResources().
+        getString(R.string.ConversationActivity_sorry_the_selected_audio_exceeds_message_size_restrictions,
+                 (Slide.MAX_MESSAGE_SIZE/1024)),
                      Toast.LENGTH_LONG).show();
       Log.w("ComposeMessageActivity", e);
     }
