aa26785c008a8c028415c783a00227c6fd606a05
Moxie Marlinspike <moxie@thoughtcrime.org>
2015-02-12 10:30:45 -0800

    Reinclude SC provider for consistency.
    // FREEBIE
    
    Fixes #2461

--- src/org/thoughtcrime/securesms/ApplicationContext.java
+++ src/org/thoughtcrime/securesms/ApplicationContext.java
@@ -74,6 +74,7 @@ public class ApplicationContext extends Application implements DependencyInjecto
 
 
   private void initializeRandomNumberFix() {
+    Security.insertProviderAt(new org.spongycastle.jce.provider.BouncyCastleProvider(), 1);
     PRNGFixes.apply();
   }
 
