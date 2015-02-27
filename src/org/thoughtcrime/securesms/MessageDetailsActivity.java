2fda4d7d47c53b759484fbff6e48b032cf4c76cd
Jake McGinty <me@jake.su>
2015-02-27 12:08:41 -0800

    error header for message details
    // FREEBIE

--- src/org/thoughtcrime/securesms/MessageDetailsActivity.java
+++ src/org/thoughtcrime/securesms/MessageDetailsActivity.java
@@ -18,6 +18,7 @@ package org.thoughtcrime.securesms;
 
 import android.content.Context;
 import android.database.Cursor;
+import android.graphics.Paint;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.LoaderManager.LoaderCallbacks;
@@ -62,6 +63,8 @@ public class MessageDetailsActivity extends PassphraseRequiredActionBarActivity 
   private boolean          isPushGroup;
   private ConversationItem conversationItem;
   private ViewGroup        itemParent;
+  private View             metadataContainer;
+  private TextView         errorText;
   private TextView         sentDate;
   private TextView         receivedDate;
   private View             receivedContainer;
@@ -88,6 +91,8 @@ public class MessageDetailsActivity extends PassphraseRequiredActionBarActivity 
     isPushGroup       = getIntent().getBooleanExtra(IS_PUSH_GROUP_EXTRA, false);
     itemParent        = (ViewGroup) findViewById(R.id.item_container );
     recipientsList    = (ListView ) findViewById(R.id.recipients_list);
+    metadataContainer =             header.findViewById(R.id.metadata_container);
+    errorText         = (TextView ) header.findViewById(R.id.error_text);
     sentDate          = (TextView ) header.findViewById(R.id.sent_time);
     receivedContainer =             header.findViewById(R.id.received_container);
     receivedDate      = (TextView ) header.findViewById(R.id.received_time);
@@ -250,8 +255,15 @@ public class MessageDetailsActivity extends PassphraseRequiredActionBarActivity 
       inflateMessageViewIfAbsent(messageRecord);
 
       updateRecipients(messageRecord, recipients);
-      updateTransport(messageRecord);
-      updateTime(messageRecord);
+      if (messageRecord.isFailed()) {
+        errorText.setVisibility(View.VISIBLE);
+        metadataContainer.setVisibility(View.GONE);
+      } else {
+        updateTransport(messageRecord);
+        updateTime(messageRecord);
+        errorText.setVisibility(View.GONE);
+        metadataContainer.setVisibility(View.VISIBLE);
+      }
     }
 
   }
