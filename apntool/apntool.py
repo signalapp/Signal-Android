b536feb2e8ebab32c58df84385bd5718cec3e2ac
Jake McGinty <me@jake.su>
2015-03-06 15:34:03 -0800

    update APN list, fix MMSC issues
    Fixes #2413
    // FREEBIE

--- apntool/apntool.py
+++ apntool/apntool.py
@@ -1,18 +1,30 @@
 import sys
+import re
 import argparse
 import sqlite3
 import gzip
 from progressbar import ProgressBar, Counter, Timer
 from lxml import etree
 
-parser = argparse.ArgumentParser(prog='apntool', description="""Process Android's apn xml files and drop them into an easily
-                                                             queryable SQLite db. Tested up to version 9 of their APN file.""")
-parser.add_argument('-v', '--version', action='version', version='%(prog)s v1.0')
+parser = argparse.ArgumentParser(prog='apntool', description="""Process Android's apn xml files and drop them into an
+                                                             easily queryable SQLite db. Tested up to version 9 of
+                                                             their APN file.""")
+parser.add_argument('-v', '--version', action='version', version='%(prog)s v1.1')
 parser.add_argument('-i', '--input', help='the xml file to parse', default='apns.xml', required=False)
 parser.add_argument('-o', '--output', help='the sqlite db output file', default='apns.db', required=False)
 parser.add_argument('--quiet', help='do not show progress or verbose instructions', action='store_true', required=False)
 parser.add_argument('--no-gzip', help="do not gzip after creation", action='store_true', required=False)
 args = parser.parse_args()
+
+
+def normalized(target):
+    o2_typo = re.compile(r"02\.co\.uk")
+    port_typo = re.compile(r"(\d+\.\d+\.\d+\.\d+)\.(\d+)")
+    leading_zeros = re.compile(r"(/|\.|^)0+(\d+)")
+    subbed = o2_typo.sub(r'o2.co.uk', target)
+    subbed = port_typo.sub(r'\1:\2', subbed)
+    subbed = leading_zeros.sub(r'\1\2', subbed)
+    return subbed
 
 try:
     connection = sqlite3.connect(args.output)
@@ -28,26 +40,39 @@ try:
     cursor.execute("PRAGMA page_size=32768")
     cursor.execute("VACUUM")
     cursor.execute("DROP TABLE IF EXISTS apns")
-    cursor.execute("""CREATE TABLE apns(_id INTEGER PRIMARY KEY, mccmnc TEXT, mcc TEXT, mnc TEXT, carrier TEXT, apn TEXT,
-                mmsc TEXT, port INTEGER, type TEXT, protocol TEXT, bearer TEXT, roaming_protocol TEXT,
+    cursor.execute("""CREATE TABLE apns(_id INTEGER PRIMARY KEY, mccmnc TEXT, mcc TEXT, mnc TEXT, carrier TEXT,
+                apn TEXT, mmsc TEXT, port INTEGER, type TEXT, protocol TEXT, bearer TEXT, roaming_protocol TEXT,
                 carrier_enabled INTEGER, mmsproxy TEXT, mmsport INTEGER, proxy TEXT,  mvno_match_data TEXT,
                 mvno_type TEXT, authtype INTEGER, user TEXT, password TEXT, server TEXT)""")
 
     apns = etree.parse(args.input)
     root = apns.getroot()
-    pbar = ProgressBar(widgets=['Processed: ', Counter(), ' apns (', Timer(), ')'], maxval=len(list(root))).start() if not args.quiet else None
+    pbar = None
+    if not args.quiet:
+        pbar = ProgressBar(widgets=['Processed: ', Counter(), ' apns (', Timer(), ')'], maxval=len(list(root))).start()
 
     count = 0
     for apn in root.iter("apn"):
-        if apn.get("mmsc") == None:
+        if apn.get("mmsc") is None:
             continue
         sqlvars = ["?" for x in apn.attrib.keys()] + ["?"]
-        mccmnc  = "%s%s" % (apn.get("mcc"), apn.get("mnc"))
-        values  = [apn.get(attrib) for attrib in apn.attrib.keys()] + [mccmnc]
-        keys    = apn.attrib.keys() + ["mccmnc"]
+        mccmnc = "%s%s" % (apn.get("mcc"), apn.get("mnc"))
+        normalized_mmsc = normalized(apn.get("mmsc"))
+        if normalized_mmsc != apn.get("mmsc"):
+            print("normalize MMSC: %s => %s" % (apn.get("mmsc"), normalized_mmsc))
+            apn.set("mmsc", normalized_mmsc)
+
+        if not apn.get("mmsproxy") is None:
+            normalized_mmsproxy = normalized(apn.get("mmsproxy"))
+            if normalized_mmsproxy != apn.get("mmsproxy"):
+                print("normalize proxy: %s => %s" % (apn.get("mmsproxy"), normalized_mmsproxy))
+                apn.set("mmsproxy", normalized_mmsproxy)
+
+        values = [apn.get(attrib) for attrib in apn.attrib.keys()] + [mccmnc]
+        keys = apn.attrib.keys() + ["mccmnc"]
 
         cursor.execute("SELECT 1 FROM apns WHERE mccmnc = ? AND apn = ?", [mccmnc, apn.get("apn")])
-        if cursor.fetchone() == None:
+        if cursor.fetchone() is None:
             statement = "INSERT INTO apns (%s) VALUES (%s)" % (", ".join(keys), ", ".join(sqlvars))
             cursor.execute(statement, values)
 
