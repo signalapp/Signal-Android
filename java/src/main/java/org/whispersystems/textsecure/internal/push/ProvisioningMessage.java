9cada7e229c588d971b153fa1abc807fb266533c
lilia <liliakai@gmail.com>
2015-03-09 23:51:07 -0700

    Fix provisioning flow
    Make ProvisionMessage serializable. Previously the lack of jackson
    annotations caused this object to serialize as empty string, which
    elicits a 500 from the server.
    
    Closes #3

--- java/src/main/java/org/whispersystems/textsecure/internal/push/ProvisioningMessage.java
+++ java/src/main/java/org/whispersystems/textsecure/internal/push/ProvisioningMessage.java
@@ -1,7 +1,10 @@
 package org.whispersystems.textsecure.internal.push;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public class ProvisioningMessage {
 
+  @JsonProperty
   private String body;
 
   public ProvisioningMessage(String body) {
