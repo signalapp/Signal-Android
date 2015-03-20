a3f11afa3e1bb5a51e9176969717f20a19599c20
Rhodey Orbits <rhodey@anhonesteffort.org>
2015-03-20 15:29:02 -0700

    removed name from class file comments, added copyright

--- androidTest/java/org/thoughtcrime/securesms/preferences/NotificationsPreferenceFragmentTest.java
+++ androidTest/java/org/thoughtcrime/securesms/preferences/NotificationsPreferenceFragmentTest.java
@@ -1,3 +1,19 @@
+/**
+ * Copyright (C) 2015 Open Whisper Systems
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package org.thoughtcrime.securesms.preferences;
 
 import android.test.suitebuilder.annotation.LargeTest;
@@ -15,9 +31,6 @@ import static android.support.test.espresso.matcher.ViewMatchers.isChecked;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.isNotChecked;
 
-/**
- * rhodey
- */
 @LargeTest
 public class NotificationsPreferenceFragmentTest extends SkipRegistrationInstrumentationTestCase {
 
