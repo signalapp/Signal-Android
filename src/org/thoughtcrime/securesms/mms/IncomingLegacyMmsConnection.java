a8e69cfda4ae132b7e03cdf439c530ab16f45f16
Jake McGinty <me@jake.su>
2015-03-23 12:07:05 -0700

    copyright headers

--- src/org/thoughtcrime/securesms/mms/IncomingLegacyMmsConnection.java
+++ src/org/thoughtcrime/securesms/mms/IncomingLegacyMmsConnection.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (C) 2014 Open Whisper Systems
+ * Copyright (C) 2015 Open Whisper Systems
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
