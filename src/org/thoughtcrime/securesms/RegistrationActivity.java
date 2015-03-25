1f56d41f5da6576caca732cdf0b86ee6c20287e7
agrajaghh <agrajaghh@gmail.com>
2015-03-25 09:48:55 +0100

    display the phone number in the confirm-AlertDialog during registration

--- src/org/thoughtcrime/securesms/RegistrationActivity.java
+++ src/org/thoughtcrime/securesms/RegistrationActivity.java
@@ -197,8 +197,8 @@ public class RegistrationActivity extends BaseActionBarActivity {
       }
 
       AlertDialog.Builder dialog = new AlertDialog.Builder(self);
-      dialog.setMessage(String.format(getString(R.string.RegistrationActivity_we_will_now_verify_that_the_following_number_is_associated_with_your_device_s),
-                                      PhoneNumberFormatter.getInternationalFormatFromE164(e164number)));
+      dialog.setTitle(PhoneNumberFormatter.getInternationalFormatFromE164(e164number));
+      dialog.setMessage(R.string.RegistrationActivity_we_will_now_verify_that_the_following_number_is_associated_with_your_device_s);
       dialog.setPositiveButton(getString(R.string.RegistrationActivity_continue),
                                new DialogInterface.OnClickListener() {
                                  @Override
