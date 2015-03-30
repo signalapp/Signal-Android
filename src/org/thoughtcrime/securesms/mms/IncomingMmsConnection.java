53fb7c31eacdb3bfae3ff74991fa738ffa31af5e
Jake McGinty <me@jake.su>
2015-03-30 10:46:14 -0700

    Fix NPE when RetrieveConf is null
    Fixes #2828
    // FREEBIE

--- src/org/thoughtcrime/securesms/mms/IncomingMmsConnection.java
+++ src/org/thoughtcrime/securesms/mms/IncomingMmsConnection.java
@@ -1,10 +1,12 @@
 package org.thoughtcrime.securesms.mms;
 
+import android.support.annotation.Nullable;
+
 import java.io.IOException;
 
 import ws.com.google.android.mms.MmsException;
 import ws.com.google.android.mms.pdu.RetrieveConf;
 
 public interface IncomingMmsConnection {
-  RetrieveConf retrieve(String contentLocation, byte[] transactionId) throws MmsException, MmsRadioException, ApnUnavailableException, IOException;
+  @Nullable RetrieveConf retrieve(String contentLocation, byte[] transactionId) throws MmsException, MmsRadioException, ApnUnavailableException, IOException;
 }
