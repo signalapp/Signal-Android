7724964a365640287f9417177344adecb2c8ac91
Carey Metcalfe <pR0Ps.CM@gmail.com>
2015-04-16 10:52:50 -0400

    Correct the notification repeat interval
    Switch to 2min from 10sec.
    
    Closes #3002

--- src/org/thoughtcrime/securesms/notifications/MessageNotifier.java
+++ src/org/thoughtcrime/securesms/notifications/MessageNotifier.java
@@ -429,7 +429,7 @@ public class MessageNotifier {
     alarmIntent.putExtra("reminder_count", count);
 
     PendingIntent pendingIntent = PendingIntent.getBroadcast(context, 0, alarmIntent, PendingIntent.FLAG_CANCEL_CURRENT);
-    long          timeout       = TimeUnit.SECONDS.toMillis(10);
+    long          timeout       = TimeUnit.MINUTES.toMillis(2);
 
     alarmManager.set(AlarmManager.RTC_WAKEUP, System.currentTimeMillis() + timeout, pendingIntent);
   }
