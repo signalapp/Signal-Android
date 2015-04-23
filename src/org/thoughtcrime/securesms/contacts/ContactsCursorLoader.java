e290ca221ff1c49f1e5ba4c4218c270f41b009c0
Jake McGinty <me@jake.su>
2015-04-23 14:53:13 -0500

    use semaphores in loader

--- src/org/thoughtcrime/securesms/contacts/ContactsCursorLoader.java
+++ src/org/thoughtcrime/securesms/contacts/ContactsCursorLoader.java
@@ -19,6 +19,11 @@ package org.thoughtcrime.securesms.contacts;
 import android.content.Context;
 import android.database.Cursor;
 import android.support.v4.content.CursorLoader;
+import android.util.Log;
+
+import junit.framework.Assert;
+
+import java.util.concurrent.Semaphore;
 
 /**
  * CursorLoader that initializes a ContactsDatabase instance
@@ -26,11 +31,13 @@ import android.support.v4.content.CursorLoader;
  * @author Jake McGinty
  */
 public class ContactsCursorLoader extends CursorLoader {
+  private static final String TAG        = ContactsCursorLoader.class.getSimpleName();
+  private static final int    DB_PERMITS = 100;
 
   private final Context          context;
   private final String           filter;
   private final boolean          pushOnly;
-  private final Object           dbLock = new Object();
+  private final Semaphore        dbSemaphore = new Semaphore(DB_PERMITS);
   private       ContactsDatabase db;
 
   public ContactsCursorLoader(Context context, String filter, boolean pushOnly) {
@@ -43,16 +50,27 @@ public class ContactsCursorLoader extends CursorLoader {
 
   @Override
   public Cursor loadInBackground() {
-    synchronized (dbLock) {
+    try {
+      dbSemaphore.acquire();
       return db.query(filter, pushOnly);
+    } catch (InterruptedException ie) {
+      throw new AssertionError(ie);
+    } finally {
+      dbSemaphore.release();
     }
   }
 
   @Override
   public void onReset() {
-    synchronized (dbLock) {
+    Log.w(TAG, "onReset()");
+    try {
+      dbSemaphore.acquire(DB_PERMITS);
       db.close();
       db = new ContactsDatabase(context);
+    } catch (InterruptedException ie) {
+      throw new AssertionError(ie);
+    } finally {
+      dbSemaphore.release(DB_PERMITS);
     }
     super.onReset();
   }
