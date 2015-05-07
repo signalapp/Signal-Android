4ad9d6b5cc476106974b76c13984707ee52ec39f
Moxie Marlinspike <moxie@thoughtcrime.org>
2015-05-07 11:34:07 -0700

    Fix for drawable modification on background thread.
    // FREEBIE

--- src/org/thoughtcrime/securesms/util/BitmapUtil.java
+++ src/org/thoughtcrime/securesms/util/BitmapUtil.java
@@ -268,7 +268,6 @@ public class BitmapUtil {
 
     Bitmap bitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
     Canvas canvas = new Canvas(bitmap);
-    drawable.setBounds(0, 0, canvas.getWidth(), canvas.getHeight());
     drawable.draw(canvas);
 
     return bitmap;
