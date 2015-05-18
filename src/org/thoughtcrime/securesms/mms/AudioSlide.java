98644917f873293b3b7c0a2ac9a4c38370b0547c
Moxie Marlinspike <moxie@thoughtcrime.org>
2015-05-18 08:38:48 -0700

    Fix for media thumbnails flickering on model updates.
    Only update ImageView contents when they have changed.
    
    Fixes #1004
    Fixes #2663
    
    // FREEBIE

--- src/org/thoughtcrime/securesms/mms/AudioSlide.java
+++ src/org/thoughtcrime/securesms/mms/AudioSlide.java
@@ -42,12 +42,12 @@ public class AudioSlide extends Slide {
   }
 
   @Override
-    public boolean hasImage() {
+  public boolean hasImage() {
     return true;
   }
 
   @Override
-    public boolean hasAudio() {
+  public boolean hasAudio() {
     return true;
   }
 
