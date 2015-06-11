26bcb590a7a32159a09e289a6170db15ea0860e7
Moxie Marlinspike <moxie@thoughtcrime.org>
2015-06-11 09:43:34 -0700

    Log retrieve exception.
    // FREEBIE

--- src/org/thoughtcrime/securesms/jobs/PushNotificationReceiveJob.java
+++ src/org/thoughtcrime/securesms/jobs/PushNotificationReceiveJob.java
@@ -43,6 +43,7 @@ public class PushNotificationReceiveJob extends PushReceivedJob implements Injec
 
   @Override
   public boolean onShouldRetry(Exception e) {
+    Log.w(TAG, e);
     return e instanceof PushNetworkException;
   }
 
