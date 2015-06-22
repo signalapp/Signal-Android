98d1ab2dd087fa8866a43627a4d17ba8a84bc0a1
Moxie Marlinspike <moxie@thoughtcrime.org>
2015-06-22 08:46:43 -0700

    Support for quick reply from notifications.
    Fixes #483
    
    // FREEBIE

--- src/org/thoughtcrime/securesms/notifications/NotificationItem.java
+++ src/org/thoughtcrime/securesms/notifications/NotificationItem.java
@@ -8,6 +8,7 @@ import android.support.annotation.Nullable;
 import android.text.SpannableStringBuilder;
 
 import org.thoughtcrime.securesms.ConversationActivity;
+import org.thoughtcrime.securesms.ConversationPopupActivity;
 import org.thoughtcrime.securesms.recipients.Recipient;
 import org.thoughtcrime.securesms.recipients.Recipients;
 import org.thoughtcrime.securesms.util.Util;
@@ -81,8 +82,7 @@ public class NotificationItem {
   }
 
   public PendingIntent getPendingIntent(Context context) {
-    Intent intent = new Intent(context, ConversationActivity.class);
-
+    Intent     intent           = new Intent(context, ConversationActivity.class);
     Recipients notifyRecipients = threadRecipients != null ? threadRecipients : recipients;
     if (notifyRecipients != null) intent.putExtra("recipients", notifyRecipients.getIds());
 
@@ -91,5 +91,17 @@ public class NotificationItem {
 
     return PendingIntent.getActivity(context, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
   }
+
+  public PendingIntent getReplyIntent(Context context) {
+    Intent     intent           = new Intent(context, ConversationPopupActivity.class);
+    Recipients notifyRecipients = threadRecipients != null ? threadRecipients : recipients;
+    if (notifyRecipients != null) intent.putExtra(ConversationActivity.RECIPIENTS_EXTRA, notifyRecipients.getIds());
+
+    intent.putExtra(ConversationActivity.THREAD_ID_EXTRA, threadId);
+    intent.setData((Uri.parse("custom://"+System.currentTimeMillis())));
+
+    return PendingIntent.getActivity(context, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+  }
+
 
 }
