af226673da3a8eb27b2db90ee8f321681090f7ae
Moxie Marlinspike <moxie@thoughtcrime.org>
2015-06-25 09:50:52 -0700

    Make the click listener equally inaccessible for now.
    // FREEBIE

--- src/org/thoughtcrime/securesms/ApplicationPreferencesActivity.java
+++ src/org/thoughtcrime/securesms/ApplicationPreferencesActivity.java
@@ -132,8 +132,8 @@ public class ApplicationPreferencesActivity extends PassphraseRequiredActionBarA
         .setOnPreferenceClickListener(new CategoryClickListener(masterSecret, PREFERENCE_CATEGORY_APPEARANCE));
       this.findPreference(PREFERENCE_CATEGORY_STORAGE)
         .setOnPreferenceClickListener(new CategoryClickListener(masterSecret, PREFERENCE_CATEGORY_STORAGE));
-      this.findPreference(PREFERENCE_CATEGORY_DEVICES)
-        .setOnPreferenceClickListener(new CategoryClickListener(masterSecret, PREFERENCE_CATEGORY_DEVICES));
+//      this.findPreference(PREFERENCE_CATEGORY_DEVICES)
+//        .setOnPreferenceClickListener(new CategoryClickListener(masterSecret, PREFERENCE_CATEGORY_DEVICES));
       this.findPreference(PREFERENCE_CATEGORY_ADVANCED)
         .setOnPreferenceClickListener(new CategoryClickListener(masterSecret, PREFERENCE_CATEGORY_ADVANCED));
     }
