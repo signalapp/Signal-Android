276495b48edc0462506b810ce4a9cccd51184f8a
Jake McGinty <me@jake.su>
2015-07-02 13:18:56 -0700

    fix emoji height

--- src/org/thoughtcrime/securesms/util/SoftKeyboardUtil.java
+++ src/org/thoughtcrime/securesms/util/SoftKeyboardUtil.java
@@ -3,13 +3,18 @@ package org.thoughtcrime.securesms.util;
 import android.app.Activity;
 import android.content.Context;
 import android.graphics.Rect;
+import android.os.Build;
+import android.os.Build.VERSION_CODES;
 import android.preference.PreferenceManager;
 import android.util.Log;
 import android.view.Surface;
+import android.view.View;
 import android.view.ViewGroup;
 import android.view.WindowManager;
 
 import org.thoughtcrime.securesms.R;
+
+import java.lang.reflect.Field;
 
 public class SoftKeyboardUtil {
   private static final String TAG  = SoftKeyboardUtil.class.getSimpleName();
@@ -19,7 +24,7 @@ public class SoftKeyboardUtil {
     view.getWindowVisibleDisplayFrame(rect);
     final int res               = view.getResources().getIdentifier("status_bar_height", "dimen", "android");
     final int statusBarHeight   = res > 0 ? view.getResources().getDimensionPixelSize(res) : 0;
-    final int availableHeight   = view.getRootView().getHeight() - statusBarHeight;
+    final int availableHeight   = view.getRootView().getHeight() - statusBarHeight - getViewInset(view);
     final int keyboardHeight    = availableHeight                - (rect.bottom - rect.top);
     final int minKeyboardHeight = view.getResources().getDimensionPixelSize(R.dimen.min_emoji_drawer_height);
 
@@ -28,6 +33,30 @@ public class SoftKeyboardUtil {
     }
     return Math.max(keyboardHeight, minKeyboardHeight);
   }
+
+  private static int getViewInset(ViewGroup view) {
+    if (Build.VERSION.SDK_INT < VERSION_CODES.LOLLIPOP) {
+      return 0;
+    }
+
+    try {
+      Field attachInfoField = View.class.getDeclaredField("mAttachInfo");
+      attachInfoField.setAccessible(true);
+      Object attachInfo = attachInfoField.get(view);
+      if (attachInfo != null) {
+        Field stableInsetsField = attachInfo.getClass().getDeclaredField("mStableInsets");
+        stableInsetsField.setAccessible(true);
+        Rect insets = (Rect)stableInsetsField.get(attachInfo);
+        return insets.bottom;
+      }
+    } catch (NoSuchFieldException nsfe) {
+      Log.w(TAG, "field reflection error when measuring view inset", nsfe);
+    } catch (IllegalAccessException iae) {
+      Log.w(TAG, "access reflection error when measuring view inset", iae);
+    }
+    return 0;
+  }
+
 
   private static void onKeyboardShown(Context context, int keyboardHeight) {
     Log.w(TAG, "keyboard shown, height " + keyboardHeight);
