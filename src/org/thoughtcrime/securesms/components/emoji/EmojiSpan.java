4b2eb2f303dd1f4100d89c9cfdd303b2d7e5bb31
Jake McGinty <me@jake.su>
2015-07-10 14:58:05 -0700

    add EmojiSpan that scaled w/ TextView
    fixes #3317
    // FREEBIE

---
+++ src/org/thoughtcrime/securesms/components/emoji/EmojiSpan.java
@@ -0,0 +1,18 @@
+package org.thoughtcrime.securesms.components.emoji;
+
+import android.graphics.Paint.FontMetricsInt;
+import android.graphics.drawable.Drawable;
+import android.support.annotation.NonNull;
+import android.widget.TextView;
+
+import org.thoughtcrime.securesms.R;
+
+public class EmojiSpan extends AnimatingImageSpan {
+  public EmojiSpan(@NonNull Drawable drawable, @NonNull TextView tv) {
+    super(drawable, tv);
+    FontMetricsInt fm = tv.getPaint().getFontMetricsInt();
+    final int size = fm != null ? Math.abs(fm.descent) + Math.abs(fm.ascent)
+                                : tv.getResources().getDimensionPixelSize(R.dimen.conversation_item_body_text_size);
+    getDrawable().setBounds(0, 0, size, size);
+  }
+}
