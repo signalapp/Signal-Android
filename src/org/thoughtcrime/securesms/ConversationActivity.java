3eab88dfe172be75a318f716485d64a2ecd48793
Jake McGinty <me@jake.su>
2015-07-13 12:32:35 -0700

    update IME action label
    fixes #3623
    // FREEBIE

--- src/org/thoughtcrime/securesms/ConversationActivity.java
+++ src/org/thoughtcrime/securesms/ConversationActivity.java
@@ -823,6 +823,7 @@ public class ConversationActivity extends PassphraseRequiredActionBarActivity
         calculateCharactersRemaining();
         composeText.setHint(newTransport.getComposeHint());
         composeText.setImeActionLabel(newTransport.getComposeHint(), EditorInfo.IME_ACTION_SEND);
+        composeText.setInputType(composeText.getInputType());
         buttonToggle.getBackground().setColorFilter(newTransport.getBackgroundColor(), Mode.MULTIPLY);
       }
     });
