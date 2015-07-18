4d8cf16e088d6c2917b07194d1c172f1423ff978
Calvin Hu <fivepercentjuice@gmail.com>
2015-07-18 17:06:02 -0400

    when camera preview is open, the preview toggle button will close the preview
    closes #3574

--- src/org/thoughtcrime/securesms/ConversationActivity.java
+++ src/org/thoughtcrime/securesms/ConversationActivity.java
@@ -1367,8 +1367,12 @@ public class ConversationActivity extends PassphraseRequiredActionBarActivity
   private class QuickAttachmentToggleListener implements OnClickListener {
     @Override
     public void onClick(View v) {
-      composeText.clearFocus();
-      container.show(composeText, quickAttachmentDrawer);
+      if (!quickAttachmentDrawer.isShowing()) {
+        composeText.clearFocus();
+        container.show(composeText, quickAttachmentDrawer);
+      } else {
+        container.hideAttachedInput(false);
+      }
     }
   }
 
