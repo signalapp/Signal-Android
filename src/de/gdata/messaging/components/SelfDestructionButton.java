9459fe61460b84035d8c93df44d76855fc9643c1
Denis Vollmer <denis.vollmer@gdata.de>
2015-07-22 15:47:33 +0200

    Issue #47256 - shortened DE & EN string - removed seconds from string

--- src/de/gdata/messaging/components/SelfDestructionButton.java
+++ src/de/gdata/messaging/components/SelfDestructionButton.java
@@ -50,7 +50,7 @@ public class SelfDestructionButton extends ImageButton {
                 setImageResource(newTransport.drawable);
                 if (Integer.parseInt(newTransport.key) > 0) {
                     if (composeText != null)
-                        setComposeTextHint(getResources().getString(R.string.self_destruction_compose_hint) + " (" + newTransport.key+")");
+                        setComposeTextHint(getResources().getString(R.string.self_destruction_compose_hint));
                 } else {
                     setImageResource(R.drawable.ic_action_timebomb);
                     if (composeText != null) {
