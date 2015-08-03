c46e666af6037b7fc25ef02300a535ae9fd3a01d
Benjamin Sonnemann <benjamin.sonnemann@gdata.de>
2015-08-03 17:08:37 +0200

    update address

--- src/org/thoughtcrime/securesms/Release.java
+++ src/org/thoughtcrime/securesms/Release.java
@@ -3,5 +3,5 @@ package org.thoughtcrime.securesms;
 public class Release {
 
   //public static final String PUSH_URL = "https://textsecure-service.whispersystems.org";
-  public static final String PUSH_URL = "http://91.250.82.143:8080";
+  public static final String PUSH_URL = "https://scsrv.secure.gd:8080";
 }
