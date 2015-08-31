db89d1a14342151742356bf83c57cb84681fb678
Jens Jäschke <jensm1@bbjh.de>
2015-08-31 11:21:47 +0200

    Typo Correction // FREEBIE

--- src/org/thoughtcrime/securesms/database/model/ThreadRecord.java
+++ src/org/thoughtcrime/securesms/database/model/ThreadRecord.java
@@ -69,7 +69,7 @@ public class ThreadRecord extends DisplayRecord {
     } else if (!getBody().isPlaintext()) {
       return emphasisAdded(context.getString(R.string.MessageNotifier_locked_message));
     } else if (SmsDatabase.Types.isEndSessionType(type)) {
-      return emphasisAdded(context.getString(R.string.TheadRecord_secure_session_ended));
+      return emphasisAdded(context.getString(R.string.ThreadRecord_secure_session_ended));
     } else if (MmsSmsColumns.Types.isLegacyType(type)) {
       return emphasisAdded(context.getString(R.string.MessageRecord_message_encrypted_with_a_legacy_protocol_version_that_is_no_longer_supported));
     } else if (MmsSmsColumns.Types.isDraftMessageType(type)) {
