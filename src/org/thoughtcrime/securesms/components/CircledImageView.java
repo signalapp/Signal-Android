22fff9f4758af32fee4f68a77d2eb41b25b80468
Jan Lenke <jan.lenke@gdata.de>
2015-09-07 13:15:27 +0200

    Bug #48646 - thumbnail circle scale was wrong

--- src/org/thoughtcrime/securesms/components/CircledImageView.java
+++ src/org/thoughtcrime/securesms/components/CircledImageView.java
@@ -58,7 +58,7 @@ public class CircledImageView extends ImageView {
     if (!(drawable instanceof GlideBitmapDrawable) && !(drawable instanceof SquaringDrawable)) {
       b = ((BitmapDrawable) drawable).getBitmap();
       bitmap = b.copy(Bitmap.Config.ARGB_8888, true);
-      roundBitmap = getRoundedCroppedBitmap(bitmap, low);
+      roundBitmap = BitmapUtil.scaleCircleCenterCrop(context, bitmap, low);
     } else if(!(drawable instanceof SquaringDrawable)){
       b = ((GlideBitmapDrawable) drawable).getBitmap();
       bitmap = b.copy(Bitmap.Config.ARGB_8888, true);
@@ -72,37 +72,5 @@ public class CircledImageView extends ImageView {
     canvas.drawBitmap(roundBitmap, 0, padding, null);
     bitmap.recycle();
     roundBitmap.recycle();
-  }
-
-  public static Bitmap getRoundedCroppedBitmap(Bitmap bitmap, int radius) {
-    Bitmap finalBitmap;
-    boolean recycle = false;
-    if (bitmap.getWidth() != radius || bitmap.getHeight() != radius) {
-      finalBitmap = Bitmap.createScaledBitmap(bitmap, radius, radius, false);
-      recycle = true;
-    }else {
-      finalBitmap = bitmap;
-    }
-    Bitmap output = Bitmap.createBitmap(finalBitmap.getWidth(),
-        finalBitmap.getHeight(), Bitmap.Config.ARGB_8888);
-    Canvas canvas = new Canvas(output);
-
-    final Paint paint = new Paint();
-    final Rect rect = new Rect(0, 0, finalBitmap.getWidth(),
-        finalBitmap.getHeight());
-
-    paint.setAntiAlias(true);
-    paint.setFilterBitmap(true);
-    paint.setDither(true);
-    canvas.drawARGB(0, 0, 0, 0);
-    paint.setColor(Color.parseColor("#BAB399"));
-    canvas.drawCircle(finalBitmap.getWidth() / 2 + 0.7f,
-        finalBitmap.getHeight() / 2 + 0.7f,
-        finalBitmap.getWidth() / 2 + 0.1f, paint);
-    paint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.SRC_IN));
-    canvas.drawBitmap(finalBitmap, rect, rect, paint);
-    if(recycle)
-      finalBitmap.recycle();
-    return output;
   }
 }
