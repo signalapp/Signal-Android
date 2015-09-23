92593d459b80ae0a9a275e7869a448dbf8003142
Moxie Marlinspike <moxie@thoughtcrime.org>
2015-09-23 14:43:50 -0700

    Remove the final vestiges of encrypted SMS
    Fixes #4026
    // FREEBIE

--- src/org/thoughtcrime/securesms/protocol/EndSessionWirePrefix.java
+++
@@ -1,8 +0,0 @@
-package org.thoughtcrime.securesms.protocol;
-
-public class EndSessionWirePrefix extends WirePrefix {
-  @Override
-  public String calculatePrefix(String message) {
-    return super.calculateEndSessionPrefix(message);
-  }
-}
