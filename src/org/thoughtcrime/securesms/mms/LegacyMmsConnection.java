1a67f33764b0ad88b4eb8be8597534c5f4ed9ee8
char32 <char32@mailbox.org>
2015-09-28 21:13:52 +0200

    Adding Verizon MSISDN header
    This patch adds the verizon MSISDN x-vzw-mdn, which
    is the number without leading country code.
    The parsing of the number is done by the PhoneNumberUtil.
    
    fixes #3150

--- src/org/thoughtcrime/securesms/mms/LegacyMmsConnection.java
+++ src/org/thoughtcrime/securesms/mms/LegacyMmsConnection.java
@@ -43,6 +43,9 @@ import org.thoughtcrime.securesms.util.Conversions;
 import org.thoughtcrime.securesms.util.TextSecurePreferences;
 import org.thoughtcrime.securesms.util.Util;
 import org.whispersystems.libaxolotl.util.guava.Optional;
+import com.google.i18n.phonenumbers.PhoneNumberUtil;
+import com.google.i18n.phonenumbers.NumberParseException;
+import com.google.i18n.phonenumbers.Phonenumber;
 
 import java.io.BufferedInputStream;
 import java.io.ByteArrayOutputStream;
@@ -195,7 +198,15 @@ public abstract class LegacyMmsConnection {
 
   protected List<Header> getBaseHeaders() {
     final String number = TelephonyUtil.getManager(context).getLine1Number();
-    final String number_without_cc = number.substring(2, number.length()); // strip of country code
+    final String numberWithoutCC;
+    PhoneNumberUtil numberUtil = PhoneNumberUtil.getInstance();
+    Phonenumber pn = numberUtil.parse(number, null);
+    try {
+      numberWithoutCC = numberUtil.getNationalSignificantNumber(pn);
+    } catch (NumberParseException e) {
+      Log.w(TAG, e);
+    }
+ 
     return new LinkedList<Header>() {{
       add(new BasicHeader("Accept", "*/*, application/vnd.wap.mms-message, application/vnd.wap.sic"));
       add(new BasicHeader("x-wap-profile", "http://www.google.com/oha/rdf/ua-profile-kila.xml"));
@@ -204,9 +215,7 @@ public abstract class LegacyMmsConnection {
       if (!TextUtils.isEmpty(number)) {
         add(new BasicHeader("x-up-calling-line-id", number));
         add(new BasicHeader("X-MDN", number));
-        
-        // adding Verizon header
-        add(new BasicHeader("x-vzw-mdn", number_without_cc));
+        add(new BasicHeader("x-vzw-mdn", numberWithoutCC));
       }
     }};
   }
