a2b7b4635019905199de13041405a60d828e2969
Manuel Geier <mangei@ymail.com>
2015-10-23 23:01:53 +0200

    show total number of messages or threads to be deleted in delete-confirm-dialogs
    Closes #4284
    
    // FREEBIE

--- src/org/thoughtcrime/securesms/ConversationListFragment.java
+++ src/org/thoughtcrime/securesms/ConversationListFragment.java
@@ -161,10 +161,15 @@ public class ConversationListFragment extends Fragment
   }
 
   private void handleDeleteAllSelected() {
+    int conversationsCount = getListAdapter().getBatchSelections().size();
     AlertDialogWrapper.Builder alert = new AlertDialogWrapper.Builder(getActivity());
     alert.setIconAttribute(R.attr.dialog_alert_icon);
-    alert.setTitle(R.string.ConversationListFragment_delete_threads_question);
-    alert.setMessage(R.string.ConversationListFragment_are_you_sure_you_wish_to_delete_all_selected_conversation_threads);
+    alert.setTitle(getContext().getResources().getQuantityString(
+            R.plurals.ConversationListFragment_delete_selected_conversations,
+            conversationsCount, conversationsCount));
+    alert.setMessage(getContext().getResources().getQuantityString(
+            R.plurals.ConversationListFragment_this_will_permanently_delete_all_n_selected_conversations,
+            conversationsCount, conversationsCount));
     alert.setCancelable(true);
 
     alert.setPositiveButton(R.string.delete, new DialogInterface.OnClickListener() {
