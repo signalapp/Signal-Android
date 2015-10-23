89f04cb1ac684d0d94cfea767f93cb2a729a3e7d
Jake McGinty <me@jake.su>
2015-10-23 12:47:21 -0700

    go back to old reminder view look
    // FREEBIE

--- src/org/thoughtcrime/securesms/components/reminder/ReminderView.java
+++ src/org/thoughtcrime/securesms/components/reminder/ReminderView.java
@@ -7,6 +7,7 @@ import android.util.AttributeSet;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
@@ -17,11 +18,11 @@ import org.thoughtcrime.securesms.util.ViewUtil;
  * View to display actionable reminders to the user
  */
 public class ReminderView extends LinearLayout {
-  private ViewGroup   container;
-  private TextView    acceptButton;
-  private TextView    closeButton;
-  private TextView    title;
-  private TextView    text;
+  private ViewGroup       container;
+  private ImageButton     closeButton;
+  private TextView        title;
+  private TextView        text;
+  private OnClickListener dismissListener;
 
   public ReminderView(Context context) {
     super(context);
@@ -41,19 +42,17 @@ public class ReminderView extends LinearLayout {
 
   private void initialize() {
     LayoutInflater.from(getContext()).inflate(R.layout.reminder_header, this, true);
-    container    = ViewUtil.findById(this, R.id.container);
-    acceptButton = ViewUtil.findById(this, R.id.accept);
-    closeButton  = ViewUtil.findById(this, R.id.cancel);
-    title        = ViewUtil.findById(this, R.id.reminder_title);
-    text         = ViewUtil.findById(this, R.id.reminder_text);
+    container = ViewUtil.findById(this, R.id.container);
+    closeButton = ViewUtil.findById(this, R.id.cancel);
+    title       = ViewUtil.findById(this, R.id.reminder_title);
+    text        = ViewUtil.findById(this, R.id.reminder_text);
   }
 
   public void showReminder(final Reminder reminder) {
     title.setText(reminder.getTitle());
     text.setText(reminder.getText());
-    acceptButton.setText(reminder.getButtonText());
 
-    acceptButton.setOnClickListener(reminder.getOkListener());
+    setOnClickListener(reminder.getOkListener());
 
     if (reminder.isDismissable()) {
       closeButton.setOnClickListener(new OnClickListener() {
@@ -61,6 +60,7 @@ public class ReminderView extends LinearLayout {
         public void onClick(View v) {
           hide();
           if (reminder.getDismissListener() != null) reminder.getDismissListener().onClick(v);
+          if (dismissListener != null)               dismissListener.onClick(v);
         }
       });
     } else {
@@ -68,6 +68,10 @@ public class ReminderView extends LinearLayout {
     }
 
     container.setVisibility(View.VISIBLE);
+  }
+
+  public void setDismissClickListener(OnClickListener dismissListener) {
+    this.dismissListener = dismissListener;
   }
 
   public void requestDismiss() {
