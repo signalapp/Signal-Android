23a2a5e5f34fb41673e9f4fb259a2c1f81cd0d1b
Moxie Marlinspike <moxie@thoughtcrime.org>
2015-10-23 12:24:43 -0700

    Fix test
    // FREEBIE

--- test/unitTest/java/org/thoughtcrime/securesms/jobs/AttachmentDownloadJobTest.java
+++ test/unitTest/java/org/thoughtcrime/securesms/jobs/AttachmentDownloadJobTest.java
@@ -5,12 +5,11 @@ import android.content.Context;
 import org.junit.Before;
 import org.junit.Test;
 import org.thoughtcrime.securesms.BaseUnitTest;
-import org.thoughtcrime.securesms.database.AttachmentDatabase.AttachmentId;
+import org.thoughtcrime.securesms.attachments.Attachment;
+import org.thoughtcrime.securesms.attachments.AttachmentId;
 import org.thoughtcrime.securesms.jobs.AttachmentDownloadJob.InvalidPartException;
-import org.thoughtcrime.securesms.util.Util;
 
-import ws.com.google.android.mms.pdu.PduPart;
-
+import static org.mockito.Mockito.when;
 import static org.powermock.api.mockito.PowerMockito.mock;
 
 public class AttachmentDownloadJobTest extends BaseUnitTest {
@@ -25,15 +24,19 @@ public class AttachmentDownloadJobTest extends BaseUnitTest {
 
   @Test(expected = InvalidPartException.class)
   public void testCreateAttachmentPointerInvalidId() throws Exception {
-    PduPart part = new PduPart();
-    part.setContentDisposition(Util.toIsoBytes("a long and acceptable valid key like we all want"));
-    job.createAttachmentPointer(masterSecret, part);
+    Attachment attachment = mock(Attachment.class);
+    when(attachment.getLocation()).thenReturn(null);
+    when(attachment.getKey()).thenReturn("a long and acceptable valid key like we all want");
+
+    job.createAttachmentPointer(masterSecret, attachment);
   }
 
   @Test(expected = InvalidPartException.class)
   public void testCreateAttachmentPointerInvalidKey() throws Exception {
-    PduPart part = new PduPart();
-    part.setContentDisposition(Util.toIsoBytes("1"));
-    job.createAttachmentPointer(masterSecret, part);
+    Attachment attachment = mock(Attachment.class);
+    when(attachment.getLocation()).thenReturn("something");
+    when(attachment.getKey()).thenReturn(null);
+
+    job.createAttachmentPointer(masterSecret, attachment);
   }
 }
