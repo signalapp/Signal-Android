7eaceb21bb5e1c9c16562381301335c933f6975b
Christian Ascheberg <c.ascheberg@gmx.de>
2015-11-04 14:03:01 +0100

    warn user before build expires
    // FREEBIE

--- src/org/thoughtcrime/securesms/components/reminder/OutdatedBuildReminder.java
+++ src/org/thoughtcrime/securesms/components/reminder/OutdatedBuildReminder.java
@@ -1,20 +1,38 @@
 package org.thoughtcrime.securesms.components.reminder;
 
 import android.content.Context;
-import android.content.res.Resources;
+import android.content.Intent;
+import android.net.Uri;
+import android.view.View;
 
 import org.thoughtcrime.securesms.R;
 import org.thoughtcrime.securesms.util.Util;
 
-public class OutdatedBuildReminder extends ExpiredBuildReminder {
+public class OutdatedBuildReminder extends Reminder {
 
   public OutdatedBuildReminder(final Context context) {
-    super(context);
+    super(context.getString(R.string.reminder_header_outdated_build),
+          getPluralsText(context));
+    setOkListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        try {
+          context.startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("market://details?id=" + context.getPackageName())));
+        } catch (android.content.ActivityNotFoundException anfe) {
+          context.startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://play.google.com/store/apps/details?id=" + context.getPackageName())));
+        }
+      }
+    });
+  }
 
-    Resources res = context.getResources();
-    this.setText(res.getString(R.string.reminder_header_outdated_build));
+  private static CharSequence getPluralsText(final Context context) {
     int days = Util.getDaysTillBuildExpiry();
-    this.setTitle(res.getQuantityString(R.plurals.reminder_header_outdated_build_details, days, days));
+    return context.getResources().getQuantityString(R.plurals.reminder_header_outdated_build_details, days, days);
+  }
+
+  @Override
+  public boolean isDismissable() {
+    return false;
   }
 
   public static boolean isEligible() {
