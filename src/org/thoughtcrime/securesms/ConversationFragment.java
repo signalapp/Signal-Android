a8f4dadaf5f23642cba89c3f15e42a462bbd2cda
Geonu Kang <blamethegnu@unrulygnu.com>
2015-11-24 15:55:32 +0900

    Delete Selected Message button strings "No"/"Yes" -> "Cancel"/"Delete"
    Closes #4705
    Fixes #4704
    // FREEBIE

--- src/org/thoughtcrime/securesms/ConversationFragment.java
+++ src/org/thoughtcrime/securesms/ConversationFragment.java
@@ -263,7 +263,7 @@ public class ConversationFragment extends Fragment
     builder.setMessage(getActivity().getResources().getQuantityString(R.plurals.ConversationFragment_this_will_permanently_delete_all_n_selected_messages, messagesCount, messagesCount));
     builder.setCancelable(true);
 
-    builder.setPositiveButton(R.string.yes, new DialogInterface.OnClickListener() {
+    builder.setPositiveButton(R.string.delete, new DialogInterface.OnClickListener() {
       @Override
       public void onClick(DialogInterface dialog, int which) {
         new ProgressDialogAsyncTask<MessageRecord, Void, Void>(getActivity(),
@@ -293,7 +293,7 @@ public class ConversationFragment extends Fragment
       }
     });
 
-    builder.setNegativeButton(R.string.no, null);
+    builder.setNegativeButton(android.R.string.cancel, null);
     builder.show();
   }
 
