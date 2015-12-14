ee9b89c4d459f20817b7b52c9d81e67ea148a0cf
David Novák <kaladivo@gmail.com>
2015-12-14 16:26:56 +0100

    Make the 'try again' device management option actually try again.
    Fixes #4648
    Closes #4882

--- src/org/thoughtcrime/securesms/DeviceListFragment.java
+++ src/org/thoughtcrime/securesms/DeviceListFragment.java
@@ -142,6 +142,7 @@ public class DeviceListFragment extends ListFragment
                               new DialogInterface.OnClickListener() {
       @Override
       public void onClick(DialogInterface dialog, int which) {
+        getLoaderManager().getLoader(0).forceLoad();
         getLoaderManager().initLoader(0, null, DeviceListFragment.this);
       }
     });
