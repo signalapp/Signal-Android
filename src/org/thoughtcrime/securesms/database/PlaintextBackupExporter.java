e818bef20b0372f59ed7da736b8c4200eda1cd28
AsamK <asamk@gmx.de>
2015-12-18 16:57:02 +0100

    Export the text of MMS messages in plaintext backup
    Also fixes exporting outgoing group messages. Those are added to the mms
    database even if they're only text.

--- src/org/thoughtcrime/securesms/database/PlaintextBackupExporter.java
+++ src/org/thoughtcrime/securesms/database/PlaintextBackupExporter.java
@@ -5,6 +5,8 @@ import android.content.Context;
 import android.os.Environment;
 
 import org.thoughtcrime.securesms.crypto.MasterSecret;
+import org.thoughtcrime.securesms.database.model.MediaMmsMessageRecord;
+import org.thoughtcrime.securesms.database.model.MessageRecord;
 import org.thoughtcrime.securesms.database.model.SmsMessageRecord;
 import org.thoughtcrime.securesms.recipients.Recipients;
 
@@ -41,7 +43,7 @@ public class PlaintextBackupExporter {
     SmsMessageRecord record;
     EncryptingSmsDatabase.Reader reader = null;
     int skip                            = 0;
-    int ROW_LIMIT                       = 500;
+    final int ROW_LIMIT                 = 500;
 
     do {
       if (reader != null)
@@ -67,6 +69,34 @@ public class PlaintextBackupExporter {
 
       skip += ROW_LIMIT;
     } while (reader.getCount() > 0);
+
+    skip = 0;
+    MmsDatabase.Reader mmsReader = null;
+    do {
+      if (mmsReader != null)
+        mmsReader.close();
+
+      mmsReader = DatabaseFactory.getMmsDatabase(context).getMessages(masterSecret, skip, ROW_LIMIT);
+
+      MessageRecord mmsRecord;
+      while ((mmsRecord = mmsReader.getNext()) != null) {
+        String threadAddress = null;
+        Recipients threadRecipients = threads.getRecipientsForThreadId(mmsRecord.getThreadId());
+        if (threadRecipients != null && !threadRecipients.isEmpty()) {
+          threadAddress = threadRecipients.getPrimaryRecipient().getNumber();
+        }
+        XmlBackup.XmlBackupItem item =
+                new XmlBackup.XmlBackupItem(1, mmsRecord.getIndividualRecipient().getNumber(),
+                        threadAddress, mmsRecord.getDateReceived(),
+                        MmsSmsColumns.Types.translateToSystemBaseType(mmsRecord.getType()),
+                        null, mmsRecord.getDisplayBody().toString(), null,
+                        1, mmsRecord.getDeliveryStatus());
+
+        writer.writeItem(item);
+      }
+
+      skip += ROW_LIMIT;
+    } while (mmsReader.getCount() > 0);
 
     writer.close();
   }
