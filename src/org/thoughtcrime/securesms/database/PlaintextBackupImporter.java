b3b7438c3ae817ade5c98494351839008938cc2d
Alex Peterka <mail@alexp.cz>
2015-12-20 21:20:22 +0100

    Fix for #4849 (more stylistic adjustments)

--- src/org/thoughtcrime/securesms/database/PlaintextBackupImporter.java
+++ src/org/thoughtcrime/securesms/database/PlaintextBackupImporter.java
@@ -37,12 +37,12 @@ public class PlaintextBackupImporter {
 
   private static String getPlaintextExportDirectoryPath() {
     File sdDirectory = Environment.getExternalStorageDirectory();
+    File signalBackup = new File(sdDirectory.getAbsolutePath(), "SignalPlaintextBackup.xml");
 
-    File signalBackup = new File(sdDirectory.getAbsolutePath() + File.separator, "SignalPlaintextBackup.xml");
-    if(signalBackup.exists()) {
-        return sdDirectory.getAbsolutePath() + File.separator + "SignalPlaintextBackup.xml";
+    if (signalBackup.exists()) {
+      return sdDirectory.getAbsolutePath() + File.separator + "SignalPlaintextBackup.xml";
     } else {
-        return sdDirectory.getAbsolutePath() + File.separator + "TextSecurePlaintextBackup.xml";
+      return sdDirectory.getAbsolutePath() + File.separator + "TextSecurePlaintextBackup.xml";
     }
   }
 
