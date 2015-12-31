20044392ce836b8a69cbbfddac46eb5d5becd037
Christian Ascheberg <c.ascheberg@gmx.de>
2015-12-31 17:09:33 +0100

    play sound while connecting

--- src/org/thoughtcrime/redphone/RedPhoneService.java
+++ src/org/thoughtcrime/redphone/RedPhoneService.java
@@ -443,6 +443,7 @@ public class RedPhoneService extends Service implements CallStateListener, CallS
   }
 
   public void notifyConnectingtoInitiator() {
+    outgoingRinger.playHandshake();
     sendMessage(Type.CONNECTING_TO_INITIATOR, getRecipient(), null);
   }
 
