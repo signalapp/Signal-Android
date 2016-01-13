81e5310953928f304fe7336777a1afa0b7110307
Geonu Kang <blamethegnu@unrulygnu.com>
2016-01-13 16:36:52 +0900

    Use file extension in Persistent Blob URI to store/query MIME type
    See https://github.com/WhisperSystems/Signal-Android/pull/4689#issuecomment-167116574
    Fixes #4536

--- src/org/thoughtcrime/securesms/providers/PersistentBlobProvider.java
+++ src/org/thoughtcrime/securesms/providers/PersistentBlobProvider.java
@@ -1,7 +1,6 @@
 package org.thoughtcrime.securesms.providers;
 
 import android.annotation.SuppressLint;
-import android.content.ContentUris;
 import android.content.Context;
 import android.content.UriMatcher;
 import android.net.Uri;
@@ -33,8 +32,7 @@ public class PersistentBlobProvider {
   private static final String     URI_STRING            = "content://org.thoughtcrime.securesms/capture";
   public  static final Uri        CONTENT_URI           = Uri.parse(URI_STRING);
   public  static final String     AUTHORITY             = "org.thoughtcrime.securesms";
-  public  static final String     EXPECTED_PATH         = "capture/*/*/#";
-  private static final int        MIMETYPE_PATH_SEGMENT = 1;
+  public  static final String     EXPECTED_PATH         = "capture/*/*";
   private static final String     BLOB_DIRECTORY        = "captures";
   private static final String     BLOB_EXTENSION        = "blob";
   private static final int        MATCH                 = 1;
@@ -82,11 +80,10 @@ public class PersistentBlobProvider {
 
   private Uri create(MasterSecret masterSecret, InputStream input, long id, String mimeType) {
     persistToDisk(masterSecret, id, input);
-    final Uri uniqueUri = CONTENT_URI.buildUpon()
-                                     .appendPath(mimeType)
-                                     .appendEncodedPath(String.valueOf(System.currentTimeMillis()))
-                                     .build();
-    return ContentUris.withAppendedId(uniqueUri, id);
+    return CONTENT_URI.buildUpon()
+                      .appendEncodedPath(String.valueOf(System.currentTimeMillis()))
+                      .appendEncodedPath(id + "." + getExtensionFromMimeType(mimeType))
+                      .build();
   }
 
   private void persistToDisk(final MasterSecret masterSecret, final long id, final InputStream input) {
@@ -115,18 +112,30 @@ public class PersistentBlobProvider {
   public boolean delete(@NonNull Uri uri) {
     switch (MATCHER.match(uri)) {
     case MATCH:
-      long id = ContentUris.parseId(uri);
+      long id = getId(uri);
       cache.remove(id);
-      return getFile(ContentUris.parseId(uri)).delete();
+      return getFile(id).delete();
     default:
       return new File(uri.getPath()).delete();
     }
   }
 
-  public @NonNull InputStream getStream(MasterSecret masterSecret, long id) throws IOException {
+  public @Nullable InputStream getStream(MasterSecret masterSecret, Uri persistentBlobUri) throws IOException {
+    return MATCHER.match(persistentBlobUri) == MATCH ? getStream(masterSecret, getId(persistentBlobUri))
+                                                     : null;
+  }
+
+  private @NonNull InputStream getStream(MasterSecret masterSecret, long id) throws IOException {
     final byte[] cached = cache.get(id);
     return cached != null ? new ByteArrayInputStream(cached)
                           : new DecryptingPartInputStream(getFile(id), masterSecret);
+  }
+
+  private static long getId(Uri persistentBlobUri) {
+    final String filename = persistentBlobUri.getLastPathSegment();
+    int index = filename.lastIndexOf(".");
+    return index == -1 ? Long.parseLong(filename)
+                       : Long.parseLong(filename.substring(0, index));
   }
 
   private File getFile(long id) {
@@ -135,7 +144,8 @@ public class PersistentBlobProvider {
 
   public static @Nullable String getMimeType(@NonNull Context context, @NonNull Uri persistentBlobUri) {
     if (!isAuthority(context, persistentBlobUri)) return null;
-    return persistentBlobUri.getPathSegments().get(MIMETYPE_PATH_SEGMENT);
+    return MimeTypeMap.getSingleton()
+                      .getMimeTypeFromExtension(MimeTypeMap.getFileExtensionFromUrl(persistentBlobUri.toString()));
   }
 
   private static @NonNull String getExtensionFromMimeType(String mimeType) {
