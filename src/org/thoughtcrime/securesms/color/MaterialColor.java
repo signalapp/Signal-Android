db5ac83355f114d428aa32f8386c71da03127058
agrajaghh <agrajaghh@gmail.com>
2016-02-16 19:37:11 +0100

    swap statusbar color in dark theme
    fixes #5232
    
    // FREEBIE

--- src/org/thoughtcrime/securesms/color/MaterialColor.java
+++ src/org/thoughtcrime/securesms/color/MaterialColor.java
@@ -54,8 +54,8 @@ public enum MaterialColor {
     this.serialized             = serialized;
   }
 
-  MaterialColor(int lightColor, int darkColor, int statusBarColor, String serialized) {
-    this(lightColor, lightColor, statusBarColor, darkColor, darkColor, statusBarColor, serialized);
+  MaterialColor(int color500, int color900, int color700, String serialized) {
+    this(color500, color500, color700, color700, color700, color900, serialized);
   }
 
   public int toConversationColor(@NonNull Context context) {
