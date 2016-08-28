63f7faf5bbe95cdbac80af3a0f307aa2d6904d27
FeuRenard <feurenard@posteo.de>
2016-08-28 22:42:50 +0200

    Clear invite reminder if recipient isn't a contact
    Fixes #5637
    Closes #5640
    // FREEBIE

--- src/org/thoughtcrime/securesms/ConversationActivity.java
+++ src/org/thoughtcrime/securesms/ConversationActivity.java
@@ -884,11 +884,7 @@ public class ConversationActivity extends PassphraseRequiredActionBarActivity
   }
 
   private void updateRecipientPreferences() {
-    if (recipients.getPrimaryRecipient() != null &&
-        recipients.getPrimaryRecipient().getContactUri() != null)
-    {
-      new RecipientPreferencesTask().execute(recipients);
-    }
+    new RecipientPreferencesTask().execute(recipients);
   }
 
   protected void updateInviteReminder(boolean seenInvite) {
@@ -896,7 +892,9 @@ public class ConversationActivity extends PassphraseRequiredActionBarActivity
     if (TextSecurePreferences.isPushRegistered(this) &&
         !isSecureText                                &&
         !seenInvite                                  &&
-        recipients.isSingleRecipient())
+        recipients.isSingleRecipient()               &&
+        recipients.getPrimaryRecipient() != null     &&
+        recipients.getPrimaryRecipient().getContactUri() != null)
     {
       InviteReminder reminder = new InviteReminder(this, recipients);
       reminder.setOkListener(new OnClickListener() {
