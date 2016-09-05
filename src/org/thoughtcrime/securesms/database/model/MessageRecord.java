a203f934ac07dadb2a5da0eeaa963f047eb7bf06
FeuRenard <feurenard@posteo.de>
2016-09-05 21:02:24 +0200

    Extract method

--- src/org/thoughtcrime/securesms/database/model/MessageRecord.java
+++ src/org/thoughtcrime/securesms/database/model/MessageRecord.java
@@ -109,15 +109,7 @@ public abstract class MessageRecord extends DisplayRecord {
     } else if (isJoined()) {
       return emphasisAdded(context.getString(R.string.MessageRecord_s_is_on_signal_say_hey, getIndividualRecipient().toShortString()));
     } else if (isExpirationTimerUpdate()) {
-      String time   = ExpirationUtil.getExpirationDisplayValue(context, (int)(getExpiresIn() / 1000));
-      String sender,senderAndText;
-      if (isOutgoing()) {
-        senderAndText = context.getString(R.string.MessageRecord_you_set_disappearing_message_time_to_s, time);
-      } else {
-        sender = getIndividualRecipient().toShortString();
-        senderAndText = context.getString(R.string.MessageRecord_s_set_disappearing_message_time_to_s, sender, time);
-      }
-      return emphasisAdded(senderAndText);
+      return emphasisAdded(getExpirationTimerUpdateDisplayBody());
     } else if (isIdentityUpdate()) {
       return emphasisAdded(context.getString(R.string.MessageRecord_your_safety_numbers_with_s_have_changed, getIndividualRecipient().toShortString()));
     } else if (getBody().getBody().length() > MAX_DISPLAY_LENGTH) {
@@ -125,6 +117,18 @@ public abstract class MessageRecord extends DisplayRecord {
     }
 
     return new SpannableString(getBody().getBody());
+  }
+
+  private String getExpirationTimerUpdateDisplayBody() {
+    String time   = ExpirationUtil.getExpirationDisplayValue(context, (int)(getExpiresIn() / 1000));
+    String senderAndText;
+    if (isOutgoing()) {
+      senderAndText = context.getString(R.string.MessageRecord_you_set_disappearing_message_time_to_s, time);
+    } else {
+      String sender = getIndividualRecipient().toShortString();
+      senderAndText = context.getString(R.string.MessageRecord_s_set_disappearing_message_time_to_s, sender, time);
+    }
+    return senderAndText;
   }
 
   public long getId() {
