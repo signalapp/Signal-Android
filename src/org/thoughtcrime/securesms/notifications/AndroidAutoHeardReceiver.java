9148b7da5f4604dae1106d3932bfa7d3bfa63ad4
Mattias Eriksson <snaggen@mayam.com>
2016-09-14 23:58:24 +0200

    Initial Android Auto support
    This adds android auto support accordign to
    https://developer.android.com/training/auto/messaging/index.html#messaging
    However, since android auto is not officially supported in my country,
    the functionality is limited. Which means that I have not been able
    to fully test everything yet.
    
    What work is:
    * Message notification is shown.
    * When you click on it, the message is read.
    
    Closes #5880

---
+++ src/org/thoughtcrime/securesms/notifications/AndroidAutoHeardReceiver.java
@@ -0,0 +1,79 @@
+/**
+ * Copyright (C) 2011 Whisper Systems
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.thoughtcrime.securesms.notifications;
+
+import android.app.NotificationManager;
+import android.content.Context;
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.support.annotation.Nullable;
+
+import org.thoughtcrime.securesms.ApplicationContext;
+import org.thoughtcrime.securesms.crypto.MasterSecret;
+import org.thoughtcrime.securesms.database.DatabaseFactory;
+import org.thoughtcrime.securesms.database.MessagingDatabase.MarkedMessageInfo;
+import org.thoughtcrime.securesms.database.MessagingDatabase.SyncMessageId;
+import org.thoughtcrime.securesms.jobs.MultiDeviceReadUpdateJob;
+import org.whispersystems.libsignal.logging.Log;
+
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * Marks an Android Auto as read after the driver have listened to it
+ */
+public class AndroidAutoHeardReceiver extends MasterSecretBroadcastReceiver {
+
+  public static final String TAG              = AndroidAutoHeardReceiver.class.getSimpleName();
+  public static final String HEARD_ACTION     = "org.thoughtcrime.securesms.notifications.ANDROID_AUTO_HEARD";
+  public static final String THREAD_IDS_EXTRA = "car_heard_thread_ids";
+
+  @Override
+  protected void onReceive(final Context context, Intent intent,
+                           @Nullable final MasterSecret masterSecret)
+  {
+    if (!HEARD_ACTION.equals(intent.getAction()))
+      return;
+
+    final long[] threadIds = intent.getLongArrayExtra(THREAD_IDS_EXTRA);
+
+    if (threadIds != null) {
+      ((NotificationManager)context.getSystemService(Context.NOTIFICATION_SERVICE))
+              .cancel(MessageNotifier.NOTIFICATION_ID);
+
+      new AsyncTask<Void, Void, Void>() {
+        @Override
+        protected Void doInBackground(Void... params) {
+          List<MarkedMessageInfo> messageIdsCollection = new LinkedList<>();
+
+          for (long threadId : threadIds) {
+            Log.i(TAG, "Marking meassage as read: " + threadId);
+            List<MarkedMessageInfo> messageIds = DatabaseFactory.getThreadDatabase(context).setRead(threadId);
+
+            messageIdsCollection.addAll(messageIds);
+          }
+
+          MessageNotifier.updateNotification(context, masterSecret);
+          MarkReadReceiver.process(context, messageIdsCollection);
+
+          return null;
+        }
+      }.execute();
+    }
+  }
+}
--- src/org/thoughtcrime/securesms/notifications/MessageNotifier.java
+++ src/org/thoughtcrime/securesms/notifications/MessageNotifier.java
@@ -258,6 +258,8 @@ public class MessageNotifier {
                        notificationState.getMarkAsReadIntent(context),
                        notificationState.getQuickReplyIntent(context, notifications.get(0).getRecipients()),
                        notificationState.getWearableReplyIntent(context, notifications.get(0).getRecipients()));
+    builder.addAndroidAutoAction(notificationState.getAndroidAutoReplyIntent(context, notifications.get(0).getRecipients()),
+                                 notificationState.getAndroidAutoHeardIntent(context, notifications.get(0).getRecipients()), notifications.get(0).getTimestamp());
 
     ListIterator<NotificationItem> iterator = notifications.listIterator(notifications.size());
 
