92aeafc26172fbbce036cdf99b82f8313acb204b
FeuRenard <feurenard@posteo.de>
2016-11-12 12:59:54 +0100

    Add share button for safety numbers
    Fixes #5798
    Closes #5854
    // FREEBIE

--- src/org/thoughtcrime/securesms/VerifyIdentityActivity.java
+++ src/org/thoughtcrime/securesms/VerifyIdentityActivity.java
@@ -16,7 +16,9 @@
  */
 package org.thoughtcrime.securesms;
 
+import android.content.ActivityNotFoundException;
 import android.content.Context;
+import android.content.Intent;
 import android.content.res.Configuration;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
@@ -33,6 +35,8 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
@@ -123,9 +127,21 @@ public class VerifyIdentityActivity extends PassphraseRequiredActionBarActivity 
   }
 
   @Override
+  public boolean onPrepareOptionsMenu(Menu menu) {
+    super.onPrepareOptionsMenu(menu);
+
+    menu.clear();
+    MenuInflater inflater = this.getMenuInflater();
+    inflater.inflate(R.menu.verify_identity, menu);
+
+    return true;
+  }
+
+  @Override
   public boolean onOptionsItemSelected(MenuItem item) {
     switch (item.getItemId()) {
-      case android.R.id.home: finish(); return true;
+      case R.id.verify_identity__share: handleShare();  return true;
+      case android.R.id.home:           finish();       return true;
     }
 
     return false;
@@ -172,6 +188,25 @@ public class VerifyIdentityActivity extends PassphraseRequiredActionBarActivity 
       getWindow().setStatusBarColor(color.toStatusBarColor(this));
     }
   }
+
+  private void handleShare() {
+    String shareString =
+        getString(R.string.VerifyIdentityActivity_our_signal_safety_numbers) + "\n" +
+        displayFragment.getFormattedSafetyNumbers() + "\n";
+
+    Intent intent = new Intent();
+    intent.setAction(Intent.ACTION_SEND);
+    intent.putExtra(Intent.EXTRA_SUBJECT, getString(R.string.VerifyIdentityActivity_our_signal_safety_numbers));
+    intent.putExtra(Intent.EXTRA_TEXT, shareString);
+    intent.setType("text/plain");
+
+    try {
+      startActivity(Intent.createChooser(intent, getString(R.string.VerifyIdentityActivity_share_safety_numbers_via)));
+    } catch (ActivityNotFoundException e) {
+      Toast.makeText(VerifyIdentityActivity.this, R.string.VerifyIdentityActivity_no_app_to_share_to, Toast.LENGTH_LONG).show();
+    }
+  }
+
 
   public static class VerifyDisplayFragment extends Fragment implements Recipients.RecipientsModifiedListener {
 
@@ -292,6 +327,21 @@ public class VerifyIdentityActivity extends PassphraseRequiredActionBarActivity 
 
     public void setClickListener(View.OnClickListener listener) {
       this.clickListener = listener;
+    }
+
+    public String getFormattedSafetyNumbers() {
+      StringBuilder result = new StringBuilder();
+
+      for (int i = 0; i < codes.length; i++) {
+        result.append(codes[i].getText());
+
+        if (i != codes.length - 1) {
+          if (((i+1) % 4) == 0) result.append('\n');
+          else                  result.append(' ');
+        }
+      }
+
+      return result.toString();
     }
 
     private void setFingerprintViews(Fingerprint fingerprint) {
