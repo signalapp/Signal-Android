4ad989cc38f6cbe2963524ae3ace455c2ee951b4
Moxie Marlinspike <moxie@thoughtcrime.org>
2017-01-08 13:06:11 -0800

    Fix rounded corners bitmap double free
    Fixes #4732
    // FREEBIE

--- src/org/thoughtcrime/securesms/mms/RoundedCorners.java
+++ src/org/thoughtcrime/securesms/mms/RoundedCorners.java
@@ -38,19 +38,24 @@ public class RoundedCorners extends BitmapTransformation {
   {
     final Bitmap toRound = crop ? centerCrop(pool, toTransform, outWidth, outHeight)
                                 : fitCenter(pool, toTransform, outWidth, outHeight);
+
     final Bitmap rounded = round(pool, toRound);
-    if (toRound != null && toRound != rounded && !pool.put(toRound)) {
+
+    if (toRound != null && toRound != rounded && toRound != toTransform && !pool.put(toRound)) {
       toRound.recycle();
     }
+
     return rounded;
   }
 
   private Bitmap centerCrop(BitmapPool pool, Bitmap toTransform, int outWidth, int outHeight) {
     final Bitmap toReuse     = pool.get(outWidth, outHeight, getSafeConfig(toTransform));
     final Bitmap transformed = TransformationUtils.centerCrop(toReuse, toTransform, outWidth, outHeight);
+
     if (toReuse != null && toReuse != transformed && !pool.put(toReuse)) {
       toReuse.recycle();
     }
+
     return transformed;
   }
 
@@ -63,28 +68,29 @@ public class RoundedCorners extends BitmapTransformation {
       return null;
     }
 
-    final Bitmap result;
-    final Bitmap toReuse = pool.get(toRound.getWidth(), toRound.getHeight(), getSafeConfig(toRound));
-    if (toReuse != null) {
-      result = toReuse;
-    } else {
+    Bitmap result = pool.get(toRound.getWidth(), toRound.getHeight(), getSafeConfig(toRound));
+
+    if (result == null) {
       result = Bitmap.createBitmap(toRound.getWidth(), toRound.getHeight(), getSafeConfig(toRound));
     }
 
-    final Canvas canvas      = new Canvas(result);
-    final Paint  cornerPaint = new Paint();
-    final Paint  shaderPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+    Canvas canvas = new Canvas(result);
 
-    shaderPaint.setShader(new BitmapShader(toRound, TileMode.CLAMP, TileMode.CLAMP));
-    cornerPaint.setColor(colorHint);
     if (Config.RGB_565.equals(result.getConfig())) {
+      Paint  cornerPaint = new Paint();
+      cornerPaint.setColor(colorHint);
+
       canvas.drawRect(0, 0, radius, radius, cornerPaint);
       canvas.drawRect(0, toRound.getHeight() - radius, radius, toRound.getHeight(), cornerPaint);
       canvas.drawRect(toRound.getWidth() - radius, 0, toRound.getWidth(), radius, cornerPaint);
       canvas.drawRect(toRound.getWidth() - radius, toRound.getHeight() - radius, toRound.getWidth(), toRound.getHeight(), cornerPaint);
     }
+
+    Paint shaderPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+    shaderPaint.setShader(new BitmapShader(toRound, TileMode.CLAMP, TileMode.CLAMP));
+
     canvas.drawRoundRect(new RectF(0, 0, toRound.getWidth(), toRound.getHeight()), radius, radius, shaderPaint);
-//    Log.w("RoundedCorners", "in was " + toRound.getWidth() + "x" + toRound.getHeight() + ", out to " + result.getWidth() + "x" + result.getHeight());
+
     return result;
   }
 
@@ -92,7 +98,8 @@ public class RoundedCorners extends BitmapTransformation {
     return bitmap.getConfig() != null ? bitmap.getConfig() : Bitmap.Config.ARGB_8888;
   }
 
-  @Override public String getId() {
+  @Override
+  public String getId() {
     return RoundedCorners.class.getCanonicalName();
   }
 }
