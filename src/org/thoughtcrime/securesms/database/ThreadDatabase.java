369bfd94599a5a6122d2678c2b8423064ff86789
Dominik Dafert <dafnikdev@gmail.com>
2017-01-19 21:34:19 +0100

    Remove conflict

--- src/org/thoughtcrime/securesms/database/ThreadDatabase.java
+++ src/org/thoughtcrime/securesms/database/ThreadDatabase.java
@@ -349,6 +349,11 @@ public class ThreadDatabase extends Database {
     return cursor;
   }
 
+  public Cursor getDirectShareList() {
+    SQLiteDatabase db = databaseHelper.getReadableDatabase();
+    return db.query(TABLE_NAME, null, null, null, null, null, DATE + " DESC");
+  }
+
   public Cursor getShortcutList() {
     SQLiteDatabase db = databaseHelper.getReadableDatabase();
     return db.query(TABLE_NAME, null, null, null, null, null, DATE + " DESC");
