ddf6126abf05548b3bb777e135361c2a7e4fa0f2
Moxie Marlinspike <moxie@thoughtcrime.org>
2017-04-16 11:11:08 -0700

    Fix VideoPlayer Build.VERSION minimum
    // FREEBIE

--- src/org/thoughtcrime/securesms/video/VideoPlayer.java
+++ src/org/thoughtcrime/securesms/video/VideoPlayer.java
@@ -75,7 +75,7 @@ public class VideoPlayer extends FrameLayout {
   public void setVideoSource(@NonNull MasterSecret masterSecret, @NonNull VideoSlide videoSource)
       throws IOException
   {
-    if (Build.VERSION.SDK_INT >= 14) setExoViewSource(masterSecret, videoSource);
+    if (Build.VERSION.SDK_INT >= 16) setExoViewSource(masterSecret, videoSource);
     else                             setVideoViewSource(masterSecret, videoSource);
   }
 
