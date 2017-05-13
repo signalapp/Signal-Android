865678c4f99aa6466fbe9fd5375aed99fbbb4a36
Nicholas Rizzio <nrizzio@users.noreply.github.com>
2017-05-13 18:48:56 -0400

    Set the advanced menu icon correctly
    Fixes #6638
    // FREEBIE

--- src/org/thoughtcrime/securesms/ApplicationPreferencesActivity.java
+++ src/org/thoughtcrime/securesms/ApplicationPreferencesActivity.java
@@ -215,7 +215,7 @@ public class ApplicationPreferencesActivity extends PassphraseRequiredActionBarA
       this.findPreference(PREFERENCE_CATEGORY_APPEARANCE).setIcon(appearance);
       this.findPreference(PREFERENCE_CATEGORY_CHATS).setIcon(chats);
       this.findPreference(PREFERENCE_CATEGORY_DEVICES).setIcon(devices);
-      this.findPreference(PREFERENCE_CATEGORY_ADVANCED).setIcon(appearance);
+      this.findPreference(PREFERENCE_CATEGORY_ADVANCED).setIcon(advanced);
     }
 
     private class CategoryClickListener implements Preference.OnPreferenceClickListener {
