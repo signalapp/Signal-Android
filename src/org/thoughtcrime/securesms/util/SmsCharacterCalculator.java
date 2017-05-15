66062f200a1c3e7fd52a60d9b2a5915a683e3cdd
Markus Brenneis <support.gulp21@googlemail.com>
2017-05-15 21:13:27 +0200

    Work around SmsMessage.calculateLength NPE on broken ROMs
    //FREEBIE

--- src/org/thoughtcrime/securesms/util/SmsCharacterCalculator.java
+++ src/org/thoughtcrime/securesms/util/SmsCharacterCalculator.java
@@ -17,16 +17,30 @@
 package org.thoughtcrime.securesms.util;
 
 import android.telephony.SmsMessage;
+import android.util.Log;
 
 public class SmsCharacterCalculator extends CharacterCalculator {
 
+  private static final String TAG = SmsCharacterCalculator.class.getSimpleName();
+
   @Override
   public CharacterState calculateCharacters(String messageBody) {
+    int[] length;
+    int   messagesSpent;
+    int   charactersSpent;
+    int   charactersRemaining;
 
-    int[] length            = SmsMessage.calculateLength(messageBody, false);
-    int messagesSpent       = length[0];
-    int charactersSpent     = length[1];
-    int charactersRemaining = length[2];
+    try {
+      length              = SmsMessage.calculateLength(messageBody, false);
+      messagesSpent       = length[0];
+      charactersSpent     = length[1];
+      charactersRemaining = length[2];
+    } catch (NullPointerException e) {
+      Log.w(TAG, e);
+      messagesSpent       = 1;
+      charactersSpent     = messageBody.length();
+      charactersRemaining = 1000;
+    }
 
     int maxMessageSize;
 
