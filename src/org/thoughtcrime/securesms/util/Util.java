0b39224d88e0ce3a063b8b642509e664df255791
Moxie Marlinspike <moxie@thoughtcrime.org>
2017-07-21 16:01:32 -0700

    getMemoryClass -> getLargeMemoryClass
    Fixes #6812
    
    // FREEBIe

--- src/org/thoughtcrime/securesms/util/Util.java
+++ src/org/thoughtcrime/securesms/util/Util.java
@@ -428,7 +428,7 @@ public class Util {
     ActivityManager activityManager = (ActivityManager) context.getSystemService(Context.ACTIVITY_SERVICE);
 
     return (VERSION.SDK_INT >= VERSION_CODES.KITKAT && activityManager.isLowRamDevice()) ||
-           activityManager.getMemoryClass() <= 64;
+           activityManager.getLargeMemoryClass() <= 64;
   }
 
   public static int clamp(int value, int min, int max) {
