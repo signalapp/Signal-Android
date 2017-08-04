8de926108170c2a0cd6cf098dd6b7d79191b437a
Moxie Marlinspike <moxie@thoughtcrime.org>
2017-08-04 09:22:58 -0700

    MMS addresses can be email addresses
    Fixes #6853
    // FREEBIE

--- src/org/thoughtcrime/securesms/database/MmsDatabase.java
+++ src/org/thoughtcrime/securesms/database/MmsDatabase.java
@@ -280,7 +280,7 @@ public class MmsDatabase extends MessagingDatabase {
       PhoneNumberUtil.MatchType match;
 
       if (localNumber == null) match = PhoneNumberUtil.MatchType.NO_MATCH;
-      else                     match = PhoneNumberUtil.getInstance().isNumberMatch(localNumber, cc.toPhoneString());
+      else                     match = PhoneNumberUtil.getInstance().isNumberMatch(localNumber, cc.serialize());
 
       if (match == PhoneNumberUtil.MatchType.NO_MATCH ||
           match == PhoneNumberUtil.MatchType.NOT_A_NUMBER)
@@ -295,7 +295,7 @@ public class MmsDatabase extends MessagingDatabase {
         PhoneNumberUtil.MatchType match;
 
         if (localNumber == null) match = PhoneNumberUtil.MatchType.NO_MATCH;
-        else                     match = PhoneNumberUtil.getInstance().isNumberMatch(localNumber, to.toPhoneString());
+        else                     match = PhoneNumberUtil.getInstance().isNumberMatch(localNumber, to.serialize());
 
         if (match == PhoneNumberUtil.MatchType.NO_MATCH ||
             match == PhoneNumberUtil.MatchType.NOT_A_NUMBER)
