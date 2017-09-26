23a23ebd72e4f338c91d52d9137f88d7805577e3
Greg Cooksey <cooksey@cs.wisc.edu>
2017-09-26 17:31:28 -0500

    CreateProfileActivity uses DynamicTheme
    Fixes #6981
    Closes #6997
    // FREEBIE

--- src/org/thoughtcrime/securesms/CreateProfileActivity.java
+++ src/org/thoughtcrime/securesms/CreateProfileActivity.java
@@ -43,6 +43,8 @@ import org.thoughtcrime.securesms.profiles.ProfileMediaConstraints;
 import org.thoughtcrime.securesms.profiles.SystemProfileUtil;
 import org.thoughtcrime.securesms.util.BitmapDecodingException;
 import org.thoughtcrime.securesms.util.BitmapUtil;
+import org.thoughtcrime.securesms.util.DynamicLanguage;
+import org.thoughtcrime.securesms.util.DynamicTheme;
 import org.thoughtcrime.securesms.util.IntentUtils;
 import org.thoughtcrime.securesms.util.TextSecurePreferences;
 import org.thoughtcrime.securesms.util.Util;
@@ -73,6 +75,9 @@ public class CreateProfileActivity extends BaseActionBarActivity implements Inje
 
   private static final int REQUEST_CODE_AVATAR = 1;
 
+  private final DynamicTheme    dynamicTheme    = new DynamicTheme();
+  private final DynamicLanguage dynamicLanguage = new DynamicLanguage();
+
   @Inject SignalServiceAccountManager accountManager;
 
   private InputAwareLayout       container;
@@ -91,6 +96,9 @@ public class CreateProfileActivity extends BaseActionBarActivity implements Inje
   public void onCreate(Bundle bundle) {
     super.onCreate(bundle);
 
+    dynamicTheme.onCreate(this);
+    dynamicLanguage.onCreate(this);
+
     setContentView(R.layout.profile_create_activity);
 
     getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_RESIZE);
@@ -102,6 +110,13 @@ public class CreateProfileActivity extends BaseActionBarActivity implements Inje
     initializeProfileAvatar(getIntent().getBooleanExtra(EXCLUDE_SYSTEM, false));
 
     ApplicationContext.getInstance(this).injectDependencies(this);
+  }
+
+  @Override
+  public void onResume() {
+    super.onResume();
+    dynamicTheme.onResume(this);
+    dynamicLanguage.onResume(this);
   }
 
   @Override
