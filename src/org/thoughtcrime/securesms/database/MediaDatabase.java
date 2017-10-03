b7340cc356f8d9e9758ea4d3a8274e040486a187
Moxie Marlinspike <moxie@thoughtcrime.org>
2017-10-03 16:41:20 -0700

    Don't show voice notes in conversation documents overview
    Fixes #7029
    // FREEBIE

--- src/org/thoughtcrime/securesms/database/MediaDatabase.java
+++ src/org/thoughtcrime/securesms/database/MediaDatabase.java
@@ -42,7 +42,7 @@ public class MediaDatabase extends Database {
         + "ORDER BY " + AttachmentDatabase.TABLE_NAME + "." + AttachmentDatabase.ROW_ID + " DESC";
 
   private static final String GALLERY_MEDIA_QUERY  = String.format(BASE_MEDIA_QUERY, AttachmentDatabase.CONTENT_TYPE + " LIKE 'image/%' OR " + AttachmentDatabase.CONTENT_TYPE + " LIKE 'video/%'");
-  private static final String DOCUMENT_MEDIA_QUERY = String.format(BASE_MEDIA_QUERY, AttachmentDatabase.CONTENT_TYPE + " NOT LIKE 'image/%' AND " + AttachmentDatabase.CONTENT_TYPE + " NOT LIKE 'video/%'");
+  private static final String DOCUMENT_MEDIA_QUERY = String.format(BASE_MEDIA_QUERY, AttachmentDatabase.CONTENT_TYPE + " NOT LIKE 'image/%' AND " + AttachmentDatabase.CONTENT_TYPE + " NOT LIKE 'video/%' AND " + AttachmentDatabase.CONTENT_TYPE + " NOT LIKE 'audio/%'");
 
   public MediaDatabase(Context context, SQLiteOpenHelper databaseHelper) {
     super(context, databaseHelper);
