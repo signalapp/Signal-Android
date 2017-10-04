9dc126178fd66a77fab69488cc8e613b0eee8c18
Christian Ascheberg <c.ascheberg@gmx.de>
2017-10-04 20:50:12 +0200

    Do not show sent checkmark for verification status changes
    Fixes #6743
    
    // FREEBIE

--- src/org/thoughtcrime/securesms/ConversationListItem.java
+++ src/org/thoughtcrime/securesms/ConversationListItem.java
@@ -202,7 +202,7 @@ public class ConversationListItem extends RelativeLayout
   }
 
   private void setStatusIcons(ThreadRecord thread) {
-    if (!thread.isOutgoing() || thread.isOutgoingCall()) {
+    if (!thread.isOutgoing() || thread.isOutgoingCall() || thread.isVerificationStatusChange()) {
       deliveryStatusIndicator.setNone();
       alertView.setNone();
     } else if (thread.isFailed()) {
