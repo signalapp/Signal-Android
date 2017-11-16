de812d3f0f6151b8e1c77268d62a836979d84556
Moxie Marlinspike <moxie@thoughtcrime.org>
2017-11-16 12:28:29 -0800

    Extract string resources

--- src/org/thoughtcrime/securesms/components/reminder/UnauthorizedReminder.java
+++ src/org/thoughtcrime/securesms/components/reminder/UnauthorizedReminder.java
@@ -4,14 +4,15 @@ package org.thoughtcrime.securesms.components.reminder;
 import android.content.Context;
 import android.content.Intent;
 
+import org.thoughtcrime.securesms.R;
 import org.thoughtcrime.securesms.RegistrationActivity;
 import org.thoughtcrime.securesms.util.TextSecurePreferences;
 
 public class UnauthorizedReminder extends Reminder {
 
   public UnauthorizedReminder(final Context context) {
-    super("Device no longer registered",
-          "This is likely because you registered your phone number with Signal on a different device. Tap to re-register.");
+    super(context.getString(R.string.UnauthorizedReminder_device_no_longer_registered),
+          context.getString(R.string.UnauthorizedReminder_this_is_likely_because_you_registered_your_phone_number_with_Signal_on_a_different_device));
 
     setOkListener(v -> context.startActivity(new Intent(context, RegistrationActivity.class)));
   }
