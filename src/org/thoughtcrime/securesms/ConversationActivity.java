f72e35bf4247724c20c97dd304acb3d2271dcf69
Moxie Marlinspike <moxie@thoughtcrime.org>
2017-12-13 10:29:19 -0800

    Fix typo that requests SMS permission for media/group message
    Fixes #7271

--- src/org/thoughtcrime/securesms/ConversationActivity.java
+++ src/org/thoughtcrime/securesms/ConversationActivity.java
@@ -1669,7 +1669,7 @@ public class ConversationActivity extends PassphraseRequiredActionBarActivity
 
     Permissions.with(this)
                .request(Manifest.permission.SEND_SMS)
-               .ifNecessary(isSecureText || forceSms)
+               .ifNecessary(!isSecureText || forceSms)
                .withPermanentDenialDialog(getString(R.string.ConversationActivity_signal_needs_sms_permission_in_order_to_send_an_sms))
                .onAllGranted(() -> {
                  attachmentManager.clear(glideRequests, false);
