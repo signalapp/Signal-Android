ec596a3c829e3ebf12aa7c0b1d0dd5cd16dcd940
veken1199 <super_veko@yahoo.com>
2018-02-10 17:19:40 -0500

    # related to #10 and #12 and #11: Unit tests 1- Testing PinnedMessagesHandler class and its behaviour

---
+++ test/unitTest/java/org/thoughtcrime/securesms/PinnedMessagesMocks.java
@@ -0,0 +1,86 @@
+package org.thoughtcrime.securesms;
+
+import android.content.Context;
+import android.database.Cursor;
+
+import org.junit.runner.RunWith;
+import org.mockito.BDDMockito;
+
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+import org.thoughtcrime.securesms.database.CursorRecyclerViewAdapter;
+import org.thoughtcrime.securesms.database.DatabaseFactory;
+import org.thoughtcrime.securesms.database.MessagingDatabase;
+import org.thoughtcrime.securesms.database.model.MessageRecord;
+import org.thoughtcrime.securesms.database.MmsDatabase;
+import org.thoughtcrime.securesms.database.MmsSmsDatabase;
+import org.thoughtcrime.securesms.database.SmsDatabase;
+
+import static org.powermock.api.mockito.PowerMockito.when;
+
+/**
+ * Created by BABO99 on 2018-02-09.
+ */
+@RunWith(PowerMockRunner.class)
+@PrepareForTest(DatabaseFactory.class)
+public class PinnedMessagesMocks extends BaseUnitTest {
+    protected Context                   context;
+
+    protected DatabaseFactory           databaseFactory;
+
+    protected MessageRecord             messageRecordSms;
+    protected MessageRecord             messageRecordMms;
+    protected MessagingDatabase         messagingDatabase;
+    protected MmsDatabase               mmsDatabase;
+    protected MmsSmsDatabase            mmsSmsDatabase;
+
+    protected SmsDatabase               smsDatabase;
+
+
+    protected void setUpPinMethod() {
+        when(messagingDatabase.pinMessage(1)).thenReturn(true); // message id 1 is pinned
+        when(messagingDatabase.pinMessage(2)).thenReturn(false); // message id 2 is already pinned
+        when(messagingDatabase.pinMessage(3)).thenReturn(false); // message id 3 does not exists
+    }
+
+    protected void setUpUnpinMethod() {
+        when(messagingDatabase.unpinMessage(1)).thenReturn(true);
+        when(messagingDatabase.unpinMessage(2)).thenReturn(false);
+        when(messagingDatabase.unpinMessage(3)).thenReturn(false);
+    }
+
+    protected void setUpMessageRecord() {
+        when(messageRecordSms.isMms()).thenReturn(false);
+        when(messageRecordMms.isMms()).thenReturn(true);
+
+        when(messageRecordSms.getId()).thenReturn(1l);
+        when(messageRecordMms.getId()).thenReturn(1l);
+    }
+
+    protected void setUpStaticMessagingDatabase() throws Exception {
+        PowerMockito.mockStatic(DatabaseFactory.class);
+        BDDMockito.given(DatabaseFactory.getInstance(context)).willReturn(databaseFactory);
+        BDDMockito.given(DatabaseFactory.getSmsDatabase(context)).willReturn(smsDatabase);
+        BDDMockito.given(DatabaseFactory.getMmsDatabase(context)).willReturn(mmsDatabase);
+
+        when( smsDatabase.pinMessage(1)).thenReturn(true);
+        when( smsDatabase.pinMessage(2)).thenReturn(false);
+        when( smsDatabase.pinMessage(3)).thenReturn(false);
+
+        when( mmsDatabase.pinMessage(1)).thenReturn(true);
+        when( mmsDatabase.pinMessage(2)).thenReturn(false);
+        when( mmsDatabase.pinMessage(3)).thenReturn(false);
+
+        when( smsDatabase.unpinMessage(1)).thenReturn(false); // originally not pinned
+        when( smsDatabase.unpinMessage(2)).thenReturn(true);  // pinned
+
+        when( mmsDatabase.unpinMessage(1)).thenReturn(false); // originally not pinned
+        when( mmsDatabase.unpinMessage(2)).thenReturn(true);  // pinned
+    }
+
+    public void setMockGetIdReturnValue(long val) {
+        when(messageRecordSms.getId()).thenReturn(val);
+        when(messageRecordMms.getId()).thenReturn(val);
+    }
+}
--- test/unitTest/java/org/thoughtcrime/securesms/database/PinnedMessagesTest.java
+++ test/unitTest/java/org/thoughtcrime/securesms/database/PinnedMessagesTest.java
@@ -1,23 +1,13 @@
 package org.thoughtcrime.securesms.database;
 
-import android.content.Context;
-import android.database.Cursor;
-import android.database.MatrixCursor;
-
-import com.fasterxml.jackson.databind.node.ObjectNode;
-
 import org.junit.runners.JUnit4;
 import org.junit.runner.RunWith;
 import org.junit.Test;
-
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.verify;
-import static org.powermock.api.mockito.PowerMockito.when;
-
-import org.mockito.Mockito;
-import org.thoughtcrime.securesms.BaseUnitTest;
+import org.thoughtcrime.securesms.PinnedMessagesMocks;
 
 import static junit.framework.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
 
 
 /**
@@ -25,57 +15,40 @@ import static junit.framework.Assert.assertEquals;
  */
 
 @RunWith(JUnit4.class)
-public class PinnedMessagesTest extends BaseUnitTest {
-    private CursorRecyclerViewAdapter adapter;
-    private Context context;
-    private Cursor cursor;
-    private MmsSmsDatabase mmsSmsDb;
-    private DatabaseFactory dbFactory;
-    private MessagingDatabase messagingDb;
+public class PinnedMessagesTest extends PinnedMessagesMocks {
+
 
     @Override
     public void setUp() {
-        messagingDb = mock(MessagingDatabase.class);
-        mmsSmsDb    = mock(MmsSmsDatabase.class);
+        messagingDatabase = mock(MessagingDatabase.class);
+        mmsSmsDatabase = mock(MmsSmsDatabase.class);
 
-        this.setUpPinMethod();
-        this.setUpUnpinMethod();
-    }
-
-    private void setUpPinMethod() {
-        when(messagingDb.pinMessage(1)).thenReturn(true); // message id 1 is pinned
-        when(messagingDb.pinMessage(2)).thenReturn(false); // message id 2 is already pinned
-        when(messagingDb.pinMessage(3)).thenReturn(false); // message id 3 does not exists
-    }
-
-    private void setUpUnpinMethod() {
-        when(messagingDb.unpinMessage(1)).thenReturn(true);
-        when(messagingDb.unpinMessage(2)).thenReturn(false);
-        when(messagingDb.unpinMessage(3)).thenReturn(false);
+        super.setUpPinMethod();
+        super.setUpUnpinMethod();
     }
 
     @Test
     // This test is to check the behaviour of the method
     public void testPinMessageMethod() {
-        assertEquals(messagingDb.pinMessage(1), true);
-        assertEquals(messagingDb.pinMessage(2), false);
-        assertEquals(messagingDb.pinMessage(3), false);
+        assertEquals(messagingDatabase.pinMessage(1), true);
+        assertEquals(messagingDatabase.pinMessage(2), false);
+        assertEquals(messagingDatabase.pinMessage(3), false);
 
-        verify(messagingDb).pinMessage(3);
-        verify(messagingDb).pinMessage(3);
-        verify(messagingDb).pinMessage(3);
+        verify(messagingDatabase).pinMessage(3);
+        verify(messagingDatabase).pinMessage(3);
+        verify(messagingDatabase).pinMessage(3);
     }
 
     @Test
     // This test is to check the behaviour of the method
     public void testUnpinMessageMethod() {
-        assertEquals(messagingDb.unpinMessage(1), true);
-        assertEquals(messagingDb.unpinMessage(2), false);
-        assertEquals(messagingDb.unpinMessage(3), false);
+        assertEquals(messagingDatabase.unpinMessage(1), true);
+        assertEquals(messagingDatabase.unpinMessage(2), false);
+        assertEquals(messagingDatabase.unpinMessage(3), false);
 
-        verify(messagingDb).unpinMessage(1);
-        verify(messagingDb).unpinMessage(2);
-        verify(messagingDb).unpinMessage(3);
+        verify(messagingDatabase).unpinMessage(1);
+        verify(messagingDatabase).unpinMessage(2);
+        verify(messagingDatabase).unpinMessage(3);
     }
 
 }
