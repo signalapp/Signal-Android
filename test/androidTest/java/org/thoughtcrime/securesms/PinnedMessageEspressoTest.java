c55b355948ed714ac0a62bab50eb09f9fd8de55d
g-harel <gabrielj.harel@gmail.com>
2018-02-10 19:34:37 -0500

    #13 added data to startup intent for pinned message activity

--- test/androidTest/java/org/thoughtcrime/securesms/PinnedMessageEspressoTest.java
+++ test/androidTest/java/org/thoughtcrime/securesms/PinnedMessageEspressoTest.java
@@ -3,6 +3,8 @@ package org.thoughtcrime.securesms;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import android.content.Intent;
 import android.support.test.runner.AndroidJUnit4;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.filters.LargeTest;
@@ -16,11 +18,20 @@ import static android.support.test.espresso.assertion.ViewAssertions.matches;
 public class PinnedMessageEspressoTest {
 
     @Rule
-    public ActivityTestRule<PinnedMessageActivity> mActivityRule =
-            new ActivityTestRule(PinnedMessageActivity.class);
+    public ActivityTestRule<PinnedMessageActivity> pinnedActivityRule =
+            new ActivityTestRule(PinnedMessageActivity.class, true, false);
 
     @Test
     public void pageExists() {
+        Intent intent = new Intent();
+
+        intent.putExtra("address", "514 123 4567");
+        intent.putExtra("THREADID", 1);
+
+        pinnedActivityRule.launchActivity(intent);
+
+        // onView(withId(R.id.pinned_message_body)).check(matches(isDisplayed()));
+
         onView(withText("Signal needs access to your contacts and media in order to connect with friends, exchange messages, and make secure calls")).check(matches(isDisplayed()));
     }
 }
