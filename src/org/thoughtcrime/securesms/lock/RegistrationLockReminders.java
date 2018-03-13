c9457df92b5d7ec364d592fdcf73509e37edd97f
FeuRenard <feurenard@posteo.de>
2018-03-13 20:12:46 +0100

    Allow skipping registration PIN intervals on success

--- src/org/thoughtcrime/securesms/lock/RegistrationLockReminders.java
+++ src/org/thoughtcrime/securesms/lock/RegistrationLockReminders.java
@@ -6,8 +6,6 @@ import android.support.annotation.NonNull;
 
 import org.thoughtcrime.securesms.util.TextSecurePreferences;
 
-import java.util.HashMap;
-import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
 public class RegistrationLockReminders {
@@ -24,16 +22,17 @@ public class RegistrationLockReminders {
   }
 
   public static void scheduleReminder(@NonNull Context context, boolean success) {
-    long lastReminderInterval = TextSecurePreferences.getRegistrationLockNextReminderInterval(context);
     long nextReminderInterval;
 
     if (success) {
-      if      (lastReminderInterval <= TimeUnit.HOURS.toMillis(6))  nextReminderInterval = TimeUnit.HOURS.toMillis(12);
-      else if (lastReminderInterval <= TimeUnit.HOURS.toMillis(12)) nextReminderInterval = TimeUnit.DAYS.toMillis(1);
-      else if (lastReminderInterval <= TimeUnit.DAYS.toMillis(1))   nextReminderInterval = TimeUnit.DAYS.toMillis(3);
-      else if (lastReminderInterval <= TimeUnit.DAYS.toMillis(3))   nextReminderInterval = TimeUnit.DAYS.toMillis(7);
-      else                                                                  nextReminderInterval = TimeUnit.DAYS.toMillis(7);
+      long timeSinceLastReminder = System.currentTimeMillis() - TextSecurePreferences.getRegistrationLockLastReminderTime(context);
+      if      (timeSinceLastReminder >= TimeUnit.DAYS.toMillis(3))   nextReminderInterval = TimeUnit.DAYS.toMillis(7);
+      else if (timeSinceLastReminder >= TimeUnit.DAYS.toMillis(1))   nextReminderInterval = TimeUnit.DAYS.toMillis(3);
+      else if (timeSinceLastReminder >= TimeUnit.HOURS.toMillis(12)) nextReminderInterval = TimeUnit.DAYS.toMillis(1);
+      else if (timeSinceLastReminder >= TimeUnit.HOURS.toMillis(6))  nextReminderInterval = TimeUnit.HOURS.toMillis(12);
+      else                                                           nextReminderInterval = TimeUnit.HOURS.toMillis(6);
     } else {
+      long lastReminderInterval = TextSecurePreferences.getRegistrationLockNextReminderInterval(context);
       if      (lastReminderInterval >= TimeUnit.DAYS.toMillis(7)) nextReminderInterval = TimeUnit.DAYS.toMillis(3);
       else if (lastReminderInterval >= TimeUnit.DAYS.toMillis(3)) nextReminderInterval = TimeUnit.DAYS.toMillis(1);
       else if (lastReminderInterval >= TimeUnit.DAYS.toMillis(1)) nextReminderInterval = TimeUnit.HOURS.toMillis(12);
