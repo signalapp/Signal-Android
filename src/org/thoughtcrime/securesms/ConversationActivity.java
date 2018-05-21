2fb4c8d81851585c5e6189de8408d806b9ffd406
Greyson Parrelli <greyson@signal.org>
2018-05-21 10:44:30 -0400

    Disable shared contact sending.
    Sending shared contacts isn't backwards-compatible, so we want to have a
    few releases where receiving is enabled, but not sending. That way, when
    we enable sending, most users should be able to properly receive the
    messages.

--- src/org/thoughtcrime/securesms/ConversationActivity.java
+++ src/org/thoughtcrime/securesms/ConversationActivity.java
@@ -426,11 +426,13 @@ public class ConversationActivity extends PassphraseRequiredActionBarActivity
       setMedia(data.getData(), MediaType.AUDIO);
       break;
     case PICK_CONTACT:
-      if (isSecureText && !isSmsForced()) {
-        openContactShareEditor(data.getData());
-      } else {
-        addAttachmentContactInfo(data.getData());
-      }
+      // TODO(greyson): Re-enable shared contact sending after receiving has been enabled for a few releases
+      addAttachmentContactInfo(data.getData());
+//      if (isSecureText && !isSmsForced()) {
+//        openContactShareEditor(data.getData());
+//      } else {
+//        addAttachmentContactInfo(data.getData());
+//      }
       break;
     case GET_CONTACT_DETAILS:
       sendSharedContact(data.getParcelableArrayListExtra(ContactShareEditActivity.KEY_CONTACTS));
