26786ed0b523bc773bed80c0e3b77d2e439620b3
Django Cass <dj.cass44@gmail.com>
2018-06-03 18:56:50 +1000

    Added missing theme check for no-action-bar style

--- src/org/thoughtcrime/securesms/util/DynamicNoActionBarTheme.java
+++ src/org/thoughtcrime/securesms/util/DynamicNoActionBarTheme.java
@@ -1,6 +1,7 @@
 package org.thoughtcrime.securesms.util;
 
 import android.app.Activity;
+import android.content.res.Configuration;
 
 import org.thoughtcrime.securesms.R;
 
@@ -9,7 +10,15 @@ public class DynamicNoActionBarTheme extends DynamicTheme {
   protected int getSelectedTheme(Activity activity) {
     String theme = TextSecurePreferences.getTheme(activity);
 
-    if (theme.equals("dark")) return R.style.TextSecure_DarkNoActionBar;
+    if (theme.equals(SYSTEM)) {
+      int systemFlags = activity.getResources().getConfiguration().uiMode & Configuration.UI_MODE_NIGHT_MASK;
+      if(systemFlags == Configuration.UI_MODE_NIGHT_YES)
+        return R.style.TextSecure_DarkNoActionBar;
+      else
+        return R.style.TextSecure_LightNoActionBar;
+    }
+    else if (theme.equals(DARK))
+      return R.style.TextSecure_DarkNoActionBar;
 
     return R.style.TextSecure_LightNoActionBar;
   }
