a1b2887bf3cc18d5c21c61be92db1a58128dbde9
Greyson Parrelli <greyson@signal.org>
2018-08-28 11:52:26 -0700

    Fixed off-by-one crash when navigating to message from FTS.
    Related to #8145

--- src/org/thoughtcrime/securesms/ConversationFragment.java
+++ src/org/thoughtcrime/securesms/ConversationFragment.java
@@ -465,7 +465,7 @@ public class ConversationFragment extends Fragment
 
     int limit  = args.getInt(KEY_LIMIT, PARTIAL_CONVERSATION_LIMIT);
     int offset = 0;
-    if (limit != 0 && startingPosition > limit) {
+    if (limit != 0 && startingPosition >= limit) {
       offset = Math.max(startingPosition - (limit / 2) + 1, 0);
       startingPosition -= offset - 1;
     }
