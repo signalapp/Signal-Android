f26c6f890f33ea9c215f3403ca3aac6d68e88cc9
Greyson Parrelli <greyson@signal.org>
2018-10-04 11:35:19 -0700

    Fix non-contact icon in recipient preferences being too small.
    Relates to #8252

--- src/org/thoughtcrime/securesms/recipients/Recipient.java
+++ src/org/thoughtcrime/securesms/recipients/Recipient.java
@@ -426,7 +426,7 @@ public class Recipient implements RecipientModifiedListener {
     if      (isResolving())            return new TransparentContactPhoto();
     else if (isGroupRecipient())       return new ResourceContactPhoto(R.drawable.ic_group_white_24dp, R.drawable.ic_group_large);
     else if (!TextUtils.isEmpty(name)) return new GeneratedContactPhoto(name, R.drawable.ic_profile_default);
-    else                               return new ResourceContactPhoto(R.drawable.ic_profile_default);
+    else                               return new ResourceContactPhoto(R.drawable.ic_profile_default, R.drawable.ic_person_large);
   }
 
   public synchronized @Nullable ContactPhoto getContactPhoto() {
