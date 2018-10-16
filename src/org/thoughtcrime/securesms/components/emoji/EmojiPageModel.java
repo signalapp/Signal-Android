1999d0990186fc95f31804bf28f2cfc13cbf54ad
Greyson Parrelli <greyson@signal.org>
2018-10-16 09:31:46 -0700

    Updated emoji set.
    Includes display support for more genders, and more notably, skin tones.
    These are not currently selectable in the UI, but they will be rendered
    properly when other clients send them.

--- src/org/thoughtcrime/securesms/components/emoji/EmojiPageModel.java
+++ src/org/thoughtcrime/securesms/components/emoji/EmojiPageModel.java
@@ -3,6 +3,7 @@ package org.thoughtcrime.securesms.components.emoji;
 public interface EmojiPageModel {
   int getIconAttr();
   String[] getEmoji();
+  String[] getDisplayEmoji();
   boolean hasSpriteMap();
   String getSprite();
   boolean isDynamic();
