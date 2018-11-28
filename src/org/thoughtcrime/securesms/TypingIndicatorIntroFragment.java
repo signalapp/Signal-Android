5a53268534455ed0b4471d783cabd38f6a3b3292
Greyson Parrelli <greyson@signal.org>
2018-11-27 17:45:58 -0800

    Update typing indicator intro screen.

--- src/org/thoughtcrime/securesms/TypingIndicatorIntroFragment.java
+++ src/org/thoughtcrime/securesms/TypingIndicatorIntroFragment.java
@@ -1,6 +1,7 @@
 package org.thoughtcrime.securesms;
 
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.SwitchCompat;
@@ -14,6 +15,8 @@ import org.thoughtcrime.securesms.util.TextSecurePreferences;
 import org.thoughtcrime.securesms.util.ViewUtil;
 
 public class TypingIndicatorIntroFragment extends Fragment {
+
+  private Controller controller;
 
   public static TypingIndicatorIntroFragment newInstance() {
     TypingIndicatorIntroFragment fragment = new TypingIndicatorIntroFragment();
@@ -30,23 +33,44 @@ public class TypingIndicatorIntroFragment extends Fragment {
   }
 
   @Override
+  public void onAttach(Context context) {
+    super.onAttach(context);
+
+    if (!(getActivity() instanceof Controller)) {
+      throw new IllegalStateException("Parent activity must implement the Controller interface.");
+    }
+
+    controller = (Controller) getActivity();
+  }
+
+  @Override
   public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-    View         v          = inflater.inflate(R.layout.experience_upgrade_typing_indicators_fragment, container, false);
-    SwitchCompat preference = ViewUtil.findById(v, R.id.preference);
+    View view      = inflater.inflate(R.layout.experience_upgrade_typing_indicators_fragment, container, false);
+    View yesButton = view.findViewById(R.id.experience_yes_button);
+    View noButton  = view.findViewById(R.id.experience_no_button);
 
-    ((TypingIndicatorView) v.findViewById(R.id.typing_indicator)).startAnimation();
+    ((TypingIndicatorView) view.findViewById(R.id.typing_indicator)).startAnimation();
 
-    preference.setChecked(TextSecurePreferences.isTypingIndicatorsEnabled(getContext()));
-    preference.setOnCheckedChangeListener((buttonView, isChecked) -> {
-      TextSecurePreferences.setTypingIndicatorsEnabled(getContext(), isChecked);
-      ApplicationContext.getInstance(requireContext())
-                        .getJobManager()
-                        .add(new MultiDeviceConfigurationUpdateJob(getContext(),
-                                                                   TextSecurePreferences.isReadReceiptsEnabled(requireContext()),
-                                                                   isChecked,
-                                                                   TextSecurePreferences.isShowUnidentifiedDeliveryIndicatorsEnabled(getContext())));
-    });
 
-    return v;
+    yesButton.setOnClickListener(v -> onButtonClicked(true));
+    noButton.setOnClickListener(v -> onButtonClicked(false));
+
+    return view;
+  }
+
+  private void onButtonClicked(boolean typingEnabled) {
+    TextSecurePreferences.setTypingIndicatorsEnabled(getContext(), typingEnabled);
+    ApplicationContext.getInstance(requireContext())
+        .getJobManager()
+        .add(new MultiDeviceConfigurationUpdateJob(getContext(),
+                                                   TextSecurePreferences.isReadReceiptsEnabled(requireContext()),
+                                                   typingEnabled,
+                                                   TextSecurePreferences.isShowUnidentifiedDeliveryIndicatorsEnabled(getContext())));
+
+    controller.onFinished();
+  }
+
+  public interface Controller {
+    void onFinished();
   }
 }
