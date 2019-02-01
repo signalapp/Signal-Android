4c344ef82bc2021c191411262a6de5a6e0c5e9bd
oittaa <oittaa@users.noreply.github.com>
2019-02-01 02:51:22 +0200

    updated apkdiff.py work with current version
    The guide for reproducible builds uses this script, but the script wasn't working with the current versions. These changes fixed the problems for me.

--- apkdiff/apkdiff.py
+++ apkdiff/apkdiff.py
@@ -1,20 +1,20 @@
-#! /usr/bin/env python
+#! /usr/bin/env python3
 
 import sys
 from zipfile import ZipFile
 
 class ApkDiff:
 
-    IGNORE_FILES = ["META-INF/CERT.RSA", "META-INF/CERT.SF", "META-INF/MANIFEST.MF"]
+    IGNORE_FILES = ["META-INF/MANIFEST.MF", "META-INF/SIGNAL_S.RSA", "META-INF/SIGNAL_S.SF"]
 
     def compare(self, sourceApk, destinationApk):
         sourceZip      = ZipFile(sourceApk, 'r')
         destinationZip = ZipFile(destinationApk, 'r')
 
         if self.compareManifests(sourceZip, destinationZip) and self.compareEntries(sourceZip, destinationZip) == True:
-            print "APKs match!"
+            print("APKs match!")
         else:
-            print "APKs don't match!"
+            print("APKs don't match!")
 
     def compareManifests(self, sourceZip, destinationZip):
         sourceEntrySortedList      = sorted(sourceZip.namelist())
@@ -23,23 +23,23 @@ class ApkDiff:
         for ignoreFile in self.IGNORE_FILES:
             while ignoreFile in sourceEntrySortedList: sourceEntrySortedList.remove(ignoreFile)
             while ignoreFile in destinationEntrySortedList: destinationEntrySortedList.remove(ignoreFile)
-                    
+
         if len(sourceEntrySortedList) != len(destinationEntrySortedList):
-            print "Manifest lengths differ!"
-        
+            print("Manifest lengths differ!")
+
         for (sourceEntryName, destinationEntryName) in zip(sourceEntrySortedList, destinationEntrySortedList):
             if sourceEntryName != destinationEntryName:
-                print "Sorted manifests don't match, %s vs %s" % (sourceEntryName, destinationEntryName)   
+                print("Sorted manifests don't match, %s vs %s" % (sourceEntryName, destinationEntryName))
                 return False
 
         return True
-            
+
     def compareEntries(self, sourceZip, destinationZip):
         sourceInfoList      = filter(lambda sourceInfo: sourceInfo.filename not in self.IGNORE_FILES, sourceZip.infolist())
         destinationInfoList = filter(lambda destinationInfo: destinationInfo.filename not in self.IGNORE_FILES, destinationZip.infolist())
-        
-        if len(sourceInfoList) != len(destinationInfoList):
-            print "APK info lists of different length!"
+
+        if len(list(sourceInfoList)) != len(list(destinationInfoList)):
+            print("APK info lists of different length!")
             return False
 
         for sourceEntryInfo in sourceInfoList:
@@ -49,12 +49,12 @@ class ApkDiff:
                     destinationEntry = destinationZip.open(destinationEntryInfo, 'r')
 
                     if self.compareFiles(sourceEntry, destinationEntry) != True:
-                        print "APK entry %s does not match %s!" % (sourceEntryInfo.filename, destinationEntryInfo.filename)
+                        print("APK entry %s does not match %s!" % (sourceEntryInfo.filename, destinationEntryInfo.filename))
                         return False
 
                     destinationInfoList.remove(destinationEntryInfo)
                     break
-                
+
         return True
 
     def compareFiles(self, sourceFile, destinationFile):
@@ -72,7 +72,7 @@ class ApkDiff:
 
 if __name__ == '__main__':
     if len(sys.argv) != 3:
-        print "Usage: apkdiff <pathToFirstApk> <pathToSecondApk>"
+        print("Usage: apkdiff <pathToFirstApk> <pathToSecondApk>")
         sys.exit(1)
 
     ApkDiff().compare(sys.argv[1], sys.argv[2])
