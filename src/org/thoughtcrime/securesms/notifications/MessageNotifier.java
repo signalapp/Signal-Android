45ddb7e1ad05a599a89cf84185a6087279483ee8
Greyson Parrelli <greyson@signal.org>
2019-02-07 16:17:17 -0800

    Revert "Fix notification badge display."
    This reverts commit a0c1446e9ff283b6e77cceb79ee16ba4e3c089d9.

--- src/org/thoughtcrime/securesms/notifications/MessageNotifier.java
+++ src/org/thoughtcrime/securesms/notifications/MessageNotifier.java
@@ -311,6 +311,7 @@ public class MessageNotifier {
     builder.setGroup(NOTIFICATION_GROUP);
     builder.setDeleteIntent(notificationState.getDeleteIntent(context));
     builder.setOnlyAlertOnce(!signal);
+    builder.setGroupAlertBehavior(NotificationCompat.GROUP_ALERT_SUMMARY);
 
     long timestamp = notifications.get(0).getTimestamp();
     if (timestamp != 0) builder.setWhen(timestamp);
@@ -335,8 +336,8 @@ public class MessageNotifier {
                         notifications.get(0).getText());
     }
 
-    if (bundled) {
-      builder.setGroupAlertBehavior(NotificationCompat.GROUP_ALERT_SUMMARY);
+    if (!bundled) {
+      builder.setGroupSummary(true);
     }
 
     Notification notification = builder.build();
