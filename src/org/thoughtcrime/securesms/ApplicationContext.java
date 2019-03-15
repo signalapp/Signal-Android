8aa185070bf16fae370e3c7cac17bbcc4d9f5852
Moxie Marlinspike <moxie@thoughtcrime.org>
2019-03-15 15:21:53 -0700

    Update libsignalservice to 2.13.0
    - Eliminate the explicit spongycastle dependency. All access to
      primitives is done through the JCE interfaces now, which allows
      us to use a secure native-backed provider like conscrypt.
    
    - Use conscrypt for our default security provider. This gives us
      fast TLS 1.2 and 1.3 support on all devices, even before they
      had platform support (like 4.4).
    
    - Update minSdk to 18. Unfortunately the JCE interfaces for GCM
      primitives are JDK 7+ (!) only, which became supported by Android
      at 18.

--- src/org/thoughtcrime/securesms/ApplicationContext.java
+++ src/org/thoughtcrime/securesms/ApplicationContext.java
@@ -28,11 +28,9 @@ import android.support.multidex.MultiDexApplication;
 
 import com.google.android.gms.security.ProviderInstaller;
 
+import org.conscrypt.Conscrypt;
 import org.thoughtcrime.securesms.components.TypingStatusRepository;
 import org.thoughtcrime.securesms.components.TypingStatusSender;
-import org.thoughtcrime.securesms.crypto.PRNGFixes;
-import org.thoughtcrime.securesms.database.DatabaseFactory;
-import org.thoughtcrime.securesms.database.helpers.SQLCipherOpenHelper;
 import org.thoughtcrime.securesms.dependencies.AxolotlStorageModule;
 import org.thoughtcrime.securesms.dependencies.InjectableType;
 import org.thoughtcrime.securesms.dependencies.SignalCommunicationModule;
@@ -66,6 +64,7 @@ import org.webrtc.voiceengine.WebRtcAudioManager;
 import org.webrtc.voiceengine.WebRtcAudioUtils;
 import org.whispersystems.libsignal.logging.SignalProtocolLoggerProvider;
 
+import java.security.Security;
 import java.util.HashSet;
 import java.util.Set;
 import java.util.concurrent.TimeUnit;
@@ -104,7 +103,7 @@ public class ApplicationContext extends MultiDexApplication implements Dependenc
   public void onCreate() {
     super.onCreate();
     Log.i(TAG, "onCreate()");
-    initializeRandomNumberFix();
+    initializeSecurityProvider();
     initializeLogging();
     initializeCrashHandling();
     initializeDependencyInjection();
@@ -171,8 +170,8 @@ public class ApplicationContext extends MultiDexApplication implements Dependenc
     return persistentLogger;
   }
 
-  private void initializeRandomNumberFix() {
-    PRNGFixes.apply();
+  private void initializeSecurityProvider() {
+    Security.insertProviderAt(Conscrypt.newProvider(), 1);
   }
 
   private void initializeLogging() {
