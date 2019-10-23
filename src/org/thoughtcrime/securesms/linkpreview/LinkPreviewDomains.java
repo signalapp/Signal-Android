bc750964a8e112f4c59dd849100b8d9e4f2b4fd0
Niels Andriesse <andriesseniels@gmail.com>
2019-10-23 11:55:17 +1100

    Fix Giphy link previews

--- src/org/thoughtcrime/securesms/linkpreview/LinkPreviewDomains.java
+++ src/org/thoughtcrime/securesms/linkpreview/LinkPreviewDomains.java
@@ -8,22 +8,34 @@ public class LinkPreviewDomains {
   public static final String STICKERS = "signal.org";
 
   public static final Set<String> LINKS = new HashSet<>(Arrays.asList(
+        // YouTube
       "youtube.com",
       "www.youtube.com",
       "m.youtube.com",
       "youtu.be",
+      // Reddit
       "reddit.com",
       "www.reddit.com",
       "m.reddit.com",
+      // Imgur
       "imgur.com",
       "www.imgur.com",
       "m.imgur.com",
+      // Instagram
       "instagram.com",
       "www.instagram.com",
       "m.instagram.com",
+      // Pinterest
       "pinterest.com",
       "www.pinterest.com",
-      "pin.it"
+      "pin.it",
+      // Giphy
+      "giphy.com",
+      "media.giphy.com",
+      "media1.giphy.com",
+      "media2.giphy.com",
+      "media3.giphy.com",
+      "gph.is"
   ));
 
   public static final Set<String> IMAGES = new HashSet<>(Arrays.asList(
