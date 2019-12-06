8cb77689a6d920c4eb1b8e957724b9ec88ca1c67
yate <yate@users.noreply.github.com>
2019-12-06 02:49:32 -0500

    missing import

--- src/org/thoughtcrime/securesms/mms/IncomingLollipopMmsConnection.java
+++ src/org/thoughtcrime/securesms/mms/IncomingLollipopMmsConnection.java
@@ -26,6 +26,8 @@ import androidx.annotation.Nullable;
 
 import android.os.Bundle;
 import android.telephony.SmsManager;
+import android.text.TextUtils;
+
 import org.thoughtcrime.securesms.logging.Log;
 
 import com.google.android.mms.InvalidHeaderValueException;
