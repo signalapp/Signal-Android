8b640251b7fb99653562eaafbd590c89c9292b50
Niels Andriesse <andriesseniels@gmail.com>
2019-12-16 17:09:10 +0100

    Polish

--- src/org/thoughtcrime/securesms/loki/redesign/RegisterActivity.kt
+++ src/org/thoughtcrime/securesms/loki/redesign/RegisterActivity.kt
@@ -2,6 +2,10 @@ package org.thoughtcrime.securesms.loki.redesign
 
 import android.content.Intent
 import android.os.Bundle
+import android.support.v7.app.ActionBar
+import android.view.Gravity
+import android.widget.ImageView
+import android.widget.RelativeLayout
 import kotlinx.android.synthetic.main.activity_register.*
 import network.loki.messenger.R
 import org.thoughtcrime.securesms.BaseActionBarActivity
@@ -11,6 +15,16 @@ class RegisterActivity : BaseActionBarActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+        supportActionBar!!.setDisplayShowHomeEnabled(false)
+        supportActionBar!!.setDisplayShowTitleEnabled(false)
+        val logoImageView = ImageView(this)
+        logoImageView.setImageResource(R.drawable.session_logo)
+        val logoImageViewContainer = RelativeLayout(this)
+        logoImageViewContainer.addView(logoImageView)
+        logoImageViewContainer.gravity = Gravity.CENTER
+        val logoImageViewContainerLayoutParams = ActionBar.LayoutParams(ActionBar.LayoutParams.MATCH_PARENT, ActionBar.LayoutParams.WRAP_CONTENT)
+        supportActionBar!!.setCustomView(logoImageViewContainer, logoImageViewContainerLayoutParams)
+        supportActionBar!!.setDisplayShowCustomEnabled(true)
         registerButton.setOnClickListener { register() }
     }
 
--- src/org/thoughtcrime/securesms/loki/redesign/RestoreActivity.kt
+++ src/org/thoughtcrime/securesms/loki/redesign/RestoreActivity.kt
@@ -1,6 +1,10 @@
 package org.thoughtcrime.securesms.loki.redesign
 
 import android.os.Bundle
+import android.support.v7.app.ActionBar
+import android.view.Gravity
+import android.widget.ImageView
+import android.widget.RelativeLayout
 import kotlinx.android.synthetic.main.activity_restore.*
 import network.loki.messenger.R
 import org.thoughtcrime.securesms.BaseActionBarActivity
@@ -9,6 +13,16 @@ class RestoreActivity : BaseActionBarActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        supportActionBar!!.setDisplayShowHomeEnabled(false)
+        supportActionBar!!.setDisplayShowTitleEnabled(false)
+        val logoImageView = ImageView(this)
+        logoImageView.setImageResource(R.drawable.session_logo)
+        val logoImageViewContainer = RelativeLayout(this)
+        logoImageViewContainer.addView(logoImageView)
+        logoImageViewContainer.gravity = Gravity.CENTER
+        val logoImageViewContainerLayoutParams = ActionBar.LayoutParams(ActionBar.LayoutParams.MATCH_PARENT, ActionBar.LayoutParams.WRAP_CONTENT)
+        supportActionBar!!.setCustomView(logoImageViewContainer, logoImageViewContainerLayoutParams)
+        supportActionBar!!.setDisplayShowCustomEnabled(true)
         setContentView(R.layout.activity_restore)
     }
 
