460f25629b8408cbf96472558adaf3e3bda29f76
Niels Andriesse <andriesseniels@gmail.com>
2020-01-28 09:30:26 +1100

    Don't show seed reminder after restoring from seed

--- src/org/thoughtcrime/securesms/loki/redesign/activities/RestoreActivity.kt
+++ src/org/thoughtcrime/securesms/loki/redesign/activities/RestoreActivity.kt
@@ -87,6 +87,7 @@ class RestoreActivity : BaseActionBarActivity() {
                     IdentityKeyUtil.getIdentityKeyPair(this).publicKey, IdentityDatabase.VerifiedStatus.VERIFIED,
                     true, System.currentTimeMillis(), true)
             TextSecurePreferences.setLocalNumber(this, userHexEncodedPublicKey)
+            TextSecurePreferences.setHasViewedSeed(this, true)
             val intent = Intent(this, DisplayNameActivity::class.java)
             push(intent)
         } catch (e: Exception) {
