2fbe33736c48fbd4efcdf38c68d21120cc8b8b39
Niels Andriesse <andriesseniels@gmail.com>
2020-02-11 11:37:06 +1100

    Reduce background polling interval

--- src/org/thoughtcrime/securesms/loki/redesign/messaging/BackgroundPollWorker.kt
+++ src/org/thoughtcrime/securesms/loki/redesign/messaging/BackgroundPollWorker.kt
@@ -15,7 +15,7 @@ import java.util.concurrent.TimeUnit
 class BackgroundPollWorker : PersistentAlarmManagerListener() {
 
     companion object {
-        private val pollInterval = TimeUnit.MINUTES.toMillis(2)
+        private val pollInterval = TimeUnit.MINUTES.toMillis(1)
 
         @JvmStatic
         fun schedule(context: Context) {
