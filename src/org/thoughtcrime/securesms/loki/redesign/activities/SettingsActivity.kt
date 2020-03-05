a20ccd659a032b3b20d443be639f450bf953dd7d
Niels Andriesse <andriesseniels@gmail.com>
2020-03-05 14:24:10 +1100

    Clean

--- src/org/thoughtcrime/securesms/loki/redesign/activities/SettingsActivity.kt
+++ src/org/thoughtcrime/securesms/loki/redesign/activities/SettingsActivity.kt
@@ -95,8 +95,7 @@ class SettingsActivity : PassphraseRequiredActionBarActivity() {
         linkedDevicesButton.setOnClickListener { showLinkedDevices() }
         seedButton.setOnClickListener { showSeed() }
         clearAllDataButton.setOnClickListener { clearAllData() }
-
-        versionText.text = String.format(getString(R.string.version_s), BuildConfig.VERSION_NAME)
+        versionTextView.text = String.format(getString(R.string.version_s), BuildConfig.VERSION_NAME)
     }
 
     public override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
