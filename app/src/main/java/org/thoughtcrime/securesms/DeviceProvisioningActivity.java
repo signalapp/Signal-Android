1bb0f0d7d64324cca9ebd8b21149ea24308f9134
henry <henry.ph2@gmail.com>
2020-05-23 13:49:03 +0800

    Fix crash when open app through tsdevice:// deep link.

--- app/src/main/java/org/thoughtcrime/securesms/DeviceProvisioningActivity.java
+++ app/src/main/java/org/thoughtcrime/securesms/DeviceProvisioningActivity.java
@@ -2,41 +2,41 @@ package org.thoughtcrime.securesms;
 
 import android.content.Intent;
 import android.os.Bundle;
-import androidx.appcompat.app.AlertDialog;
-import android.view.Window;
 
-public class DeviceProvisioningActivity extends PassphraseRequiredActionBarActivity {
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import org.thoughtcrime.securesms.logging.Log;
+import org.thoughtcrime.securesms.util.DynamicNoActionBarTheme;
+import org.thoughtcrime.securesms.util.DynamicTheme;
+
+public class DeviceProvisioningActivity extends AppCompatActivity {
 
   @SuppressWarnings("unused")
-  private static final String TAG = DeviceProvisioningActivity.class.getSimpleName();
+  private static final String TAG = Log.tag(DeviceProvisioningActivity.class);
+
+  private final DynamicTheme dynamicTheme = new DynamicNoActionBarTheme();
 
   @Override
-  protected void onPreCreate() {
-    supportRequestWindowFeature(Window.FEATURE_NO_TITLE);
-  }
+  protected void onCreate(Bundle bundle) {
+    super.onCreate(bundle);
+    dynamicTheme.onCreate(this);
 
-  @Override
-  protected void onCreate(Bundle bundle, boolean ready) {
-    assert  getSupportActionBar() != null;
-    getSupportActionBar().hide();
-
-    AlertDialog dialog = new AlertDialog.Builder(this)
-        .setTitle(getString(R.string.DeviceProvisioningActivity_link_a_signal_device))
-        .setMessage(getString(R.string.DeviceProvisioningActivity_it_looks_like_youre_trying_to_link_a_signal_device_using_a_3rd_party_scanner))
-        .setPositiveButton(R.string.DeviceProvisioningActivity_continue, (dialog1, which) -> {
-          Intent intent = new Intent(DeviceProvisioningActivity.this, DeviceActivity.class);
-          intent.putExtra("add", true);
-          startActivity(intent);
-          finish();
-        })
-        .setNegativeButton(R.string.DeviceProvisioningActivity_cancel, (dialog12, which) -> {
-          dialog12.dismiss();
-          finish();
-        })
-        .setOnDismissListener(dialog13 -> finish())
-        .create();
-
-    dialog.setIcon(getResources().getDrawable(R.drawable.icon_dialog));
-    dialog.show();
+    new AlertDialog.Builder(this)
+                   .setTitle(getString(R.string.DeviceProvisioningActivity_link_a_signal_device))
+                   .setMessage(getString(R.string.DeviceProvisioningActivity_it_looks_like_youre_trying_to_link_a_signal_device_using_a_3rd_party_scanner))
+                   .setIcon(getResources().getDrawable(R.drawable.icon_dialog))
+                   .setPositiveButton(R.string.DeviceProvisioningActivity_continue, (dialog1, which) -> {
+                     Intent intent = new Intent(DeviceProvisioningActivity.this, DeviceActivity.class);
+                     intent.putExtra("add", true);
+                     startActivity(intent);
+                     finish();
+                   })
+                   .setNegativeButton(R.string.DeviceProvisioningActivity_cancel, (dialog12, which) -> {
+                     dialog12.dismiss();
+                     finish();
+                   })
+                   .setOnDismissListener(dialog13 -> finish())
+                   .show();
   }
 }
