66f26683269c9cbae9ad1c2eb5dc81985af2ef86
Fumiaki Yoshimatsu <fumiakiy@gmail.com>
2020-06-18 09:40:23 -0400

    Do not cache locale in each conversation object.
    Fixes #9751

--- app/src/main/java/org/thoughtcrime/securesms/conversationlist/model/Conversation.java
+++ app/src/main/java/org/thoughtcrime/securesms/conversationlist/model/Conversation.java
@@ -4,24 +4,15 @@ import androidx.annotation.NonNull;
 
 import org.thoughtcrime.securesms.database.model.ThreadRecord;
 
-import java.util.Locale;
-import java.util.Objects;
-
 public class Conversation {
   private final ThreadRecord threadRecord;
-  private final Locale       locale;
 
-  public Conversation(@NonNull ThreadRecord threadRecord, @NonNull Locale locale) {
+  public Conversation(@NonNull ThreadRecord threadRecord) {
     this.threadRecord = threadRecord;
-    this.locale       = locale;
   }
 
   public @NonNull ThreadRecord getThreadRecord() {
     return threadRecord;
-  }
-
-  public @NonNull Locale getLocale() {
-    return locale;
   }
 
   @Override
@@ -29,12 +20,11 @@ public class Conversation {
     if (this == o) return true;
     if (o == null || getClass() != o.getClass()) return false;
     Conversation that = (Conversation) o;
-    return threadRecord.equals(that.threadRecord) &&
-           locale.equals(that.locale);
+    return threadRecord.equals(that.threadRecord);
   }
 
   @Override
   public int hashCode() {
-    return Objects.hash(threadRecord, locale);
+    return threadRecord.hashCode();
   }
 }
