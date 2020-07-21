4e55d2d94178300440cae14b326259a01e3f6e95
Alan Evans <alan@signal.org>
2020-07-21 15:54:34 -0300

    Tint pending group invites menu icon.

--- app/src/main/java/org/thoughtcrime/securesms/groups/ui/PopupMenuView.java
+++ app/src/main/java/org/thoughtcrime/securesms/groups/ui/PopupMenuView.java
@@ -1,6 +1,9 @@
 package org.thoughtcrime.securesms.groups.ui;
 
 import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
 import android.util.AttributeSet;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -12,8 +15,12 @@ import androidx.annotation.MenuRes;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.widget.PopupMenu;
+import androidx.core.content.ContextCompat;
+import androidx.core.graphics.drawable.DrawableCompat;
 
 import org.thoughtcrime.securesms.R;
+
+import java.util.Objects;
 
 public final class PopupMenuView extends View {
 
@@ -23,21 +30,33 @@ public final class PopupMenuView extends View {
 
   public PopupMenuView(Context context) {
     super(context);
-    init();
+    init(null);
   }
 
   public PopupMenuView(Context context, @Nullable AttributeSet attrs) {
     super(context, attrs);
-    init();
+    init(attrs);
   }
 
   public PopupMenuView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
     super(context, attrs, defStyleAttr);
-    init();
+    init(attrs);
   }
 
-  private void init() {
+  private void init(@Nullable AttributeSet attrs) {
     setBackgroundResource(R.drawable.ic_more_vert_24);
+
+    if (attrs != null) {
+      TypedArray typedArray = getContext().getTheme().obtainStyledAttributes(attrs, R.styleable.PopupMenuView, 0, 0);
+      int        tint       = typedArray.getColor(R.styleable.PopupMenuView_background_tint, Color.BLACK);
+      Drawable   drawable   = ContextCompat.getDrawable(getContext(), R.drawable.ic_more_vert_24);
+
+      DrawableCompat.setTint(Objects.requireNonNull(drawable), tint);
+
+      setBackground(drawable);
+
+      typedArray.recycle();
+    }
 
     setOnClickListener(v -> {
       if (callback != null) {
