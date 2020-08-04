fdf4ad9543248a74437bc8cff631f328594e627d
Alan Evans <alan@signal.org>
2020-08-04 14:12:27 -0300

    Remove the GV2 "anyone" access level.

--- app/src/main/java/org/thoughtcrime/securesms/groups/GV2AccessLevelUtil.java
+++ app/src/main/java/org/thoughtcrime/securesms/groups/GV2AccessLevelUtil.java
@@ -14,7 +14,6 @@ public final class GV2AccessLevelUtil {
 
   public static String toString(@NonNull Context context, @NonNull AccessControl.AccessRequired attributeAccess) {
     switch (attributeAccess) {
-      case ANY           : return context.getString(R.string.GroupManagement_access_level_anyone);
       case MEMBER        : return context.getString(R.string.GroupManagement_access_level_all_members);
       case ADMINISTRATOR : return context.getString(R.string.GroupManagement_access_level_only_admins);
       default            : return context.getString(R.string.GroupManagement_access_level_unknown);
