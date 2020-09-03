5d6b23bae23d728a75145d16af9523ac3311296d
Anton Chekulaev <metaphore@bk.ru>
2020-09-03 11:34:49 +1000

    Light theme feature intro bottom sheet is expanded by default.

--- src/org/thoughtcrime/securesms/loki/dialogs/LightThemeFeatureIntroBottomSheet.kt
+++ src/org/thoughtcrime/securesms/loki/dialogs/LightThemeFeatureIntroBottomSheet.kt
@@ -1,13 +1,17 @@
 package org.thoughtcrime.securesms.loki.dialogs
 
+import android.app.Dialog
 import android.os.Bundle
-import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.FrameLayout
+import com.google.android.material.bottomsheet.BottomSheetBehavior
+import com.google.android.material.bottomsheet.BottomSheetDialog
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import kotlinx.android.synthetic.main.fragment_light_theme_feature_intro_bottom_sheet.*
-import kotlinx.android.synthetic.main.fragment_open_group_suggestion_bottom_sheet.*
 import network.loki.messenger.R
+
 
 class LightThemeFeatureIntroBottomSheet : BottomSheetDialogFragment() {
 
@@ -22,5 +26,16 @@ class LightThemeFeatureIntroBottomSheet : BottomSheetDialogFragment() {
         super.onViewCreated(view, savedInstanceState)
 
         okButton.setOnClickListener { dismiss() }
+    }
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        val dialog = super.onCreateDialog(savedInstanceState)
+        // Make the bottom sheet be expanded by default.
+        dialog.setOnShowListener {
+            val d = dialog as BottomSheetDialog
+            val bottomSheet = d.findViewById<FrameLayout>(com.google.android.material.R.id.design_bottom_sheet)
+            BottomSheetBehavior.from(bottomSheet!!).setState(BottomSheetBehavior.STATE_EXPANDED);
+        }
+        return dialog
     }
 }
