846d4cfa7f61cc202f417bcf99b0b8846e0f7224
Josh Soref <jsoref@users.noreply.github.com>
2020-10-21 20:33:03 -0400

    spelling: marlinspike
    Signed-off-by: Josh Soref <jsoref@users.noreply.github.com>

--- app/src/main/java/org/thoughtcrime/securesms/database/model/MessageRecord.java
+++ app/src/main/java/org/thoughtcrime/securesms/database/model/MessageRecord.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2012 Moxie Marlinpsike
+ * Copyright (C) 2012 Moxie Marlinspike
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
