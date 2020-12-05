cb1b4ec0b9fd23dbf0a21c71b52bb2debdb2f2bf
Greyson Parrelli <greyson@signal.org>
2020-12-05 14:11:36 -0500

    Rotate the GV1->GV2 auto migration flag.

--- app/src/main/java/org/thoughtcrime/securesms/util/FeatureFlags.java
+++ app/src/main/java/org/thoughtcrime/securesms/util/FeatureFlags.java
@@ -60,7 +60,7 @@ public final class FeatureFlags {
   public  static final String DONATE_MEGAPHONE             = "android.donate";
   private static final String VIEWED_RECEIPTS              = "android.viewed.receipts";
   private static final String GROUP_CALLING                = "android.groupsv2.calling.2";
-  private static final String GV1_AUTO_MIGRATE             = "android.groupsV1Migration.auto.3";
+  private static final String GV1_AUTO_MIGRATE             = "android.groupsV1Migration.auto.4";
   private static final String GV1_MANUAL_MIGRATE           = "android.groupsV1Migration.manual";
   private static final String GV1_FORCED_MIGRATE           = "android.groupsV1Migration.forced";
   private static final String SEND_VIEWED_RECEIPTS         = "android.sendViewedReceipts";
