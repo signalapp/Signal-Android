1c4e4581c8795cc6b3820a6c35a3833eb63f57ee
Ryan ZHAO <ryanzhaors@qq.com>
2020-12-18 16:44:13 +1100

    attachment in message provider

--- libsession/src/main/java/org/session/libsession/database/MessageDataProvider.kt
+++ libsession/src/main/java/org/session/libsession/database/MessageDataProvider.kt
@@ -1,9 +1,14 @@
 package org.session.libsession.database
 
 import org.session.libsession.database.dto.DatabaseAttachmentDTO
+import org.session.libsignal.service.api.messages.SignalServiceAttachmentPointer
 
 interface MessageDataProvider {
 
     fun getAttachment(uniqueID: String): DatabaseAttachmentDTO?
+    fun getAttachmentPointer(attachmentID: String): SignalServiceAttachmentPointer?
+
+    fun getMessageID(serverID: Long): Long?
+    fun deleteMessage(messageID: Long)
 
 }
