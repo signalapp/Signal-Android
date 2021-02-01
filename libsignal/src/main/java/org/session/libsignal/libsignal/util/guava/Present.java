058edbdfba5d769ecf9dd5f8c97a99d3d7fa23c1
Ryan ZHAO <ryanzhaors@qq.com>
2021-02-01 13:49:17 +1100

    minor clean

--- libsignal/src/main/java/org/session/libsignal/libsignal/util/guava/Present.java
+++ libsignal/src/main/java/org/session/libsignal/libsignal/util/guava/Present.java
@@ -16,10 +16,6 @@
 
 package org.session.libsignal.libsignal.util.guava;
 
-import org.session.libsignal.libsignal.util.guava.Function;
-import org.session.libsignal.libsignal.util.guava.Optional;
-import org.session.libsignal.libsignal.util.guava.Supplier;
-
 import static org.session.libsignal.libsignal.util.guava.Preconditions.checkNotNull;
 
 import java.util.Collections;
