ec07e4b233015949004de4ee5b7baeae1d011cf3
Greyson Parrelli <greyson@signal.org>
2021-03-13 11:50:10 -0500

    Avoid possibility of walking outside a contact cursor.

--- app/src/main/java/org/thoughtcrime/securesms/contacts/sync/DirectoryHelper.java
+++ app/src/main/java/org/thoughtcrime/securesms/contacts/sync/DirectoryHelper.java
@@ -334,7 +334,7 @@ public class DirectoryHelper {
             Log.w(TAG, "Ignoring unexpected mime type: " + mimeType);
           }
 
-          while (getLookupKey(cursor).equals(lookupKey) && isPhoneMimeType(getMimeType(cursor))) {
+          while (!cursor.isAfterLast() && getLookupKey(cursor).equals(lookupKey) && isPhoneMimeType(getMimeType(cursor))) {
             String number = CursorUtil.requireString(cursor, ContactsContract.CommonDataKinds.Phone.NUMBER);
 
             if (isValidContactNumber(number)) {
@@ -359,7 +359,7 @@ public class DirectoryHelper {
             cursor.moveToNext();
           }
 
-          if (getLookupKey(cursor).equals(lookupKey)) {
+          if (!cursor.isAfterLast() && getLookupKey(cursor).equals(lookupKey)) {
             if (isStructuredNameMimeType(getMimeType(cursor))) {
               StructuredNameRecord.Builder builder = new StructuredNameRecord.Builder();
 
