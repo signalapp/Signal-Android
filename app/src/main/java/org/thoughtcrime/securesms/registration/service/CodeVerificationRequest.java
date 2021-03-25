a59f5d953afe83ec3d131e144c9ecd0fc61fe532
Cody Henthorne <cody@signal.org>
2021-03-25 10:31:59 -0400

    Fix bug during registration when self exists already without an e164.

--- app/src/main/java/org/thoughtcrime/securesms/registration/service/CodeVerificationRequest.java
+++ app/src/main/java/org/thoughtcrime/securesms/registration/service/CodeVerificationRequest.java
@@ -238,7 +238,7 @@ public final class CodeVerificationRequest {
     }
 
     RecipientDatabase recipientDatabase = DatabaseFactory.getRecipientDatabase(context);
-    RecipientId       selfId            = recipientDatabase.getOrInsertFromE164(credentials.getE164number());
+    RecipientId       selfId            = Recipient.externalPush(context, uuid, credentials.getE164number(), true).getId();
 
     recipientDatabase.setProfileSharing(selfId, true);
     recipientDatabase.markRegisteredOrThrow(selfId, uuid);
