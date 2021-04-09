53b6cc21b16f08fc59b2f1997d62cc82b8331666
Alan Evans <alan@signal.org>
2021-04-09 16:48:49 -0300

    Make call to desugared classes from ApplicationContext indirect for API19.
    Fixes #11185

--- app/src/main/java/org/thoughtcrime/securesms/ApplicationContext.java
+++ app/src/main/java/org/thoughtcrime/securesms/ApplicationContext.java
@@ -81,8 +81,6 @@ import org.webrtc.voiceengine.WebRtcAudioUtils;
 import org.whispersystems.libsignal.logging.SignalProtocolLoggerProvider;
 
 import java.security.Security;
-import java.util.HashSet;
-import java.util.Set;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -322,31 +320,11 @@ public class ApplicationContext extends MultiDexApplication implements AppForegr
 
   private void initializeRingRtc() {
     try {
-      Set<String> HARDWARE_AEC_BLACKLIST = new HashSet<String>() {{
-        add("Pixel");
-        add("Pixel XL");
-        add("Moto G5");
-        add("Moto G (5S) Plus");
-        add("Moto G4");
-        add("TA-1053");
-        add("Mi A1");
-        add("Mi A2");
-        add("E5823"); // Sony z5 compact
-        add("Redmi Note 5");
-        add("FP2"); // Fairphone FP2
-        add("MI 5");
-      }};
-
-      Set<String> OPEN_SL_ES_WHITELIST = new HashSet<String>() {{
-        add("Pixel");
-        add("Pixel XL");
-      }};
-
-      if (HARDWARE_AEC_BLACKLIST.contains(Build.MODEL)) {
+      if (RtcDeviceLists.hardwareAECBlocked()) {
         WebRtcAudioUtils.setWebRtcBasedAcousticEchoCanceler(true);
       }
 
-      if (!OPEN_SL_ES_WHITELIST.contains(Build.MODEL)) {
+      if (!RtcDeviceLists.openSLESAllowed()) {
         WebRtcAudioManager.setBlacklistDeviceForOpenSLESUsage(true);
       }
 
