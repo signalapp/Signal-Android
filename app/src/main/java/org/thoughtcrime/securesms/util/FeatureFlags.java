3dd0a60555e322f8e9f951f04ea9a27d2f8ac02d
Greyson Parrelli <greyson@signal.org>
2021-04-15 10:57:40 -0400

    Rotate storage service V2 feature flag.

--- app/src/main/java/org/thoughtcrime/securesms/util/FeatureFlags.java
+++ app/src/main/java/org/thoughtcrime/securesms/util/FeatureFlags.java
@@ -75,7 +75,7 @@ public final class FeatureFlags {
   private static final String ANIMATED_STICKER_MIN_TOTAL_MEMORY = "android.animatedStickerMinTotalMemory";
   private static final String MESSAGE_PROCESSOR_ALARM_INTERVAL  = "android.messageProcessor.alarmIntervalMins";
   private static final String MESSAGE_PROCESSOR_DELAY           = "android.messageProcessor.foregroundDelayMs";
-  private static final String STORAGE_SYNC_V2                   = "android.storageSyncV2.2";
+  private static final String STORAGE_SYNC_V2                   = "android.storageSyncV2.3";
   private static final String NOTIFICATION_REWRITE              = "android.notificationRewrite";
 
   /**
