5ed9f88a8da22ff83741209f1fcddbbf1e92a775
Oscar Mira <valldrac@protonmail.ch>
2021-04-22 18:39:53 +0200

    Revert "Prevent crash in megaphones after backup restore."
    This reverts commit 7d949ee8fde011234e73834c3ef7c15eb4fa9e86.

--- app/src/main/java/org/thoughtcrime/securesms/megaphone/MegaphoneRepository.java
+++ app/src/main/java/org/thoughtcrime/securesms/megaphone/MegaphoneRepository.java
@@ -69,7 +69,6 @@ public class MegaphoneRepository {
   public void getNextMegaphone(@NonNull Callback<Megaphone> callback) {
     executor.execute(() -> {
       if (enabled) {
-        init();
         callback.onResult(Megaphones.getNextMegaphone(context, databaseCache));
       } else {
         callback.onResult(null);
