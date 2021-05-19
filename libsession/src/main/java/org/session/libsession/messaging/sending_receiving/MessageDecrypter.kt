99e6382f2ebd40ce2c9dff6c941c251ed4bb95c6
Harris <hjubb@users.noreply.github.com>
2021-05-19 15:12:35 +1000

    refactor: use lazysodium version on maven central instead of bintray (no longer available)

--- libsession/src/main/java/org/session/libsession/messaging/sending_receiving/MessageDecrypter.kt
+++ libsession/src/main/java/org/session/libsession/messaging/sending_receiving/MessageDecrypter.kt
@@ -1,15 +1,15 @@
 package org.session.libsession.messaging.sending_receiving
 
 import android.util.Log
-import com.goterl.lazycode.lazysodium.LazySodiumAndroid
-import com.goterl.lazycode.lazysodium.SodiumAndroid
-import com.goterl.lazycode.lazysodium.interfaces.Box
-import com.goterl.lazycode.lazysodium.interfaces.Sign
+import com.goterl.lazysodium.LazySodiumAndroid
+import com.goterl.lazysodium.SodiumAndroid
+import com.goterl.lazysodium.interfaces.Box
+import com.goterl.lazysodium.interfaces.Sign
 import org.session.libsignal.crypto.ecc.ECKeyPair
+import org.session.libsignal.utilities.Hex
 import org.session.libsignal.utilities.hexEncodedPublicKey
 import org.session.libsignal.utilities.removing05PrefixIfNeeded
 import org.session.libsignal.utilities.toHexString
-import org.session.libsignal.utilities.Hex
 
 object MessageDecrypter {
 
