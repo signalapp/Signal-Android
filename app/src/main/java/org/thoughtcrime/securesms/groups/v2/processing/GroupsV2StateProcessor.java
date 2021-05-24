513e5b45c5e718b5c3f283f4844544006b474ade
Greyson Parrelli <greyson@signal.org>
2021-05-24 14:44:11 -0400

    Show notifications for group creates.

--- app/src/main/java/org/thoughtcrime/securesms/groups/v2/processing/GroupsV2StateProcessor.java
+++ app/src/main/java/org/thoughtcrime/securesms/groups/v2/processing/GroupsV2StateProcessor.java
@@ -408,7 +408,7 @@ public final class GroupsV2StateProcessor {
           Log.d(TAG, "Skipping profile key changes only update message");
         } else {
           boolean insert = true;
-          if (entry.getChange() != null && DecryptedGroupUtil.changeIsEmpty(entry.getChange())) {
+          if (entry.getChange() != null && DecryptedGroupUtil.changeIsEmpty(entry.getChange()) && previousGroupState != null) {
             if (FeatureFlags.internalUser()) {
               Log.w(TAG, "Empty group update message seen. Inserting anyway.");
             } else {
