f96eac96f97211be0b25602a049a125ad5977548
Cody Henthorne <cody@signal.org>
2021-06-03 13:34:18 -0400

    Update keyboard colors to improve consistency.

--- app/src/main/java/org/thoughtcrime/securesms/keyboard/emoji/search/EmojiSearchFragment.kt
+++ app/src/main/java/org/thoughtcrime/securesms/keyboard/emoji/search/EmojiSearchFragment.kt
@@ -1,7 +1,6 @@
 package org.thoughtcrime.securesms.keyboard.emoji.search
 
 import android.content.Context
-import android.graphics.Rect
 import android.os.Bundle
 import android.view.View
 import android.widget.FrameLayout
