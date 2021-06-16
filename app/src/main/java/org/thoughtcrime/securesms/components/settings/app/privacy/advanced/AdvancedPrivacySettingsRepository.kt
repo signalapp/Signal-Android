3c6bed90db35671185fd007d38098956cf447168
Cody Henthorne <cody@signal.org>
2021-06-16 19:53:17 -0400

    Fix ANR by upgrading Firebase Messaging.

--- app/src/main/java/org/thoughtcrime/securesms/components/settings/app/privacy/advanced/AdvancedPrivacySettingsRepository.kt
+++ app/src/main/java/org/thoughtcrime/securesms/components/settings/app/privacy/advanced/AdvancedPrivacySettingsRepository.kt
@@ -1,7 +1,8 @@
 package org.thoughtcrime.securesms.components.settings.app.privacy.advanced
 
 import android.content.Context
-import com.google.firebase.iid.FirebaseInstanceId
+import com.google.android.gms.tasks.Tasks
+import com.google.firebase.installations.FirebaseInstallations
 import org.signal.core.util.concurrent.SignalExecutors
 import org.signal.core.util.logging.Log
 import org.thoughtcrime.securesms.database.DatabaseFactory
@@ -14,6 +15,7 @@ import org.thoughtcrime.securesms.util.TextSecurePreferences
 import org.whispersystems.libsignal.util.guava.Optional
 import org.whispersystems.signalservice.api.push.exceptions.AuthorizationFailedException
 import java.io.IOException
+import java.util.concurrent.ExecutionException
 
 private val TAG = Log.tag(AdvancedPrivacySettingsRepository::class.java)
 
@@ -29,11 +31,17 @@ class AdvancedPrivacySettingsRepository(private val context: Context) {
           Log.w(TAG, e)
         }
         if (!TextSecurePreferences.isFcmDisabled(context)) {
-          FirebaseInstanceId.getInstance().deleteInstanceId()
+          Tasks.await(FirebaseInstallations.getInstance().delete())
         }
         DisablePushMessagesResult.SUCCESS
       } catch (ioe: IOException) {
         Log.w(TAG, ioe)
+        DisablePushMessagesResult.NETWORK_ERROR
+      } catch (e: InterruptedException) {
+        Log.w(TAG, "Interrupted while deleting", e)
+        DisablePushMessagesResult.NETWORK_ERROR
+      } catch (e: ExecutionException) {
+        Log.w(TAG, "Error deleting", e.cause)
         DisablePushMessagesResult.NETWORK_ERROR
       }
 
