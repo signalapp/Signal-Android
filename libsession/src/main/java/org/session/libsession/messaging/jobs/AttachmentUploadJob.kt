9566120d662340023b88f73390f757e84eb42e33
jubb <hjubb@users.noreply.github.com>
2021-07-01 13:45:14 +1000

    fix: revert max failure count on upload and send jobs

--- libsession/src/main/java/org/session/libsession/messaging/jobs/AttachmentUploadJob.kt
+++ libsession/src/main/java/org/session/libsession/messaging/jobs/AttachmentUploadJob.kt
@@ -33,7 +33,7 @@ class AttachmentUploadJob(val attachmentID: Long, val threadID: String, val mess
     }
 
     // Settings
-    override val maxFailureCount: Int = 2
+    override val maxFailureCount: Int = 20
 
     companion object {
         val TAG = AttachmentUploadJob::class.simpleName
