8e2a265cf37fd470283ac34fc3d30927855f6453
Lucio Maciel <lucio@signal.org>
2021-08-13 11:58:05 -0300

    Update emoji search index on system locale changes.

--- app/src/main/java/org/thoughtcrime/securesms/notifications/LocaleChangedReceiver.java
+++ app/src/main/java/org/thoughtcrime/securesms/notifications/LocaleChangedReceiver.java
@@ -4,10 +4,13 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 
+import org.thoughtcrime.securesms.jobs.EmojiSearchIndexDownloadJob;
+
 public class LocaleChangedReceiver extends BroadcastReceiver {
 
   @Override
   public void onReceive(Context context, Intent intent) {
     NotificationChannels.create(context);
+    EmojiSearchIndexDownloadJob.scheduleImmediately();
   }
 }
