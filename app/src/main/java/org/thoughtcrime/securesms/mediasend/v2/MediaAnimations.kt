fae80a242d8107a54776afb14bc5a418a5407dae
Alex Hart <alex@signal.org>
2021-09-08 14:43:00 -0300

    Update animation interpolators in media send flow.

--- app/src/main/java/org/thoughtcrime/securesms/mediasend/v2/MediaAnimations.kt
+++ app/src/main/java/org/thoughtcrime/securesms/mediasend/v2/MediaAnimations.kt
@@ -1,13 +1,12 @@
 package org.thoughtcrime.securesms.mediasend.v2
 
 import android.view.animation.Interpolator
-import androidx.core.graphics.PathParser
-import androidx.core.view.animation.PathInterpolatorCompat
+import androidx.interpolator.view.animation.FastOutSlowInInterpolator
 
 object MediaAnimations {
   /**
    * Fast-In-Extra-Slow-Out Interpolator
    */
   @JvmStatic
-  val interpolator: Interpolator = PathInterpolatorCompat.create(PathParser.createPathFromPathData("M 0,0 C 0.05, 0, 0.133333, 0.06, 0.166666, 0.4 C 0.208333, 0.82, 0.25, 1, 1, 1"))
+  val interpolator: Interpolator = FastOutSlowInInterpolator()
 }
