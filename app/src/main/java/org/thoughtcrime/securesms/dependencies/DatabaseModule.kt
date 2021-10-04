d130ba5432d9d2c17ed99f1670899b90b5234866
Harris <hjubb@users.noreply.github.com>
2021-10-04 19:02:20 +1100

    fix: oom from too many db instances, adding singletons to limit object creation

--- app/src/main/java/org/thoughtcrime/securesms/dependencies/DatabaseModule.kt
+++ app/src/main/java/org/thoughtcrime/securesms/dependencies/DatabaseModule.kt
@@ -37,70 +37,92 @@ object DatabaseModule {
     }
 
     @Provides
+    @Singleton
     fun provideSmsDatabase(@ApplicationContext context: Context, openHelper: SQLCipherOpenHelper) = SmsDatabase(context, openHelper)
 
     @Provides
+    @Singleton
     fun provideMmsDatabase(@ApplicationContext context: Context, openHelper: SQLCipherOpenHelper) = MmsDatabase(context, openHelper)
 
     @Provides
+    @Singleton
     fun provideAttachmentDatabase(@ApplicationContext context: Context,
                                   openHelper: SQLCipherOpenHelper,
                                   attachmentSecret: AttachmentSecret) = AttachmentDatabase(context, openHelper, attachmentSecret)
     @Provides
+    @Singleton
     fun provideMediaDatbase(@ApplicationContext context: Context, openHelper: SQLCipherOpenHelper) = MediaDatabase(context, openHelper)
 
     @Provides
+    @Singleton
     fun provideThread(@ApplicationContext context: Context, openHelper: SQLCipherOpenHelper) = ThreadDatabase(context,openHelper)
 
     @Provides
+    @Singleton
     fun provideMmsSms(@ApplicationContext context: Context, openHelper: SQLCipherOpenHelper) = MmsSmsDatabase(context, openHelper)
 
     @Provides
+    @Singleton
     fun provideDraftDatabase(@ApplicationContext context: Context, openHelper: SQLCipherOpenHelper) = DraftDatabase(context, openHelper)
 
     @Provides
+    @Singleton
     fun providePushDatabase(@ApplicationContext context: Context, openHelper: SQLCipherOpenHelper) = PushDatabase(context,openHelper)
 
     @Provides
+    @Singleton
     fun provideGroupDatabase(@ApplicationContext context: Context, openHelper: SQLCipherOpenHelper) = GroupDatabase(context,openHelper)
 
     @Provides
+    @Singleton
     fun provideRecipientDatabase(@ApplicationContext context: Context, openHelper: SQLCipherOpenHelper) = RecipientDatabase(context,openHelper)
 
     @Provides
+    @Singleton
     fun provideGroupReceiptDatabase(@ApplicationContext context: Context, openHelper: SQLCipherOpenHelper) = GroupReceiptDatabase(context,openHelper)
 
     @Provides
+    @Singleton
     fun searchDatabase(@ApplicationContext context: Context, openHelper: SQLCipherOpenHelper) = SearchDatabase(context,openHelper)
 
     @Provides
+    @Singleton
     fun provideJobDatabase(@ApplicationContext context: Context, openHelper: SQLCipherOpenHelper) = JobDatabase(context, openHelper)
 
     @Provides
+    @Singleton
     fun provideLokiApiDatabase(@ApplicationContext context: Context, openHelper: SQLCipherOpenHelper) = LokiAPIDatabase(context,openHelper)
 
     @Provides
+    @Singleton
     fun provideLokiMessageDatabase(@ApplicationContext context: Context, openHelper: SQLCipherOpenHelper) = LokiMessageDatabase(context,openHelper)
 
     @Provides
+    @Singleton
     fun provideLokiThreadDatabase(@ApplicationContext context: Context, openHelper: SQLCipherOpenHelper) = LokiThreadDatabase(context,openHelper)
 
     @Provides
+    @Singleton
     fun provideLokiUserDatabase(@ApplicationContext context: Context, openHelper: SQLCipherOpenHelper) = LokiUserDatabase(context,openHelper)
 
     @Provides
+    @Singleton
     fun provideLokiBackupFilesDatabase(@ApplicationContext context: Context, openHelper: SQLCipherOpenHelper) = LokiBackupFilesDatabase(context,openHelper)
 
     @Provides
+    @Singleton
     fun provideSessionJobDatabase(@ApplicationContext context: Context, openHelper: SQLCipherOpenHelper) = SessionJobDatabase(context, openHelper)
 
     @Provides
+    @Singleton
     fun provideSessionContactDatabase(@ApplicationContext context: Context, openHelper: SQLCipherOpenHelper) = SessionContactDatabase(context,openHelper)
 
     @Provides
+    @Singleton
     fun provideStorage(@ApplicationContext context: Context, openHelper: SQLCipherOpenHelper) = Storage(context,openHelper)
 
     @Provides
+    @Singleton
     fun provideAttachmentProvider(@ApplicationContext context: Context, openHelper: SQLCipherOpenHelper): MessageDataProvider = DatabaseAttachmentProvider(context, openHelper)
 
 }
