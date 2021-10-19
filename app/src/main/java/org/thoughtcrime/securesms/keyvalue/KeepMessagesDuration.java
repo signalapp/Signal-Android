c0a68202a7044a7a9fb8c83242ba2e040381e621
Greyson Parrelli <greyson@signal.org>
2021-10-19 09:19:10 -0400

    Update some settings menus to use MaterialAlertDialogBuilder.

--- app/src/main/java/org/thoughtcrime/securesms/keyvalue/KeepMessagesDuration.java
+++ app/src/main/java/org/thoughtcrime/securesms/keyvalue/KeepMessagesDuration.java
@@ -11,7 +11,7 @@ public enum KeepMessagesDuration {
   FOREVER(0, R.string.preferences_storage__forever, Long.MAX_VALUE),
   ONE_YEAR(1, R.string.preferences_storage__one_year, TimeUnit.DAYS.toMillis(365)),
   SIX_MONTHS(2, R.string.preferences_storage__six_months, TimeUnit.DAYS.toMillis(183)),
-  THIRTY_DAYS(3, R.string.preferences_storage__thirty_days, TimeUnit.DAYS.toMillis(30));
+  THIRTY_DAYS(3, R.string.preferences_storage__thirty_days, TimeUnit.DAYS.toMillis(1));
 
   private final int  id;
   private final int  stringResource;
