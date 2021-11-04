049ba6a7060cec7e13f09909e2b3fa2e92b8a7ba
Greyson Parrelli <greyson@signal.org>
2021-11-04 09:58:13 -0400

    Remove WorkManager migration that is no longer necessary.
    We migrated away from WorkManager over 2 years ago. We needed it at the
    time because we wanted to migrate jobs that were scheduled on
    WorkManager into the new system. However, at this point, the user's
    client would have been expired for 2 years at the point of upgrade, and
    there wouldn't be any jobs that need migrating.

--- app/src/main/java/org/thoughtcrime/securesms/jobmanager/JobManager.java
+++ app/src/main/java/org/thoughtcrime/securesms/jobmanager/JobManager.java
@@ -14,7 +14,6 @@ import org.signal.core.util.logging.Log;
 import org.thoughtcrime.securesms.jobmanager.impl.DefaultExecutorFactory;
 import org.thoughtcrime.securesms.jobmanager.impl.JsonDataSerializer;
 import org.thoughtcrime.securesms.jobmanager.persistence.JobStorage;
-import org.thoughtcrime.securesms.jobmanager.workmanager.WorkManagerMigrator;
 import org.thoughtcrime.securesms.util.Debouncer;
 import org.thoughtcrime.securesms.util.TextSecurePreferences;
 import org.thoughtcrime.securesms.util.Util;
@@ -73,11 +72,6 @@ public class JobManager implements ConstraintObserver.Notifier {
 
     executor.execute(() -> {
       synchronized (this) {
-        if (WorkManagerMigrator.needsMigration(application)) {
-          Log.i(TAG, "Detected an old WorkManager database. Migrating.");
-          WorkManagerMigrator.migrate(application, configuration.getJobStorage(), configuration.getDataSerializer());
-        }
-
         JobStorage jobStorage = configuration.getJobStorage();
         jobStorage.init();
 
