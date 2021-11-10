41a5179bbf27f7b9511370b37f106a45f3a3f112
AsamK <asamk@gmx.de>
2021-11-10 17:37:03 +0100

    Close response body for two more unsuccessful requests

--- libsignal/service/src/main/java/org/whispersystems/signalservice/internal/push/PushServiceSocket.java
+++ libsignal/service/src/main/java/org/whispersystems/signalservice/internal/push/PushServiceSocket.java
@@ -521,22 +521,23 @@ public class PushServiceSocket {
   }
 
   public SignalServiceMessagesResult getMessages() throws IOException {
-    Response response = makeServiceRequest(String.format(MESSAGE_PATH, ""), "GET", (RequestBody) null, NO_HEADERS, NO_HANDLER, Optional.absent());
-    validateServiceResponse(response);
+    try (Response response = makeServiceRequest(String.format(MESSAGE_PATH, ""), "GET", (RequestBody) null, NO_HEADERS, NO_HANDLER, Optional.absent())) {
+      validateServiceResponse(response);
 
-    List<SignalServiceEnvelopeEntity> envelopes = readBodyJson(response.body(), SignalServiceEnvelopeEntityList.class).getMessages();
+      List<SignalServiceEnvelopeEntity> envelopes = readBodyJson(response.body(), SignalServiceEnvelopeEntityList.class).getMessages();
 
-    long serverDeliveredTimestamp = 0;
-    try {
-      String stringValue = response.header(SERVER_DELIVERED_TIMESTAMP_HEADER);
-      stringValue = stringValue != null ? stringValue : "0";
+      long serverDeliveredTimestamp = 0;
+      try {
+        String stringValue = response.header(SERVER_DELIVERED_TIMESTAMP_HEADER);
+        stringValue = stringValue != null ? stringValue : "0";
 
-      serverDeliveredTimestamp = Long.parseLong(stringValue);
-    } catch (NumberFormatException e) {
-      Log.w(TAG, e);
+        serverDeliveredTimestamp = Long.parseLong(stringValue);
+      } catch (NumberFormatException e) {
+        Log.w(TAG, e);
+      }
+
+      return new SignalServiceMessagesResult(envelopes, serverDeliveredTimestamp);
     }
-
-    return new SignalServiceMessagesResult(envelopes, serverDeliveredTimestamp);
   }
 
   public void acknowledgeMessage(String sender, long timestamp) throws IOException {
@@ -1616,7 +1617,8 @@ public class PushServiceSocket {
     call.enqueue(new Callback() {
       @Override
       public void onResponse(Call call, Response response) {
-        try (ResponseBody body = validateServiceResponse(response).body()) {
+        try (ResponseBody body = response.body()) {
+          validateServiceResponse(response);
           bodyFuture.set(readBodyString(body));
         } catch (IOException e) {
           bodyFuture.setException(e);
