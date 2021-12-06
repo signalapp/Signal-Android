b73bcdf774404b9b34c329aaa995329fe3c611e3
Fenil Kamdar <51909055+fenu28@users.noreply.github.com>
2021-12-06 11:02:21 +0530

    Update Android.mk

--- app/jni/Android.mk
+++ app/jni/Android.mk
@@ -4,7 +4,7 @@ include $(CLEAR_VARS)
 
 LOCAL_MODULE     := native-utils
 LOCAL_C_INCLUDES := $(JNI_DIR)/utils/
-LOCAL_CFLAGS     += -Wall
+
 
 LOCAL_SRC_FILES := $(JNI_DIR)/utils/org_thoughtcrime_securesms_util_FileUtils.cpp
 
