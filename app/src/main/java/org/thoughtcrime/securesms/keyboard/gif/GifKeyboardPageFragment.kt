3237703c259c2b2930b33e1a9770367669ebd9a8
Philipp Loose <phloose@mailbox.org>
2021-12-19 07:58:57 +0100

    Switch between giphy search and presets

--- app/src/main/java/org/thoughtcrime/securesms/keyboard/gif/GifKeyboardPageFragment.kt
+++ app/src/main/java/org/thoughtcrime/securesms/keyboard/gif/GifKeyboardPageFragment.kt
@@ -3,19 +3,19 @@ package org.thoughtcrime.securesms.keyboard.gif
 import android.net.Uri
 import android.os.Bundle
 import android.view.View
+import android.widget.LinearLayout
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.RecyclerView
 import org.thoughtcrime.securesms.LoggingFragment
 import org.thoughtcrime.securesms.R
-import org.thoughtcrime.securesms.conversation.ConversationActivity
 import org.thoughtcrime.securesms.giph.mp4.GiphyMp4Fragment
 import org.thoughtcrime.securesms.giph.mp4.GiphyMp4SaveResult
 import org.thoughtcrime.securesms.giph.mp4.GiphyMp4ViewModel
 import org.thoughtcrime.securesms.keyboard.emoji.KeyboardPageSearchView
 import org.thoughtcrime.securesms.keyboard.findListener
-import org.thoughtcrime.securesms.mms.AttachmentManager
+import org.thoughtcrime.securesms.util.ViewUtil
 import org.thoughtcrime.securesms.util.views.SimpleProgressDialog
 
 class GifKeyboardPageFragment : LoggingFragment(R.layout.gif_keyboard_page_fragment) {
@@ -29,6 +29,9 @@ class GifKeyboardPageFragment : LoggingFragment(R.layout.gif_keyboard_page_fragm
   private var progressDialog: AlertDialog? = null
   private lateinit var quickSearchList: RecyclerView
 
+  private lateinit var searchKeyboard: KeyboardPageSearchView
+  private lateinit var presetsBar: LinearLayout
+
   override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
     host = findListener<Host>() ?: throw AssertionError("Parent fragment or activity must implement Host")
 
@@ -36,14 +39,35 @@ class GifKeyboardPageFragment : LoggingFragment(R.layout.gif_keyboard_page_fragm
       .replace(R.id.gif_keyboard_giphy_frame, GiphyMp4Fragment.create(host.isMms()))
       .commitAllowingStateLoss()
 
-    val searchKeyboard: KeyboardPageSearchView = view.findViewById(R.id.gif_keyboard_search_text)
+    presetsBar = view.findViewById<LinearLayout>(R.id.gif_quick_search)
+
+    searchKeyboard = view.findViewById(R.id.gif_keyboard_search_text)
+    searchKeyboard.enableBackNavigation()
     searchKeyboard.callbacks = object : KeyboardPageSearchView.Callbacks {
       override fun onClicked() {
-        openGifSearch()
+        ViewUtil.focusAndShowKeyboard(searchKeyboard)
+      }
+
+      override fun onQueryChanged(query: String) {
+        giphyMp4ViewModel.updateSearchQuery(query)
+      }
+
+      override fun onFocusLost() {
+        ViewUtil.hideKeyboard(requireContext(), requireView())
+      }
+
+      override fun onNavigationClicked() {
+        ViewUtil.hideKeyboard(requireContext(), requireView())
+        switchBetweenSearchAndPresets()
+        searchKeyboard.clearQuery()
       }
     }
 
-    view.findViewById<View>(R.id.gif_keyboard_search).setOnClickListener { openGifSearch() }
+    view.findViewById<View>(R.id.gif_keyboard_search).setOnClickListener {
+      switchBetweenSearchAndPresets()
+      if(searchKeyboard.visibility === View.VISIBLE) ViewUtil.focusAndShowKeyboard(searchKeyboard)
+      if(presetsBar.visibility === View.VISIBLE) giphyMp4ViewModel.updateSearchQuery(viewModel.selectedTab.query)
+    }
 
     quickSearchList = view.findViewById(R.id.gif_keyboard_quick_search_recycler)
     quickSearchAdapter = GifQuickSearchAdapter(this::onQuickSearchSelected)
@@ -58,6 +82,20 @@ class GifKeyboardPageFragment : LoggingFragment(R.layout.gif_keyboard_page_fragm
     super.onActivityCreated(savedInstanceState)
     viewModel = ViewModelProvider(requireActivity()).get(GifKeyboardPageViewModel::class.java)
     updateQuickSearchTabs()
+  }
+
+  private fun switchBetweenSearchAndPresets() {
+    fun toggleVisibility(view: View) {
+      if(view.visibility === View.VISIBLE) {
+        view.visibility = View.INVISIBLE
+      } else {
+        view.visibility = View.VISIBLE
+      }
+    }
+    // searchKeyboard is invisible by default so we will never
+    // run into the case where both views are shown together
+    toggleVisibility(searchKeyboard)
+    toggleVisibility(presetsBar)
   }
 
   private fun onQuickSearchSelected(gifQuickSearchOption: GifQuickSearchOption) {
@@ -104,10 +142,6 @@ class GifKeyboardPageFragment : LoggingFragment(R.layout.gif_keyboard_page_fragm
 
   private fun handleGiphyMp4ErrorResult() {
     Toast.makeText(requireContext(), R.string.GiphyActivity_error_while_retrieving_full_resolution_gif, Toast.LENGTH_LONG).show()
-  }
-
-  private fun openGifSearch() {
-    AttachmentManager.selectGif(requireActivity(), ConversationActivity.PICK_GIF, host.isMms())
   }
 
   interface Host {
