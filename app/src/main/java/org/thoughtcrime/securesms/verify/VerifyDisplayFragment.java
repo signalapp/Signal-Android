e235ec4129b16c26e8941655764faec770aae338
bim <dsnake@protonmail.com>
2022-02-04 11:00:44 -0500

    Fix bug in name rendering on verify screen.
    Fixes #11770

--- app/src/main/java/org/thoughtcrime/securesms/verify/VerifyDisplayFragment.java
+++ app/src/main/java/org/thoughtcrime/securesms/verify/VerifyDisplayFragment.java
@@ -394,7 +394,9 @@ public class VerifyDisplayFragment extends Fragment implements ViewTreeObserver.
   }
 
   private void setRecipientText(Recipient recipient) {
-    description.setText(Html.fromHtml(String.format(getActivity().getString(R.string.verify_display_fragment__to_verify_the_security_of_your_end_to_end_encryption_with_s), recipient.getDisplayName(getContext()))));
+    String escapedDisplayName = Html.escapeHtml(recipient.getDisplayName(getContext()));
+
+    description.setText(Html.fromHtml(String.format(getActivity().getString(R.string.verify_display_fragment__to_verify_the_security_of_your_end_to_end_encryption_with_s), escapedDisplayName)));
     description.setMovementMethod(LinkMovementMethod.getInstance());
   }
 
