3fbc5423e5217ea8901458f2cf6d0b425b82536f
Cody Henthorne <cody@signal.org>
2022-03-03 09:57:04 -0500

    Use jumbo emoji in reaction pickers.

--- app/src/main/java/org/thoughtcrime/securesms/components/emoji/EmojiImageView.java
+++ app/src/main/java/org/thoughtcrime/securesms/components/emoji/EmojiImageView.java
@@ -1,26 +1,39 @@
 package org.thoughtcrime.securesms.components.emoji;
 
 import android.content.Context;
+import android.content.res.TypedArray;
 import android.util.AttributeSet;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.widget.AppCompatImageView;
 
 import org.thoughtcrime.securesms.R;
 
 public class EmojiImageView extends AppCompatImageView {
+
+  private final boolean forceJumboEmoji;
+
   public EmojiImageView(Context context) {
-    super(context);
+    this(context, null);
   }
 
   public EmojiImageView(Context context, AttributeSet attrs) {
-    super(context, attrs);
+    this(context, attrs, 0);
+  }
+
+  public EmojiImageView(@NonNull Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+    super(context, attrs, defStyleAttr);
+    TypedArray a = context.getTheme().obtainStyledAttributes(attrs, R.styleable.EmojiImageView, 0, 0);
+    forceJumboEmoji = a.getBoolean(R.styleable.EmojiImageView_forceJumbo, false);
+    a.recycle();
   }
 
   public void setImageEmoji(CharSequence emoji) {
     if (isInEditMode()) {
       setImageResource(R.drawable.ic_emoji);
     } else {
-      setImageDrawable(EmojiProvider.getEmojiDrawable(getContext(), emoji));
+      setImageDrawable(EmojiProvider.getEmojiDrawable(getContext(), emoji, forceJumboEmoji));
     }
   }
 }
