9d9e6e2972bde40d6d430799dbe47a50baa0d7f8
Greyson Parrelli <greyson@signal.org>
2022-03-03 09:40:55 -0500

    Ensure inner html is escaped when bolding.
    Fixes #12033

--- app/src/main/java/org/thoughtcrime/securesms/util/HtmlUtil.java
+++ app/src/main/java/org/thoughtcrime/securesms/util/HtmlUtil.java
@@ -1,9 +1,11 @@
 package org.thoughtcrime.securesms.util;
 
+import android.text.Html;
+
 import androidx.annotation.NonNull;
 
 public class HtmlUtil {
   public static @NonNull String bold(@NonNull String target) {
-    return "<b>" + target + "</b>";
+    return "<b>" + Html.escapeHtml(target) + "</b>";
   }
 }
