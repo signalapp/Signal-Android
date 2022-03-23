2b3f16d3ad97f1ede780659f5d5dafee1e38417e
Alex Hart <alex@signal.org>
2022-03-23 12:47:26 -0300

    Material3 Bottom Bar styling and fab for Stories only.

--- app/src/main/java/org/thoughtcrime/securesms/MainActivity.java
+++ app/src/main/java/org/thoughtcrime/securesms/MainActivity.java
@@ -118,7 +118,7 @@ public class MainActivity extends PassphraseRequiredActivity implements VoiceNot
   private void updateTabVisibility() {
     if (Stories.isFeatureEnabled()) {
       findViewById(R.id.conversation_list_tabs).setVisibility(View.VISIBLE);
-      WindowUtil.setNavigationBarColor(getWindow(), ContextCompat.getColor(this, R.color.signal_background_secondary));
+      WindowUtil.setNavigationBarColor(getWindow(), ContextCompat.getColor(this, R.color.signal_colorSecondaryContainer));
     } else {
       findViewById(R.id.conversation_list_tabs).setVisibility(View.GONE);
       WindowUtil.setNavigationBarColor(getWindow(), ContextCompat.getColor(this, R.color.signal_background_primary));
