cf4c67fd1a818dd1bb887094b1714c87a96c28b9
b <dsnake@protonmail.com>
2022-04-04 11:08:37 -0400

    Fixes #8058 makes sure pasted text doesn't have formatting

--- app/src/main/java/org/thoughtcrime/securesms/components/emoji/EmojiEditText.java
+++ app/src/main/java/org/thoughtcrime/securesms/components/emoji/EmojiEditText.java
@@ -1,9 +1,13 @@
 package org.thoughtcrime.securesms.components.emoji;
 
+import android.content.ClipData;
+import android.content.ClipboardManager;
 import android.content.Context;
 import android.content.res.TypedArray;
 import android.graphics.drawable.Drawable;
+import android.os.Build;
 import android.text.InputFilter;
+import android.text.Spanned;
 import android.util.AttributeSet;
 
 import androidx.annotation.NonNull;
@@ -68,4 +72,30 @@ public class EmojiEditText extends AppCompatEditText {
 
     return result;
   }
+
+  // clears formatting from pasted text 
+  @Override
+  public boolean onTextContextMenuItem(int id) {
+    if (id == android.R.id.paste) {
+      if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M)
+        return super.onTextContextMenuItem(android.R.id.pasteAsPlainText);
+      ClipboardManager cm               = ((ClipboardManager) getContext().getSystemService(Context.CLIPBOARD_SERVICE));
+      ClipData         originalClipData = cm.getPrimaryClip();
+      CharSequence     textToPaste      = getTextFromClipData(originalClipData);
+      if (textToPaste == null) return super.onTextContextMenuItem(id);
+      CharSequence sanitizedText = (textToPaste instanceof Spanned) ? textToPaste.toString() : textToPaste;
+      cm.setPrimaryClip(ClipData.newPlainText("signal_sanitized", sanitizedText));
+      boolean retVal = super.onTextContextMenuItem(id);
+      cm.setPrimaryClip(originalClipData);
+      return retVal;
+    }
+    return super.onTextContextMenuItem(id);
+  }
+
+  private CharSequence getTextFromClipData(ClipData data) {
+    if (data == null) return null;
+    if (data.getItemCount() == 0) return null;
+    return data.getItemAt(0).coerceToText(getContext());
+  }
+
 }
