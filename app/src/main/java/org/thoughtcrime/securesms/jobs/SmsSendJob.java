0d9a5ef9a669f4437d370496773d39616106960f
Sgn-32 <49990901+Sgn-32@users.noreply.github.com>
2022-04-23 22:38:23 +0200

    Fix SMS delivery reports

--- app/src/main/java/org/thoughtcrime/securesms/jobs/SmsSendJob.java
+++ app/src/main/java/org/thoughtcrime/securesms/jobs/SmsSendJob.java
@@ -19,6 +19,7 @@ import org.thoughtcrime.securesms.dependencies.ApplicationDependencies;
 import org.thoughtcrime.securesms.jobmanager.Data;
 import org.thoughtcrime.securesms.jobmanager.Job;
 import org.thoughtcrime.securesms.jobmanager.impl.NetworkOrCellServiceConstraint;
+import org.thoughtcrime.securesms.keyvalue.SignalStore;
 import org.thoughtcrime.securesms.phonenumbers.NumberUtil;
 import org.thoughtcrime.securesms.recipients.Recipient;
 import org.thoughtcrime.securesms.service.SmsDeliveryListener;
@@ -188,7 +189,7 @@ public class SmsSendJob extends SendJob {
   }
 
   private ArrayList<PendingIntent> constructDeliveredIntents(long messageId, long type, ArrayList<String> messages) {
-    if (!TextSecurePreferences.isSmsDeliveryReportsEnabled(context)) {
+    if (!SignalStore.settings().isSmsDeliveryReportsEnabled()) {
       return null;
     }
 
