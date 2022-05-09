df88c2fd144749bb3cbbea2c4b0c94765e111cfb
Alex Hart <alex@signal.org>
2022-05-09 14:38:26 -0300

    Update ViewModel file template to use RxStore.

--- .idea/fileTemplates/ViewModel.kt
+++ .idea/fileTemplates/ViewModel.kt
@@ -1,18 +1,18 @@
 #if (${PACKAGE_NAME} && ${PACKAGE_NAME} != "")package ${PACKAGE_NAME}
 
-import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
-import org.thoughtcrime.securesms.util.livedata.Store
+import io.reactivex.rxjava3.core.Flowable
 import io.reactivex.rxjava3.disposables.CompositeDisposable
+import org.thoughtcrime.securesms.util.rx.RxStore
 
 #end
 #parse("File Header.java")
 class ${NAME}ViewModel : ViewModel() {
 
-  private val store = Store(${NAME}State())
+  private val store = RxStore(${NAME}State())
   private val disposables = CompositeDisposable()
   
-  val state: LiveData<${NAME}State> = store.stateLiveData
+  val state: Flowable<${NAME}State> = store.stateFlowable
 
   override fun onCleared() {
     disposables.clear()
