a1025a8e9ab18a1d1e4081ebf4eb08448c9612a2
Alex Hart <alex@signal.org>
2022-05-16 12:49:17 -0300

    Add expiry information to gift conversation items.

--- app/src/main/java/org/thoughtcrime/securesms/badges/gifts/Gifts.kt
+++ app/src/main/java/org/thoughtcrime/securesms/badges/gifts/Gifts.kt
@@ -1,5 +1,9 @@
 package org.thoughtcrime.securesms.badges.gifts
 
+import android.content.Context
+import org.signal.libsignal.zkgroup.InvalidInputException
+import org.signal.libsignal.zkgroup.receipts.ReceiptCredentialPresentation
+import org.thoughtcrime.securesms.R
 import org.thoughtcrime.securesms.database.ThreadDatabase
 import org.thoughtcrime.securesms.database.model.StoryType
 import org.thoughtcrime.securesms.database.model.databaseprotos.GiftBadge
@@ -7,6 +11,8 @@ import org.thoughtcrime.securesms.mms.OutgoingMediaMessage
 import org.thoughtcrime.securesms.mms.OutgoingSecureMediaMessage
 import org.thoughtcrime.securesms.recipients.Recipient
 import org.thoughtcrime.securesms.util.Base64
+import java.lang.Integer.min
+import java.util.concurrent.TimeUnit
 
 /**
  * Helper object for Gift badges
@@ -44,5 +50,37 @@ object Gifts {
       listOf(),
       giftBadge
     )
+  }
+
+  /**
+   * @return the expiration time from the redemption token, in UNIX epoch seconds.
+   */
+  private fun GiftBadge.getExpiry(): Long {
+    return try {
+      ReceiptCredentialPresentation(redemptionToken.toByteArray()).receiptExpirationTime
+    } catch (e: InvalidInputException) {
+      return 0L
+    }
+  }
+
+  fun GiftBadge.formatExpiry(context: Context): String {
+    val expiry = getExpiry()
+    val timeRemaining = TimeUnit.SECONDS.toMillis(expiry) - System.currentTimeMillis()
+    if (timeRemaining <= 0) {
+      return context.getString(R.string.Gifts__expired)
+    }
+
+    val days = TimeUnit.MILLISECONDS.toDays(timeRemaining).toInt()
+    if (days > 0) {
+      return context.resources.getQuantityString(R.plurals.Gifts__d_days_remaining, days, days)
+    }
+
+    val hours = TimeUnit.MILLISECONDS.toHours(timeRemaining).toInt()
+    if (hours > 0) {
+      return context.resources.getQuantityString(R.plurals.Gifts__d_hours_remaining, hours, hours)
+    }
+
+    val minutes = min(1, TimeUnit.MILLISECONDS.toMinutes(timeRemaining).toInt())
+    return context.resources.getQuantityString(R.plurals.Gifts__d_minutes_remaining, minutes, minutes)
   }
 }
