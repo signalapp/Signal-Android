e113973358d9fa7f4050a49d7200f9bd38f9b463
Cody Henthorne <cody@signal.org>
2022-07-21 12:13:09 -0400

    Fix decline code infinite loop.

--- donations/lib/src/main/java/org/signal/donations/StripeDeclineCode.kt
+++ donations/lib/src/main/java/org/signal/donations/StripeDeclineCode.kt
@@ -8,7 +8,7 @@ sealed class StripeDeclineCode {
   data class Known(val code: Code) : StripeDeclineCode()
   data class Unknown(val code: String) : StripeDeclineCode()
 
-  fun isKnown(): Boolean = this.isKnown()
+  fun isKnown(): Boolean = this is Known
 
   enum class Code(val code: String) {
     AUTHENTICATION_REQUIRED("authentication_required"),
