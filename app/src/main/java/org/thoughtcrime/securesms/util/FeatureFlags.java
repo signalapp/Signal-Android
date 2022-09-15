75af1b69e89ea376aa98df39e213ab6340ffd8a9
Alex Hart <alex@signal.org>
2022-09-15 11:05:07 -0300

    Update payment toolbars to match M3 specification.

--- app/src/main/java/org/thoughtcrime/securesms/util/FeatureFlags.java
+++ app/src/main/java/org/thoughtcrime/securesms/util/FeatureFlags.java
@@ -178,7 +178,6 @@ public final class FeatureFlags {
   @SuppressWarnings("MismatchedQueryAndUpdateOfCollection")
   @VisibleForTesting
   static final Map<String, Object> FORCED_VALUES = new HashMap<String, Object>() {{
-    put(USERNAMES, true);
   }};
 
   /**
