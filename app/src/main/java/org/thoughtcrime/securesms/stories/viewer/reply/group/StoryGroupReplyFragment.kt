02db5f74e982fd8a793561e92d5e3df02b132a4e
Alex Hart <alex@signal.org>
2022-10-07 15:38:49 -0300

    Allow non-default emoji to animate in group replies.

--- app/src/main/java/org/thoughtcrime/securesms/stories/viewer/reply/group/StoryGroupReplyFragment.kt
+++ app/src/main/java/org/thoughtcrime/securesms/stories/viewer/reply/group/StoryGroupReplyFragment.kt
@@ -358,7 +358,6 @@ class StoryGroupReplyFragment :
 
           override fun onReactionSelected(emoji: String) {
             dialog.dismiss()
-            findListener<Callback>()?.onReactionEmojiSelected(emoji)
             sendReaction(emoji)
           }
 
@@ -377,6 +376,8 @@ class StoryGroupReplyFragment :
   }
 
   private fun sendReaction(emoji: String) {
+    findListener<Callback>()?.onReactionEmojiSelected(emoji)
+
     lifecycleDisposable += StoryGroupReplySender.sendReaction(requireContext(), storyId, emoji)
       .observeOn(AndroidSchedulers.mainThread())
       .subscribeBy(
