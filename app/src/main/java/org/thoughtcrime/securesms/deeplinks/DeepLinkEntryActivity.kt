17eee2b45fc41f9ae6f44e0ce900991d0209dbbc
Aditya <adisingh925@gmail.com>
2022-10-10 14:03:04 +0530

    Converted DeepLinkEntryActivity.java file to DeepLinkEntryActivity.kt file

--- app/src/main/java/org/thoughtcrime/securesms/deeplinks/DeepLinkEntryActivity.kt
+++ app/src/main/java/org/thoughtcrime/securesms/deeplinks/DeepLinkEntryActivity.kt
@@ -1,18 +1,14 @@
-package org.thoughtcrime.securesms.deeplinks;
+package org.thoughtcrime.securesms.deeplinks
 
-import android.content.Intent;
-import android.net.Uri;
-import android.os.Bundle;
+import android.os.Bundle
+import org.thoughtcrime.securesms.MainActivity
+import org.thoughtcrime.securesms.PassphraseRequiredActivity
 
-import org.thoughtcrime.securesms.MainActivity;
-import org.thoughtcrime.securesms.PassphraseRequiredActivity;
-
-public class DeepLinkEntryActivity extends PassphraseRequiredActivity {
-  @Override
-  protected void onCreate(Bundle savedInstanceState, boolean ready) {
-    Intent intent = MainActivity.clearTop(this);
-    Uri    data   = getIntent().getData();
-    intent.setData(data);
-    startActivity(intent);
-  }
+class DeepLinkEntryActivity : PassphraseRequiredActivity() {
+    override fun onCreate(savedInstanceState: Bundle, ready: Boolean) {
+        val intent = MainActivity.clearTop(this)
+        val data = getIntent().data
+        intent.data = data
+        startActivity(intent)
+    }
 }
