3d14c05114a532d1b6fc50247d31e1bb32b9fc0c
Nicholas <nicholas@signal.org>
2022-10-12 16:54:11 -0400

    Surround phone numbers with LTR unicode mark.
    This also removes the previous TextView LTR flag that I had initially added for a one-off.

--- app/src/main/java/org/thoughtcrime/securesms/phonenumbers/PhoneNumberFormatter.java
+++ app/src/main/java/org/thoughtcrime/securesms/phonenumbers/PhoneNumberFormatter.java
@@ -79,7 +79,7 @@ public class PhoneNumberFormatter {
   }
 
   public static @NonNull String prettyPrint(@NonNull String e164) {
-    return get(ApplicationDependencies.getApplication()).prettyPrintFormat(e164);
+    return StringUtil.forceLtr(get(ApplicationDependencies.getApplication()).prettyPrintFormat(e164));
   }
 
   public @NonNull String prettyPrintFormat(@NonNull String e164) {
--- core-util/src/main/java/org/signal/core/util/StringUtil.java
+++ core-util/src/main/java/org/signal/core/util/StringUtil.java
@@ -322,4 +322,8 @@ public final class StringUtil {
     iterator.setText(text);
     return iterator.countBreaks();
   }
+
+  public static String forceLtr(@NonNull CharSequence text) {
+    return "\u202a" + text + "\u202c";
+  }
 }
