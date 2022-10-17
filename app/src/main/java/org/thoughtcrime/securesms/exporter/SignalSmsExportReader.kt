1006af7d8a24c015fe074c9b4a183d8e79639893
Cody Henthorne <cody@signal.org>
2022-10-17 09:54:08 -0400

    Fix sms export crash with missing thread recipients.

--- app/src/main/java/org/thoughtcrime/securesms/exporter/SignalSmsExportReader.kt
+++ app/src/main/java/org/thoughtcrime/securesms/exporter/SignalSmsExportReader.kt
@@ -132,14 +132,16 @@ class SignalSmsExportReader(
     }
 
     private fun readExportableMmsMessageFromRecord(record: MessageRecord, exportState: MessageExportState): ExportableMessage {
-      val threadRecipient: Recipient = SignalDatabase.threads.getRecipientForThreadId(record.threadId)!!
-      val addresses: Set<String> = if (threadRecipient.isMmsGroup) {
+      val threadRecipient: Recipient? = SignalDatabase.threads.getRecipientForThreadId(record.threadId)
+      val addresses: Set<String> = if (threadRecipient?.isMmsGroup == true) {
         Recipient
           .resolvedList(threadRecipient.participantIds)
           .map { r -> r.smsExportAddress() }
           .toSet()
-      } else {
+      } else if (threadRecipient != null) {
         setOf(threadRecipient.smsExportAddress())
+      } else {
+        setOf(record.individualRecipient.smsExportAddress())
       }
 
       val parts: MutableList<ExportableMessage.Mms.Part> = mutableListOf()
