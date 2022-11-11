8cd0184962eeef923746df81eef6895b9aeaf349
Alex Hart <alex@signal.org>
2022-11-11 13:57:24 -0400

    Add default values to global duration scale resolution.

--- app/src/main/java/org/thoughtcrime/securesms/util/ContentResolverExtensions.kt
+++ app/src/main/java/org/thoughtcrime/securesms/util/ContentResolverExtensions.kt
@@ -9,8 +9,8 @@ fun ContentResolver.areSystemAnimationsDisabled(): Boolean {
   return if (Build.VERSION.SDK_INT >= 26) {
     !ValueAnimator.areAnimatorsEnabled()
   } else {
-    val durationScale = Settings.System.getFloat(this, Settings.Global.ANIMATOR_DURATION_SCALE)
-    val transitionScale = Settings.System.getFloat(this, Settings.Global.TRANSITION_ANIMATION_SCALE)
+    val durationScale = Settings.Global.getFloat(this, Settings.Global.ANIMATOR_DURATION_SCALE, 1.0f)
+    val transitionScale = Settings.Global.getFloat(this, Settings.Global.TRANSITION_ANIMATION_SCALE, 1.0f)
 
     !(durationScale > 0f && transitionScale > 0f)
   }
