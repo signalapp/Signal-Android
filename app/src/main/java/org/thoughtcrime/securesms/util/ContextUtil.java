e3954ab5e863f694008145c76729c0bb219a1724
Alex Hart <alex@signal.org>
2022-11-14 10:49:55 -0400

    Utilize logic from lottie to determine animation scale.

--- app/src/main/java/org/thoughtcrime/securesms/util/ContextUtil.java
+++ app/src/main/java/org/thoughtcrime/securesms/util/ContextUtil.java
@@ -2,6 +2,7 @@ package org.thoughtcrime.securesms.util;
 
 import android.content.Context;
 import android.graphics.drawable.Drawable;
+import android.provider.Settings;
 
 import androidx.annotation.DrawableRes;
 import androidx.annotation.NonNull;
@@ -16,4 +17,10 @@ public final class ContextUtil {
     return Objects.requireNonNull(AppCompatResources.getDrawable(context, drawable));
   }
 
+  /**
+   * Implementation "borrowed" from com.airbnb.lottie.utils.Utils#getAnimationScale(android.content.Context)
+   */
+  public static float getAnimationScale(Context context) {
+    return Settings.Global.getFloat(context.getContentResolver(), Settings.Global.ANIMATOR_DURATION_SCALE, 1.0f);
+  }
 }
