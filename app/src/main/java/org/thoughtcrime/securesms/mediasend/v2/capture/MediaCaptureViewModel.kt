4a5a65ff6c7a2417f439d2085bc9110bd3d98099
Alex Hart <alex@signal.org>
2022-12-09 13:30:45 -0400

    Remove usage of SingleLiveEvent from MediaCaptureViewModel.

--- app/src/main/java/org/thoughtcrime/securesms/mediasend/v2/capture/MediaCaptureViewModel.kt
+++ app/src/main/java/org/thoughtcrime/securesms/mediasend/v2/capture/MediaCaptureViewModel.kt
@@ -1,11 +1,13 @@
 package org.thoughtcrime.securesms.mediasend.v2.capture
 
-import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
 import io.reactivex.rxjava3.core.Flowable
+import io.reactivex.rxjava3.core.Observable
+import io.reactivex.rxjava3.subjects.PublishSubject
+import io.reactivex.rxjava3.subjects.Subject
 import org.thoughtcrime.securesms.mediasend.Media
-import org.thoughtcrime.securesms.util.SingleLiveEvent
 import org.thoughtcrime.securesms.util.rx.RxStore
 import java.io.FileDescriptor
 import java.util.Optional
@@ -14,9 +16,9 @@ class MediaCaptureViewModel(private val repository: MediaCaptureRepository) : Vi
 
   private val store: RxStore<MediaCaptureState> = RxStore(MediaCaptureState())
 
-  private val internalEvents: SingleLiveEvent<MediaCaptureEvent> = SingleLiveEvent()
+  private val internalEvents: Subject<MediaCaptureEvent> = PublishSubject.create()
 
-  val events: LiveData<MediaCaptureEvent> = internalEvents
+  val events: Observable<MediaCaptureEvent> = internalEvents.observeOn(AndroidSchedulers.mainThread())
 
   init {
     repository.getMostRecentItem { media ->
@@ -43,11 +45,11 @@ class MediaCaptureViewModel(private val repository: MediaCaptureRepository) : Vi
   }
 
   private fun onMediaRendered(media: Media) {
-    internalEvents.postValue(MediaCaptureEvent.MediaCaptureRendered(media))
+    internalEvents.onNext(MediaCaptureEvent.MediaCaptureRendered(media))
   }
 
   private fun onMediaRenderFailed() {
-    internalEvents.postValue(MediaCaptureEvent.MediaCaptureRenderFailed)
+    internalEvents.onNext(MediaCaptureEvent.MediaCaptureRenderFailed)
   }
 
   class Factory(private val repository: MediaCaptureRepository) : ViewModelProvider.Factory {
