498b7fee691122015efb6134ae102031311cfdcb
Alex Hart <alex@signal.org>
2022-12-13 12:52:04 -0400

    Remove SingleLiveEvent from EditAboutViewModel.

--- app/src/main/java/org/thoughtcrime/securesms/profiles/manage/EditAboutViewModel.java
+++ app/src/main/java/org/thoughtcrime/securesms/profiles/manage/EditAboutViewModel.java
@@ -3,42 +3,43 @@ package org.thoughtcrime.securesms.profiles.manage;
 import android.content.Context;
 
 import androidx.annotation.NonNull;
-import androidx.lifecycle.LiveData;
-import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
-import org.thoughtcrime.securesms.util.SingleLiveEvent;
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers;
+import io.reactivex.rxjava3.core.Observable;
+import io.reactivex.rxjava3.subjects.BehaviorSubject;
+import io.reactivex.rxjava3.subjects.PublishSubject;
 
 public final class EditAboutViewModel extends ViewModel {
 
   private final ManageProfileRepository    repository;
-  private final MutableLiveData<SaveState> saveState;
-  private final SingleLiveEvent<Event>     events;
+  private final BehaviorSubject<SaveState> saveState;
+  private final PublishSubject<Event>      events;
 
   public EditAboutViewModel() {
     this.repository = new ManageProfileRepository();
-    this.saveState  = new MutableLiveData<>(SaveState.IDLE);
-    this.events     = new SingleLiveEvent<>();
+    this.saveState  = BehaviorSubject.createDefault(SaveState.IDLE);
+    this.events     = PublishSubject.create();
   }
 
-  @NonNull LiveData<SaveState> getSaveState() {
-    return saveState;
+  @NonNull Observable<SaveState> getSaveState() {
+    return saveState.observeOn(AndroidSchedulers.mainThread());
   }
 
-  @NonNull LiveData<Event> getEvents() {
-    return events;
+  @NonNull Observable<Event> getEvents() {
+    return events.observeOn(AndroidSchedulers.mainThread());
   }
 
   void onSaveClicked(@NonNull Context context, @NonNull String about, @NonNull String emoji) {
-    saveState.setValue(SaveState.IN_PROGRESS);
+    saveState.onNext(SaveState.IN_PROGRESS);
     repository.setAbout(context, about, emoji, result -> {
       switch (result) {
         case SUCCESS:
-          saveState.postValue(SaveState.DONE);
+          saveState.onNext(SaveState.DONE);
           break;
         case FAILURE_NETWORK:
-          saveState.postValue(SaveState.IDLE);
-          events.postValue(Event.NETWORK_FAILURE);
+          saveState.onNext(SaveState.IDLE);
+          events.onNext(Event.NETWORK_FAILURE);
           break;
       }
     });
