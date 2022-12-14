a1c1162176598b2de4b88e190d3ca623af4057af
Hiroyuki Okuyama <h.okuyama@punkt.ch>
2022-12-14 18:04:44 +0100

    Revert "Bump version to Pigeon v1.2.0797"
    This reverts commit e07583dac746766afd4649ac5aa8cebab198203d.

--- app/src/main/java/org/thoughtcrime/securesms/migrations/ApplicationMigrations.java
+++ app/src/main/java/org/thoughtcrime/securesms/migrations/ApplicationMigrations.java
@@ -43,45 +43,45 @@ public class ApplicationMigrations {
 
   @VisibleForTesting
   static final class Version {
-    static final int LEGACY                        = 1;
-    static final int RECIPIENT_ID                  = 2;
-    static final int RECIPIENT_SEARCH              = 3;
-    static final int RECIPIENT_CLEANUP             = 4;
-    static final int AVATAR_MIGRATION              = 5;
-    static final int UUIDS                         = 6;
-    static final int CACHED_ATTACHMENTS            = 7;
-    static final int STICKERS_LAUNCH               = 8;
+    static final int LEGACY                 = 1;
+    static final int RECIPIENT_ID           = 2;
+    static final int RECIPIENT_SEARCH       = 3;
+    static final int RECIPIENT_CLEANUP      = 4;
+    static final int AVATAR_MIGRATION       = 5;
+    static final int UUIDS                  = 6;
+    static final int CACHED_ATTACHMENTS     = 7;
+    static final int STICKERS_LAUNCH        = 8;
     //static final int TEST_ARGON2          = 9;
-    static final int SWOON_STICKERS                = 10;
-    static final int STORAGE_SERVICE               = 11;
+    static final int SWOON_STICKERS         = 10;
+    static final int STORAGE_SERVICE        = 11;
     //static final int STORAGE_KEY_ROTATE   = 12;
-    static final int REMOVE_AVATAR_ID              = 13;
-    static final int STORAGE_CAPABILITY            = 14;
-    static final int PIN_REMINDER                  = 15;
-    static final int VERSIONED_PROFILE             = 16;
-    static final int PIN_OPT_OUT                   = 17;
-    static final int TRIM_SETTINGS                 = 18;
-    static final int THUMBNAIL_CLEANUP             = 19;
-    static final int GV2                           = 20;
-    static final int GV2_2                         = 21;
-    static final int CDS                           = 22;
-    static final int BACKUP_NOTIFICATION           = 23;
-    static final int GV1_MIGRATION                 = 24;
-    static final int USER_NOTIFICATION             = 25;
-    static final int DAY_BY_DAY_STICKERS           = 26;
-    static final int BLOB_LOCATION                 = 27;
-    static final int SYSTEM_NAME_SPLIT             = 28;
+    static final int REMOVE_AVATAR_ID       = 13;
+    static final int STORAGE_CAPABILITY     = 14;
+    static final int PIN_REMINDER           = 15;
+    static final int VERSIONED_PROFILE      = 16;
+    static final int PIN_OPT_OUT            = 17;
+    static final int TRIM_SETTINGS          = 18;
+    static final int THUMBNAIL_CLEANUP      = 19;
+    static final int GV2                    = 20;
+    static final int GV2_2                  = 21;
+    static final int CDS                    = 22;
+    static final int BACKUP_NOTIFICATION    = 23;
+    static final int GV1_MIGRATION          = 24;
+    static final int USER_NOTIFICATION      = 25;
+    static final int DAY_BY_DAY_STICKERS    = 26;
+    static final int BLOB_LOCATION          = 27;
+    static final int SYSTEM_NAME_SPLIT      = 28;
     // Versions 29, 30 accidentally skipped
-    static final int MUTE_SYNC                     = 31;
-    static final int PROFILE_SHARING_UPDATE        = 32;
-    static final int SMS_STORAGE_SYNC              = 33;
-    static final int APPLY_UNIVERSAL_EXPIRE        = 34;
-    static final int SENDER_KEY                    = 35;
-    static final int SENDER_KEY_2                  = 36;
-    static final int DB_AUTOINCREMENT              = 37;
-    static final int ATTACHMENT_CLEANUP            = 38;
-    static final int LOG_CLEANUP                   = 39;
-    static final int ATTACHMENT_CLEANUP_2          = 40;
+    static final int MUTE_SYNC              = 31;
+    static final int PROFILE_SHARING_UPDATE = 32;
+    static final int SMS_STORAGE_SYNC       = 33;
+    static final int APPLY_UNIVERSAL_EXPIRE = 34;
+    static final int SENDER_KEY             = 35;
+    static final int SENDER_KEY_2           = 36;
+    static final int DB_AUTOINCREMENT       = 37;
+    static final int ATTACHMENT_CLEANUP     = 38;
+    static final int LOG_CLEANUP            = 39;
+    static final int ATTACHMENT_CLEANUP_2   = 40;
     static final int ANNOUNCEMENT_GROUP_CAPABILITY = 41;
     static final int STICKER_MY_DAILY_LIFE         = 42;
     static final int SENDER_KEY_3                  = 43;
@@ -330,13 +330,12 @@ public class ApplicationMigrations {
     if (lastSeenVersion < Version.PROFILE_SHARING_UPDATE) {
       jobs.put(Version.PROFILE_SHARING_UPDATE, new ProfileSharingUpdateMigrationJob());
     }
-
     if (lastSeenVersion < Version.SMS_STORAGE_SYNC) {
       jobs.put(Version.SMS_STORAGE_SYNC, new AccountRecordMigrationJob());
     }
 
     if (lastSeenVersion < Version.APPLY_UNIVERSAL_EXPIRE) {
-      jobs.put(Version.APPLY_UNIVERSAL_EXPIRE, new ApplyUnknownFieldsToSelfMigrationJob());
+      jobs.put(Version.SMS_STORAGE_SYNC, new ApplyUnknownFieldsToSelfMigrationJob());
     }
 
     if (lastSeenVersion < Version.SENDER_KEY) {
