0e08b4ee269ac84ba6e6e873ed747177dc91d2a4
Nicholas <nicholas@signal.org>
2022-12-14 13:18:11 -0500

    Correctly animate deletion when attaching multiple media.

--- app/src/main/java/org/thoughtcrime/securesms/mediasend/v2/review/MediaReviewFragment.kt
+++ app/src/main/java/org/thoughtcrime/securesms/mediasend/v2/review/MediaReviewFragment.kt
@@ -238,7 +238,7 @@ class MediaReviewFragment : Fragment(R.layout.v2_media_review_fragment) {
       }
     })
 
-    val selectionAdapter = MappingAdapter()
+    val selectionAdapter = MappingAdapter(false)
     MediaReviewAddItem.register(selectionAdapter) {
       launchGallery()
     }
