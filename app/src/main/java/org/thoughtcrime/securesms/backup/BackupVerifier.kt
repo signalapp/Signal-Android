89b0167fd2d8b69a7b293e911743963608ec1040
Cody Henthorne <cody@signal.org>
2023-02-07 11:19:26 -0500

    Ensure backup job verification can be cancelled.

--- app/src/main/java/org/thoughtcrime/securesms/backup/BackupVerifier.kt
+++ app/src/main/java/org/thoughtcrime/securesms/backup/BackupVerifier.kt
@@ -15,31 +15,35 @@ object BackupVerifier {
   private val TAG = Log.tag(BackupVerifier::class.java)
 
   @JvmStatic
-  @Throws(IOException::class)
-  fun verifyFile(cipherStream: InputStream, passphrase: String, expectedCount: Long): Boolean {
+  @Throws(IOException::class, FullBackupExporter.BackupCanceledException::class)
+  fun verifyFile(cipherStream: InputStream, passphrase: String, expectedCount: Long, cancellationSignal: FullBackupExporter.BackupCancellationSignal): Boolean {
     val inputStream = BackupRecordInputStream(cipherStream, passphrase)
 
     var count = 0L
     var frame: BackupFrame = inputStream.readFrame()
 
-    while (!frame.end) {
-      val verified = when {
-        frame.hasAttachment() -> verifyAttachment(frame.attachment, inputStream)
-        frame.hasSticker() -> verifySticker(frame.sticker, inputStream)
-        frame.hasAvatar() -> verifyAvatar(frame.avatar, inputStream)
-        else -> true
+    cipherStream.use {
+      while (!frame.end && !cancellationSignal.isCanceled) {
+        val verified = when {
+          frame.hasAttachment() -> verifyAttachment(frame.attachment, inputStream)
+          frame.hasSticker() -> verifySticker(frame.sticker, inputStream)
+          frame.hasAvatar() -> verifyAvatar(frame.avatar, inputStream)
+          else -> true
+        }
+
+        if (!verified) {
+          return false
+        }
+
+        EventBus.getDefault().post(BackupEvent(BackupEvent.Type.PROGRESS_VERIFYING, ++count, expectedCount))
+
+        frame = inputStream.readFrame()
       }
-
-      if (!verified) {
-        return false
-      }
-
-      EventBus.getDefault().post(BackupEvent(BackupEvent.Type.PROGRESS_VERIFYING, ++count, expectedCount))
-
-      frame = inputStream.readFrame()
     }
 
-    cipherStream.close()
+    if (cancellationSignal.isCanceled) {
+      throw FullBackupExporter.BackupCanceledException()
+    }
 
     return true
   }
