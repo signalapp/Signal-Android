e9cc5a05db15f88880204e024d5b8e40ec1a8c01
Mateusz Kata <mkata@mobitouch.net>
2023-02-22 21:58:32 +0100

    UI configuration in boarding

--- app/src/main/java/pigeon/extensions/KotilinExtensions.kt
+++ app/src/main/java/pigeon/extensions/KotilinExtensions.kt
@@ -1,10 +1,12 @@
 package pigeon.extensions
 
+import android.util.TypedValue
 import android.view.View
+import android.widget.TextView
 import androidx.core.view.ViewCompat
 
 
-fun View.onFocusTextChangeListener() {
+fun View.focusOnLeft() {
 
   if (!isSignalVersion()) {
     val BUTTON_SCALE_FOCUS = 1.3f
@@ -34,4 +36,40 @@ fun View.onFocusTextChangeListener() {
     }
     this.onFocusChangeListener = focus
   }
+}
+
+fun View.focusOnRight() {
+
+  if (!isSignalVersion()) {
+    val BUTTON_SCALE_FOCUS = 1.3f
+    val BUTTON_SCALE_NON_FOCUS = 1.0f
+    val BUTTON_TRANSLATION_X_FOCUS = 50.0f
+    val BUTTON_TRANSLATION_X_NON_FOCUS = 1.0f
+
+    val focus = View.OnFocusChangeListener { _, hasFocus ->
+      val scale: Float = if (hasFocus) {
+        BUTTON_SCALE_FOCUS
+      } else {
+        BUTTON_SCALE_NON_FOCUS
+      }
+
+      val translationX: Float = if (hasFocus) {
+        BUTTON_TRANSLATION_X_FOCUS
+      } else {
+        BUTTON_TRANSLATION_X_NON_FOCUS
+      }
+
+      ViewCompat.animate(this)
+        .translationX(translationX)
+        .scaleX(scale)
+        .scaleY(scale)
+        .start()
+
+    }
+    this.onFocusChangeListener = focus
+  }
+}
+
+fun TextView.setBigText() {
+  this.setTextSize(TypedValue.COMPLEX_UNIT_SP, 36f)
 }
