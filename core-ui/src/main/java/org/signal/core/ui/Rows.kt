bd078274b57a72a168d4a3f897803e57f8cb2753
Alex Hart <alex@signal.org>
2023-03-02 14:23:04 -0400

    Fix RadioRow vertical alignment.

--- core-ui/src/main/java/org/signal/core/ui/Rows.kt
+++ core-ui/src/main/java/org/signal/core/ui/Rows.kt
@@ -7,6 +7,7 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -30,7 +31,8 @@ object Rows {
         .padding(
           horizontal = dimensionResource(id = R.dimen.core_ui__gutter),
           vertical = 16.dp
-        )
+        ),
+      verticalAlignment = Alignment.CenterVertically
     ) {
       RadioButton(
         selected = selected,
