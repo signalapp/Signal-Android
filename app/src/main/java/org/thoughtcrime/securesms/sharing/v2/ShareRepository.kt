83b0963533539e3e12d0df3a2997b9e71e73e4b3
Clark <clark@signal.org>
2023-04-03 09:58:09 -0400

    Add fix for m4a mapping to audio/mpeg.

--- app/src/main/java/org/thoughtcrime/securesms/sharing/v2/ShareRepository.kt
+++ app/src/main/java/org/thoughtcrime/securesms/sharing/v2/ShareRepository.kt
@@ -48,7 +48,9 @@ class ShareRepository(context: Context) {
     }
 
     val uri = multiShareExternal.uri
-    val mimeType = getMimeType(appContext, uri, multiShareExternal.mimeType)
+    val size = getSize(appContext, uri)
+    val name = getFileName(appContext, uri)
+    val mimeType = getMimeType(appContext, uri, multiShareExternal.mimeType, name?.substringAfterLast('.', ""))
 
     val stream: InputStream = try {
       appContext.contentResolver.openInputStream(uri)
@@ -56,9 +58,6 @@ class ShareRepository(context: Context) {
       Log.w(TAG, "Failed to read stream!", e)
       null
     } ?: return ResolvedShareData.Failure
-
-    val size = getSize(appContext, uri)
-    val name = getFileName(appContext, uri)
 
     val blobUri: Uri = try {
       BlobProvider.getInstance()
@@ -142,8 +141,8 @@ class ShareRepository(context: Context) {
   companion object {
     private val TAG = Log.tag(ShareRepository::class.java)
 
-    private fun getMimeType(context: Context, uri: Uri, mimeType: String?): String {
-      var updatedMimeType = MediaUtil.getMimeType(context, uri)
+    private fun getMimeType(context: Context, uri: Uri, mimeType: String?, fileExtension: String? = null): String {
+      var updatedMimeType = MediaUtil.getMimeType(context, uri, fileExtension)
       if (updatedMimeType == null) {
         updatedMimeType = MediaUtil.getCorrectedMimeType(mimeType)
       }
