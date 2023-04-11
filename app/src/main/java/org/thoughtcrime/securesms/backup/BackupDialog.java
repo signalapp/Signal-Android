055f4b09ee4eebbcc3ddbca597e83abe85403546
Cody Henthorne <cody@signal.org>
2023-04-10 22:02:04 -0400

    Add additional backup folder failure debug info.

--- app/src/main/java/org/thoughtcrime/securesms/backup/BackupDialog.java
+++ app/src/main/java/org/thoughtcrime/securesms/backup/BackupDialog.java
@@ -130,6 +130,7 @@ public class BackupDialog {
                                      Intent.FLAG_GRANT_READ_URI_PERMISSION);
 
                     try {
+                       Log.d(TAG, "Starting choose backup location dialog");
                        fragment.startActivityForResult(intent, requestCode);
                      } catch (ActivityNotFoundException e) {
                        Toast.makeText(fragment.requireContext(), R.string.BackupDialog_no_file_picker_available, Toast.LENGTH_LONG)
