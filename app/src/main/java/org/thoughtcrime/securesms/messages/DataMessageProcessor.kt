866408f67353fb1ed438974081a127844b03b861
Cody Henthorne <cody@signal.org>
2023-05-18 13:02:17 -0400

    Limit body ranges processed on received messages.

--- app/src/main/java/org/thoughtcrime/securesms/messages/DataMessageProcessor.kt
+++ app/src/main/java/org/thoughtcrime/securesms/messages/DataMessageProcessor.kt
@@ -110,6 +110,8 @@ import kotlin.time.Duration.Companion.seconds
 
 object DataMessageProcessor {
 
+  private const val BODY_RANGE_PROCESSING_LIMIT = 250
+
   fun process(
     context: Context,
     senderRecipient: Recipient,
@@ -834,10 +836,10 @@ object DataMessageProcessor {
       val quote: QuoteModel? = getValidatedQuote(context, envelope.timestamp, message)
       val contacts: List<Contact> = getContacts(message)
       val linkPreviews: List<LinkPreview> = getLinkPreviews(message.previewList, message.body ?: "", false)
-      val mentions: List<Mention> = getMentions(message.bodyRangesList)
+      val mentions: List<Mention> = getMentions(message.bodyRangesList.take(BODY_RANGE_PROCESSING_LIMIT))
       val sticker: Attachment? = getStickerAttachment(envelope.timestamp, message)
       val attachments: List<Attachment> = message.attachmentsList.toPointersWithinLimit()
-      val messageRanges: BodyRangeList? = if (message.bodyRangesCount > 0) message.bodyRangesList.filter { it.hasStyle() }.toList().toBodyRangeList() else null
+      val messageRanges: BodyRangeList? = if (message.bodyRangesCount > 0) message.bodyRangesList.asSequence().take(BODY_RANGE_PROCESSING_LIMIT).filter { it.hasStyle() }.toList().toBodyRangeList() else null
 
       handlePossibleExpirationUpdate(envelope, metadata, senderRecipient.id, threadRecipient, groupId, message.expireTimer.seconds, receivedTime)
 
