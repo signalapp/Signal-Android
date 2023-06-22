1a1923c6c0984ffa74cd517496578dee119676b5
Greyson Parrelli <greyson@signal.org>
2023-06-22 17:55:05 -0400

    Fix docker build by using our own mirror.
    snapshot.debian.org is super flaky and unreliable. At this point it's
    easier to host our own mirror snapshot.

--- reproducible-builds/docker/print-versions.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-while read dep; do
-  dep_name=$(echo $dep | cut -f1 -d '=')
-  version=$(dpkg -s $dep_name | grep 'Version: ' | cut -f2 -d ' ')
-  echo "$dep_name=$version"
-done < $1
