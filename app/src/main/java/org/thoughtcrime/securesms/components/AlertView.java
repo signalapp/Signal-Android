8d20669e46ddcc0d54969ea763fe8bbbba1ede12
Alex Hart <alex@signal.org>
2023-06-29 11:42:23 -0300

    Rewrite AlertView to be a single ImageView.

--- app/src/main/java/org/thoughtcrime/securesms/components/AlertView.java
+++ app/src/main/java/org/thoughtcrime/securesms/components/AlertView.java
@@ -1,23 +1,16 @@
 package org.thoughtcrime.securesms.components;
 
-import android.annotation.TargetApi;
 import android.content.Context;
 import android.content.res.TypedArray;
-import android.os.Build.VERSION_CODES;
 import android.util.AttributeSet;
 import android.view.View;
-import android.widget.ImageView;
-import android.widget.LinearLayout;
 
-import org.signal.core.util.logging.Log;
+import androidx.appcompat.widget.AppCompatImageView;
+import androidx.core.content.ContextCompat;
+
 import org.thoughtcrime.securesms.R;
 
-public class AlertView extends LinearLayout {
-
-  private static final String TAG = Log.tag(AlertView.class);
-
-  private ImageView approvalIndicator;
-  private ImageView failedIndicator;
+public class AlertView extends AppCompatImageView {
 
   public AlertView(Context context) {
     this(context, null);
@@ -25,53 +18,38 @@ public class AlertView extends LinearLayout {
 
   public AlertView(Context context, AttributeSet attrs) {
     super(context, attrs);
-    initialize(attrs);
+    initialize();
   }
 
   public AlertView(final Context context, AttributeSet attrs, int defStyle) {
     super(context, attrs, defStyle);
-    initialize(attrs);
+    initialize();
   }
 
-  private void initialize(AttributeSet attrs) {
-    inflate(getContext(), R.layout.alert_view, this);
-
-    approvalIndicator = findViewById(R.id.pending_approval_indicator);
-    failedIndicator   = findViewById(R.id.sms_failed_indicator);
-
-    if (attrs != null) {
-      TypedArray typedArray = getContext().getTheme().obtainStyledAttributes(attrs, R.styleable.AlertView, 0, 0);
-      boolean useSmallIcon = typedArray.getBoolean(R.styleable.AlertView_useSmallIcon, false);
-      typedArray.recycle();
-
-      if (useSmallIcon) {
-        int size = getResources().getDimensionPixelOffset(R.dimen.alertview_small_icon_size);
-        failedIndicator.getLayoutParams().width  = size;
-        failedIndicator.getLayoutParams().height = size;
-        requestLayout();
-      }
-    }
+  private void initialize() {
+    setImageResource(R.drawable.symbol_error_circle_compact_16);
+    setScaleType(ScaleType.FIT_CENTER);
   }
 
   public void setNone() {
-    this.setVisibility(View.GONE);
+    setVisibility(View.GONE);
   }
 
   public void setPendingApproval() {
-    this.setVisibility(View.VISIBLE);
-    approvalIndicator.setVisibility(View.VISIBLE);
-    failedIndicator.setVisibility(View.GONE);
+    setVisibility(View.VISIBLE);
+    setColorFilter(ContextCompat.getColor(getContext(), R.color.signal_colorOnSurfaceVariant));
+    setContentDescription(getContext().getString(R.string.conversation_item_sent__pending_approval_description));
   }
 
   public void setFailed() {
-    this.setVisibility(View.VISIBLE);
-    approvalIndicator.setVisibility(View.GONE);
-    failedIndicator.setVisibility(View.VISIBLE);
+    setVisibility(View.VISIBLE);
+    setColorFilter(ContextCompat.getColor(getContext(), R.color.signal_colorError));
+    setContentDescription(getContext().getString(R.string.conversation_item_sent__send_failed_indicator_description));
   }
 
   public void setRateLimited() {
-    this.setVisibility(View.VISIBLE);
-    approvalIndicator.setVisibility(View.VISIBLE);
-    failedIndicator.setVisibility(View.GONE);
+    setVisibility(View.VISIBLE);
+    setColorFilter(ContextCompat.getColor(getContext(), R.color.signal_colorOnSurfaceVariant));
+    setContentDescription(getContext().getString(R.string.conversation_item_sent__pending_approval_description));
   }
 }
