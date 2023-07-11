bfcd57881e2aeb419bf2cc2680a38c70bc73cff6
Cody Henthorne <cody@signal.org>
2023-07-11 10:36:00 -0400

    Add test to verify sync behavior during VERIFIED to DEFAULT change.

---
+++ app/src/androidTest/java/org/thoughtcrime/securesms/database/RecipientTableTest_applyStorageSyncContactUpdate.kt
@@ -0,0 +1,60 @@
+/*
+ * Copyright 2023 Signal Messenger, LLC
+ * SPDX-License-Identifier: AGPL-3.0-only
+ */
+
+package org.thoughtcrime.securesms.database
+
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.thoughtcrime.securesms.dependencies.ApplicationDependencies
+import org.thoughtcrime.securesms.recipients.Recipient
+import org.thoughtcrime.securesms.storage.StorageRecordUpdate
+import org.thoughtcrime.securesms.storage.StorageSyncModels
+import org.thoughtcrime.securesms.testing.SignalActivityRule
+import org.thoughtcrime.securesms.testing.assertIs
+import org.thoughtcrime.securesms.util.MessageTableTestUtils
+import org.whispersystems.signalservice.api.storage.SignalContactRecord
+import org.whispersystems.signalservice.internal.storage.protos.ContactRecord
+
+@Suppress("ClassName")
+@RunWith(AndroidJUnit4::class)
+class RecipientTableTest_applyStorageSyncContactUpdate {
+  @get:Rule
+  val harness = SignalActivityRule()
+
+  @Test
+  fun insertMessageOnVerifiedToDefault() {
+    // GIVEN
+    val identities = ApplicationDependencies.getProtocolStore().aci().identities()
+    val other = Recipient.resolved(harness.others[0])
+
+    MmsHelper.insert(recipient = other)
+    identities.setVerified(other.id, harness.othersKeys[0].publicKey, IdentityTable.VerifiedStatus.VERIFIED)
+
+    val oldRecord: SignalContactRecord = StorageSyncModels.localToRemoteRecord(SignalDatabase.recipients.getRecordForSync(harness.others[0])!!).contact.get()
+
+    val newProto = oldRecord
+      .toProto()
+      .toBuilder()
+      .setIdentityState(ContactRecord.IdentityState.DEFAULT)
+      .build()
+    val newRecord = SignalContactRecord(oldRecord.id, newProto)
+
+    val update = StorageRecordUpdate<SignalContactRecord>(oldRecord, newRecord)
+
+    // WHEN
+    val oldVerifiedStatus: IdentityTable.VerifiedStatus = identities.getIdentityRecord(other.id).get().verifiedStatus
+    SignalDatabase.recipients.applyStorageSyncContactUpdate(update)
+    val newVerifiedStatus: IdentityTable.VerifiedStatus = identities.getIdentityRecord(other.id).get().verifiedStatus
+
+    // THEN
+    oldVerifiedStatus assertIs IdentityTable.VerifiedStatus.VERIFIED
+    newVerifiedStatus assertIs IdentityTable.VerifiedStatus.DEFAULT
+
+    val messages = MessageTableTestUtils.getMessages(SignalDatabase.threads.getThreadIdFor(other.id)!!)
+    messages.first().isIdentityDefault assertIs true
+  }
+}
