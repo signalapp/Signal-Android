6bcced37215b2af31d9e2558d30592e403af0632
Greyson Parrelli <greyson@signal.org>
2023-07-26 20:15:57 -0400

    Fix a RRP recovery path.

--- libsignal/service/src/main/java/org/whispersystems/signalservice/internal/push/BackupAuthCheckRequest.kt
+++ libsignal/service/src/main/java/org/whispersystems/signalservice/internal/push/BackupAuthCheckRequest.kt
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.annotation.JsonCreator
 import okio.ByteString.Companion.encode
 import org.whispersystems.signalservice.internal.ServiceResponse
 import org.whispersystems.signalservice.internal.ServiceResponseProcessor
+import java.io.IOException
 import java.nio.charset.StandardCharsets
 
 /**
@@ -23,12 +24,25 @@ data class BackupAuthCheckResponse @JsonCreator constructor(
 ) {
   private val actualMatches = matches["matches"] ?: emptyMap()
 
-  val match: String? = actualMatches.entries.firstOrNull { it.value.toString() == "match" }?.key?.toBasic()
+  val match: AuthCredentials? = actualMatches.entries.firstOrNull { it.value.toString() == "match" }?.key?.toAuthCredential()
   val invalid: List<String> = actualMatches.filterValues { it.toString() == "invalid" }.keys.map { it.toBasic() }
 
   /** Server expects and returns values as <username>:<password> but we prefer the full encoded Basic auth header format */
   private fun String.toBasic(): String {
     return "Basic ${encode(StandardCharsets.ISO_8859_1).base64()}"
+  }
+
+  private fun String.toAuthCredential(): AuthCredentials {
+    val firstColonIndex = this.indexOf(":")
+
+    if (firstColonIndex < 0) {
+      throw IOException("Invalid credential returned!")
+    }
+
+    val username = this.substring(0, firstColonIndex)
+    val password = this.substring(firstColonIndex + 1)
+
+    return AuthCredentials.create(username, password)
   }
 
   fun merge(other: BackupAuthCheckResponse): BackupAuthCheckResponse {
@@ -44,7 +58,11 @@ class BackupAuthCheckProcessor(response: ServiceResponse<BackupAuthCheckResponse
     return response.result.map { it.invalid }.orElse(emptyList())
   }
 
-  fun getValid(): String? {
-    return response.result.map { it.match }.orElse(null)
+  fun hasValidSvr2AuthCredential(): Boolean {
+    return response.result.map { it.match }.orElse(null) != null
+  }
+
+  fun requireSvr2AuthCredential(): AuthCredentials {
+    return response.result.get().match!!
   }
 }
