494b2c6786da4aa38331b17c914de128821d6df8
Greyson Parrelli <greyson@signal.org>
2023-09-01 11:45:36 -0400

    Add an index specifically for improving thread count perf.

--- app/src/main/java/org/thoughtcrime/securesms/migrations/ApplicationMigrations.java
+++ app/src/main/java/org/thoughtcrime/securesms/migrations/ApplicationMigrations.java
@@ -138,9 +138,10 @@ public class ApplicationMigrations {
     static final int IDENTITY_FIX                  = 94;
     static final int COPY_USERNAME_TO_SIGNAL_STORE = 95;
     static final int RECHECK_PAYMENTS              = 96;
+    static final int THREAD_COUNT_DB_MIGRATION     = 97;
   }
 
-  public static final int CURRENT_VERSION = 96;
+  public static final int CURRENT_VERSION = 97;
 
   /**
    * This *must* be called after the {@link JobManager} has been instantiated, but *before* the call
@@ -625,6 +626,10 @@ public class ApplicationMigrations {
 
     if (lastSeenVersion < Version.RECHECK_PAYMENTS) {
       jobs.put(Version.RECHECK_PAYMENTS, new RecheckPaymentsMigrationJob());
+    }
+
+    if (lastSeenVersion < Version.THREAD_COUNT_DB_MIGRATION) {
+      jobs.put(Version.THREAD_COUNT_DB_MIGRATION, new DatabaseMigrationJob());
     }
 
     return jobs;
--- app/src/main/java/org/thoughtcrime/securesms/recipients/RecipientUtil.java
+++ app/src/main/java/org/thoughtcrime/securesms/recipients/RecipientUtil.java
@@ -280,27 +280,13 @@ public class RecipientUtil {
     return isCallRequestAccepted(threadId, threadRecipient);
   }
 
-  /**
-   * @return True if a conversation existed before we enabled message requests, otherwise false.
-   */
-  @WorkerThread
-  public static boolean isPreMessageRequestThread(@Nullable Long threadId) {
-    long beforeTime = SignalStore.misc().getMessageRequestEnableTime();
-    return threadId != null && SignalDatabase.messages().getMessageCountForThread(threadId, beforeTime) > 0;
-  }
-
   @WorkerThread
   public static void shareProfileIfFirstSecureMessage(@NonNull Recipient recipient) {
     if (recipient.isProfileSharing()) {
       return;
     }
 
-    long threadId = SignalDatabase.threads().getThreadIdIfExistsFor(recipient.getId());
-
-    if (isPreMessageRequestThread(threadId)) {
-      return;
-    }
-
+    long    threadId     = SignalDatabase.threads().getThreadIdIfExistsFor(recipient.getId());
     boolean firstMessage = SignalDatabase.messages().getOutgoingSecureMessageCount(threadId) == 0;
 
     if (firstMessage || recipient.isHidden()) {
@@ -364,8 +350,7 @@ public class RecipientUtil {
            !threadRecipient.isRegistered() ||
            (!threadRecipient.isHidden() && (
                hasSentMessageInThread(threadId) ||
-               noSecureMessagesAndNoCallsInThread(threadId) ||
-               isPreMessageRequestThread(threadId))
+               noSecureMessagesAndNoCallsInThread(threadId))
            );
   }
 
@@ -373,8 +358,7 @@ public class RecipientUtil {
   private static boolean isCallRequestAccepted(@Nullable Long threadId, @NonNull Recipient threadRecipient) {
     return threadRecipient.isProfileSharing() ||
            threadRecipient.isSystemContact() ||
-           hasSentMessageInThread(threadId) ||
-           isPreMessageRequestThread(threadId);
+           hasSentMessageInThread(threadId);
   }
 
   @WorkerThread
