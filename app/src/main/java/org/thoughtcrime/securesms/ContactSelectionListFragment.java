ddc9aa75064ea464bddd042f4834eda0884dd064
Alex Hart <alex@signal.org>
2023-09-22 13:56:42 -0300

    Remove unused padding in ContactSelectionListFragment.

--- app/src/main/java/org/thoughtcrime/securesms/ContactSelectionListFragment.java
+++ app/src/main/java/org/thoughtcrime/securesms/ContactSelectionListFragment.java
@@ -35,7 +35,6 @@ import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.annotation.Px;
 import androidx.appcompat.app.AlertDialog;
 import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.constraintlayout.widget.ConstraintSet;
@@ -50,6 +49,7 @@ import androidx.transition.TransitionManager;
 import com.google.android.material.dialog.MaterialAlertDialogBuilder;
 import com.pnikosis.materialishprogress.ProgressWheel;
 
+import org.signal.core.util.concurrent.LifecycleDisposable;
 import org.signal.core.util.concurrent.SimpleTask;
 import org.signal.core.util.logging.Log;
 import org.thoughtcrime.securesms.components.RecyclerViewFastScroller;
@@ -73,7 +73,6 @@ import org.thoughtcrime.securesms.permissions.Permissions;
 import org.thoughtcrime.securesms.recipients.Recipient;
 import org.thoughtcrime.securesms.recipients.RecipientId;
 import org.thoughtcrime.securesms.util.CommunicationActions;
-import org.signal.core.util.concurrent.LifecycleDisposable;
 import org.thoughtcrime.securesms.util.TextSecurePreferences;
 import org.thoughtcrime.securesms.util.UsernameUtil;
 import org.thoughtcrime.securesms.util.ViewUtil;
@@ -545,10 +544,6 @@ public final class ContactSelectionListFragment extends LoggingFragment {
     contactSearchMediator.clearSelection();
     fastScroller.setVisibility(View.GONE);
     headerActionView.setVisibility(View.GONE);
-  }
-
-  public void setRecyclerViewPaddingBottom(@Px int paddingBottom) {
-    ViewUtil.setPaddingBottom(recyclerView, paddingBottom);
   }
 
   private void onLoadFinished(int count) {
