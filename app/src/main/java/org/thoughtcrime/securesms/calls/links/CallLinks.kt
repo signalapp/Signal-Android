046ce30e0824fd8100898af99d68892cee35a180
Alex Hart <alex@signal.org>
2023-10-10 16:56:35 -0300

    Fix SGNL schema link for call links.

--- app/src/main/java/org/thoughtcrime/securesms/calls/links/CallLinks.kt
+++ app/src/main/java/org/thoughtcrime/securesms/calls/links/CallLinks.kt
@@ -23,7 +23,7 @@ import java.net.URLDecoder
 object CallLinks {
   private const val ROOT_KEY = "key"
   private const val HTTPS_LINK_PREFIX = "https://signal.link/call/#key="
-  private const val SNGL_LINK_PREFIX = "sgnl://signal.link/#key="
+  private const val SNGL_LINK_PREFIX = "sgnl://signal.link/call/#key="
 
   private val TAG = Log.tag(CallLinks::class.java)
 
