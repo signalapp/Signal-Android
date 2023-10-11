4f20b35dfa2257074684ab2efc9a2e019899d9d1
Nicholas <nicholas@signal.org>
2023-10-11 14:20:30 -0400

    Disable incremental MAC generation.

--- libsignal/service/src/main/java/org/whispersystems/signalservice/internal/push/http/DigestingRequestBody.kt
+++ libsignal/service/src/main/java/org/whispersystems/signalservice/internal/push/http/DigestingRequestBody.kt
@@ -41,7 +41,7 @@ class DigestingRequestBody(
   override fun writeTo(sink: BufferedSink) {
     val digestStream = ByteArrayOutputStream()
     val inner = SkippingOutputStream(contentStart, sink.outputStream())
-    val isIncremental = outputStreamFactory is AttachmentCipherOutputStreamFactory
+    val isIncremental = outputStreamFactory is AttachmentCipherOutputStreamFactory && INCREMENTAL_MAC_SENDING_ENABLED
     val sizeChoice: ChunkSizeChoice = ChunkSizeChoice.inferChunkSize(contentLength.toInt())
     val outputStream: DigestingOutputStream = if (isIncremental) {
       (outputStreamFactory as AttachmentCipherOutputStreamFactory).createIncrementalFor(inner, contentLength, sizeChoice, digestStream)
@@ -95,5 +95,6 @@ class DigestingRequestBody(
 
   companion object {
     const val TAG = "DigestingRequestBody"
+    const val INCREMENTAL_MAC_SENDING_ENABLED = false
   }
 }
