a45c6858931e301767039837f7b51676cdcd968d
Nicholas Tinsley <nicholas@signal.org>
2023-10-24 09:34:55 -0400

    Increase logging during registration.

--- app/src/main/java/org/thoughtcrime/securesms/registration/RegistrationRepository.java
+++ app/src/main/java/org/thoughtcrime/securesms/registration/RegistrationRepository.java
@@ -226,9 +226,11 @@ public final class RegistrationRepository {
   public Single<BackupAuthCheckProcessor> getSvrAuthCredential(@NonNull RegistrationData registrationData, List<String> usernamePasswords) {
     SignalServiceAccountManager accountManager = AccountManagerFactory.getInstance().createUnauthenticated(context, registrationData.getE164(), SignalServiceAddress.DEFAULT_DEVICE_ID, registrationData.getPassword());
 
+    Log.d(TAG, "Fetching SVR backup credentials.");
     return accountManager.checkBackupAuthCredentials(registrationData.getE164(), usernamePasswords)
                          .map(BackupAuthCheckProcessor::new)
                          .doOnSuccess(processor -> {
+                           Log.d(TAG, "Received SVR backup auth credential response.");
                            if (SignalStore.svr().removeAuthTokens(processor.getInvalid())) {
                              new BackupManager(context).dataChanged();
                            }
--- app/src/main/java/org/thoughtcrime/securesms/registration/viewmodel/RegistrationViewModel.java
+++ app/src/main/java/org/thoughtcrime/securesms/registration/viewmodel/RegistrationViewModel.java
@@ -336,9 +336,11 @@ public final class RegistrationViewModel extends BaseRegistrationViewModel {
 
   public @NonNull Single<Boolean> canEnterSkipSmsFlow() {
     if (userSkippedReRegisterFlow) {
+      Log.d(TAG, "User skipped re-register flow.");
       return Single.just(false);
     }
 
+    Log.d(TAG, "Querying if user can enter skip SMS flow.");
     return Single.just(hasRecoveryPassword())
                  .flatMap(hasRecoveryPassword -> {
                    Log.i(TAG, "Checking if user has existing recovery password: " + hasRecoveryPassword);
@@ -365,15 +367,19 @@ public final class RegistrationViewModel extends BaseRegistrationViewModel {
         .collect(Collectors.toList());
 
     if (usernamePasswords.isEmpty()) {
+      Log.d(TAG, "No valid SVR tokens in local store.");
       return Single.just(false);
     }
 
+    Log.d(TAG, "Valid tokens in local store, validating with SVR.");
     return registrationRepository.getSvrAuthCredential(getRegistrationData(), usernamePasswords)
                                  .flatMap(p -> {
                                    if (p.hasValidSvr2AuthCredential()) {
+                                     Log.d(TAG, "Saving valid SVR2 auth credential.");
                                      setSvrAuthCredentials(new SvrAuthCredentialSet(null, p.requireSvr2AuthCredential()));
                                      return Single.just(true);
                                    } else {
+                                     Log.d(TAG, "SVR2 response contained no valid SVR2 auth credentials.");
                                      return Single.just(false);
                                    }
                                  })
