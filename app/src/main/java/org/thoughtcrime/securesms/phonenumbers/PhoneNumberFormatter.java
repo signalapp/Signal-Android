213d996168df3da7663f46a75be136edc154eccd
Greyson Parrelli <greyson@signal.org>
2023-11-17 13:11:40 -0500

    Fix issues with some japanese numbers being detected as shortcodes.

--- app/src/main/java/org/thoughtcrime/securesms/phonenumbers/PhoneNumberFormatter.java
+++ app/src/main/java/org/thoughtcrime/securesms/phonenumbers/PhoneNumberFormatter.java
@@ -172,7 +172,7 @@ public class PhoneNumberFormatter {
   private boolean isShortCode(@NonNull String bareNumber, String localCountryCode) {
     try {
       Phonenumber.PhoneNumber parsedNumber = phoneNumberUtil.parse(bareNumber, localCountryCode);
-      return ShortNumberInfo.getInstance().isPossibleShortNumberForRegion(parsedNumber, localCountryCode);
+      return ShortNumberInfo.getInstance().isValidShortNumberForRegion(parsedNumber, localCountryCode);
     } catch (NumberParseException e) {
       return false;
     }
