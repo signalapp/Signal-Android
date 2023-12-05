804d2c200db88eb8f425d47931bb0625be7e59fb
Haris Dautovic <haris.dautovic.dev@gmail.com>
2023-12-06 00:57:20 +0100

    Enable the camera only if permission is granted
    https://github.com/signalapp/Signal-Android/issues/13299

--- app/src/main/java/org/thoughtcrime/securesms/mediasend/AvatarSelectionActivity.java
+++ app/src/main/java/org/thoughtcrime/securesms/mediasend/AvatarSelectionActivity.java
@@ -1,7 +1,9 @@
 package org.thoughtcrime.securesms.mediasend;
 
+import android.Manifest;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Point;
 import android.net.Uri;
 import android.os.Bundle;
@@ -12,6 +14,7 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
@@ -214,6 +217,10 @@ public class AvatarSelectionActivity extends AppCompatActivity implements Camera
     return !isGalleryFirst();
   }
 
+  private boolean hasCameraPermission() {
+    return ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED;
+  }
+
   private void handleSave() {
     ImageEditorFragment fragment = (ImageEditorFragment) getSupportFragmentManager().findFragmentByTag(IMAGE_EDITOR);
     if (fragment == null) {
@@ -284,6 +291,6 @@ public class AvatarSelectionActivity extends AppCompatActivity implements Camera
 
   @Override
   public boolean isCameraEnabled() {
-    return true;
+    return hasCameraPermission();
   }
 }
