1d5e108cd4cf51d617831f650e35be1f75e95745
Jameson Williams <jameson@nosemaj.org>
2024-01-24 23:20:38 -0600

    QA target can pass on ARM MacBook
    The Conscrypt library does not have a native library suitable for
    running on the ARM MacBooks. As a result, unit tests that rely on
    Conscrypt also don't work on this hardware.
    
    Fortunately, though, the tests will pass without Conscrypt anyway. As a
    workaround, we can avoid using Conscrypt only when running the unit test
    suite on these machines.
    
    See also: https://github.com/google/conscrypt/issues/1034
    
    Resolves #13382.

--- libsignal-service/src/test/java/org/whispersystems/signalservice/api/crypto/UnidentifiedAccessTest.java
+++ libsignal-service/src/test/java/org/whispersystems/signalservice/api/crypto/UnidentifiedAccessTest.java
@@ -2,6 +2,7 @@ package org.whispersystems.signalservice.api.crypto;
 
 import junit.framework.TestCase;
 
+import org.conscrypt.Conscrypt;
 import org.conscrypt.OpenSSLProvider;
 import org.signal.libsignal.zkgroup.InvalidInputException;
 import org.signal.libsignal.zkgroup.profiles.ProfileKey;
@@ -12,7 +13,10 @@ import java.util.Arrays;
 public class UnidentifiedAccessTest extends TestCase {
 
   static {
-    Security.insertProviderAt(new OpenSSLProvider(), 1);
+    // https://github.com/google/conscrypt/issues/1034
+    if (!System.getProperty("os.arch").equals("aarch64")) {
+      Security.insertProviderAt(new OpenSSLProvider(), 1);
+    }
   }
 
   private final byte[] EXPECTED_RESULT = {(byte)0x5a, (byte)0x72, (byte)0x3a, (byte)0xce, (byte)0xe5, (byte)0x2c, (byte)0x5e, (byte)0xa0, (byte)0x2b, (byte)0x92, (byte)0xa3, (byte)0xa3, (byte)0x60, (byte)0xc0, (byte)0x95, (byte)0x95};
