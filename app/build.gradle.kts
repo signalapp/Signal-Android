ca3d239ce29b6cf7abd06aa8b7a928e805e2753a
Greyson Parrelli <greyson@signal.org>
2024-02-08 12:54:26 -0500

    Bump version to 6.46.7

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,8 +21,8 @@ plugins {
 
 apply(from = "static-ips.gradle.kts")
 
-val canonicalVersionCode = 1387
-val canonicalVersionName = "6.46.6"
+val canonicalVersionCode = 1388
+val canonicalVersionName = "6.46.7"
 
 val postFixSize = 100
 val abiPostFix: Map<String, Int> = mapOf(
