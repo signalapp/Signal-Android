c9f5f91aad8559998821b5e6269372609b3b2842
Cody Henthorne <cody@signal.org>
2024-02-22 12:48:51 -0500

    Fix black bars on username scan crosshair.

--- app/src/main/java/org/thoughtcrime/securesms/components/settings/app/usernamelinks/main/UsernameQrScanScreen.kt
+++ app/src/main/java/org/thoughtcrime/securesms/components/settings/app/usernamelinks/main/UsernameQrScanScreen.kt
@@ -9,16 +9,15 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.drawWithContent
-import androidx.compose.ui.geometry.CornerRadius
 import androidx.compose.ui.geometry.Offset
-import androidx.compose.ui.geometry.Size
-import androidx.compose.ui.graphics.BlendMode
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Path
+import androidx.compose.ui.graphics.PathEffect
 import androidx.compose.ui.graphics.drawscope.DrawScope
-import androidx.compose.ui.graphics.drawscope.Fill
 import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
@@ -46,13 +45,17 @@ fun UsernameQrScanScreen(
   onQrResultHandled: () -> Unit,
   modifier: Modifier = Modifier
 ) {
+  val path = remember { Path() }
+
   when (qrScanResult) {
     QrScanResult.InvalidData -> {
       QrScanResultDialog(stringResource(R.string.UsernameLinkSettings_qr_result_invalid), onDismiss = onQrResultHandled)
     }
+
     QrScanResult.NetworkError -> {
       QrScanResultDialog(stringResource(R.string.UsernameLinkSettings_qr_result_network_error), onDismiss = onQrResultHandled)
     }
+
     is QrScanResult.NotFound -> {
       if (qrScanResult.username != null) {
         QrScanResultDialog(stringResource(R.string.UsernameLinkSettings_qr_result_not_found, qrScanResult.username), onDismiss = onQrResultHandled)
@@ -60,10 +63,12 @@ fun UsernameQrScanScreen(
         QrScanResultDialog(stringResource(R.string.UsernameLinkSettings_qr_result_not_found_no_username), onDismiss = onQrResultHandled)
       }
     }
+
     is QrScanResult.Success -> {
       CommunicationActions.startConversation(LocalContext.current, qrScanResult.recipient, null)
       onQrResultHandled()
     }
+
     null -> {}
   }
 
@@ -88,7 +93,7 @@ fun UsernameQrScanScreen(
         .weight(1f, true)
         .drawWithContent {
           drawContent()
-          drawQrCrosshair()
+          drawQrCrosshair(path)
         }
     )
 
@@ -117,33 +122,39 @@ private fun QrScanResultDialog(message: String, onDismiss: () -> Unit) {
   )
 }
 
-private fun DrawScope.drawQrCrosshair() {
+private fun DrawScope.drawQrCrosshair(path: Path) {
   val crosshairWidth: Float = size.minDimension * 0.6f
-  val clearWidth: Float = crosshairWidth * 0.75f
+  val crosshairLineLength = crosshairWidth * 0.125f
 
-  // Draw a full white rounded rect...
-  drawRoundRect(
-    color = Color.White,
-    topLeft = center - Offset(crosshairWidth / 2, crosshairWidth / 2),
-    style = Stroke(width = 3.dp.toPx()),
-    size = Size(crosshairWidth, crosshairWidth),
-    cornerRadius = CornerRadius(10.dp.toPx(), 10.dp.toPx())
-  )
+  val topLeft = center - Offset(crosshairWidth / 2, crosshairWidth / 2)
+  val topRight = center + Offset(crosshairWidth / 2, -crosshairWidth / 2)
+  val bottomRight = center + Offset(crosshairWidth / 2, crosshairWidth / 2)
+  val bottomLeft = center + Offset(-crosshairWidth / 2, crosshairWidth / 2)
 
-  // ...then cut out the middle parts with BlendMode.Clear to leave us with just the corners
-  drawRect(
-    color = Color.White,
-    topLeft = Offset(center.x - clearWidth / 2, 0f),
-    style = Fill,
-    size = Size(clearWidth, size.height),
-    blendMode = BlendMode.Clear
-  )
+  path.reset()
 
-  drawRect(
+  drawPath(
+    path = path.apply {
+      moveTo(topLeft.x, topLeft.y + crosshairLineLength)
+      lineTo(topLeft.x, topLeft.y)
+      lineTo(topLeft.x + crosshairLineLength, topLeft.y)
+
+      moveTo(topRight.x - crosshairLineLength, topRight.y)
+      lineTo(topRight.x, topRight.y)
+      lineTo(topRight.x, topRight.y + crosshairLineLength)
+
+      moveTo(bottomRight.x, bottomRight.y - crosshairLineLength)
+      lineTo(bottomRight.x, bottomRight.y)
+      lineTo(bottomRight.x - crosshairLineLength, bottomRight.y)
+
+      moveTo(bottomLeft.x + crosshairLineLength, bottomLeft.y)
+      lineTo(bottomLeft.x, bottomLeft.y)
+      lineTo(bottomLeft.x, bottomLeft.y - crosshairLineLength)
+    },
     color = Color.White,
-    topLeft = Offset(0f, center.y - clearWidth / 2),
-    style = Fill,
-    size = Size(size.width, clearWidth),
-    blendMode = BlendMode.Clear
+    style = Stroke(
+      width = 3.dp.toPx(),
+      pathEffect = PathEffect.cornerPathEffect(10.dp.toPx())
+    )
   )
 }
