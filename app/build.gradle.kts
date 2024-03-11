6754fef164dffb7f21d40c55ea63c45f10841464
Clark Chen <clark@signal.org>
2024-03-11 18:50:24 -0400

    Bump version to 7.1.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,8 +21,8 @@ plugins {
 
 apply(from = "static-ips.gradle.kts")
 
-val canonicalVersionCode = 1398
-val canonicalVersionName = "7.1.1"
+val canonicalVersionCode = 1399
+val canonicalVersionName = "7.1.2"
 
 val postFixSize = 100
 val abiPostFix: Map<String, Int> = mapOf(
