931ffd0ba30a0e39d2dc10033f3bb0b3a0e295b0
Nicholas Tinsley <nicholas@signal.org>
2024-03-27 16:01:18 -0400

    Bump version to 7.2.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,8 +21,8 @@ plugins {
 
 apply(from = "static-ips.gradle.kts")
 
-val canonicalVersionCode = 1400
-val canonicalVersionName = "7.1.3"
+val canonicalVersionCode = 1401
+val canonicalVersionName = "7.2.0"
 
 val postFixSize = 100
 val abiPostFix: Map<String, Int> = mapOf(
