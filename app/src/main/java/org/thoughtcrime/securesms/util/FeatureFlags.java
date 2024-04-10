c1d29b5c39cc5398e88c9b52dd73bf6f46b2c64d
Greyson Parrelli <greyson@signal.org>
2024-04-10 14:54:35 -0400

    Set internalUser=true for nightly builds.

--- app/src/main/java/org/thoughtcrime/securesms/util/FeatureFlags.java
+++ app/src/main/java/org/thoughtcrime/securesms/util/FeatureFlags.java
@@ -378,7 +378,7 @@ public final class FeatureFlags {
 
   /** Internal testing extensions. */
   public static boolean internalUser() {
-    return getBoolean(INTERNAL_USER, false) || Environment.IS_STAGING;
+    return getBoolean(INTERNAL_USER, false) || Environment.IS_NIGHTLY || Environment.IS_STAGING;
   }
 
   /** Whether or not to use the UUID in verification codes. */
