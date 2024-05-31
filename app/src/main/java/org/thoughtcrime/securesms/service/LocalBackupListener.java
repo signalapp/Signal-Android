1565ecdcea298463fbbe0ff6b788004b96ddfa76
Clark <clark@signal.org>
2024-05-31 10:17:37 -0400

    Fix multiple scheduled backups due to jitter.

--- app/src/main/java/org/thoughtcrime/securesms/service/LocalBackupListener.java
+++ app/src/main/java/org/thoughtcrime/securesms/service/LocalBackupListener.java
@@ -49,13 +49,12 @@ public class LocalBackupListener extends PersistentAlarmManagerListener {
     int           minute = SignalStore.settings().getBackupMinute();
     LocalDateTime next   = now.withHour(hour).withMinute(minute).withSecond(0);
 
-    int jitter = (new Random().nextInt(BACKUP_JITTER_WINDOW_SECONDS)) - (BACKUP_JITTER_WINDOW_SECONDS / 2);
-
-    next = next.plusSeconds(jitter);
-
     if (now.isAfter(next)) {
       next = next.plusDays(1);
     }
+
+    int jitter = (new Random().nextInt(BACKUP_JITTER_WINDOW_SECONDS)) - (BACKUP_JITTER_WINDOW_SECONDS / 2);
+    next = next.plusSeconds(jitter);
 
     long nextTime = JavaTimeExtensionsKt.toMillis(next);
 
