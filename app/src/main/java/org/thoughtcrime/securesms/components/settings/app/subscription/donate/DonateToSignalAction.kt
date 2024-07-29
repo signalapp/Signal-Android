ebec2f524608320b7e3d253f0225461a1fc01e83
Alex Hart <alex@signal.org>
2024-07-29 12:20:13 -0300

    Fix donation action routing.

--- app/src/main/java/org/thoughtcrime/securesms/components/settings/app/subscription/donate/DonateToSignalAction.kt
+++ app/src/main/java/org/thoughtcrime/securesms/components/settings/app/subscription/donate/DonateToSignalAction.kt
@@ -6,6 +6,6 @@ import org.thoughtcrime.securesms.database.InAppPaymentTable
 sealed class DonateToSignalAction {
   data class DisplayCurrencySelectionDialog(val inAppPaymentType: InAppPaymentType, val supportedCurrencies: List<String>) : DonateToSignalAction()
   data class DisplayGatewaySelectorDialog(val inAppPayment: InAppPaymentTable.InAppPayment) : DonateToSignalAction()
-  object CancelSubscription : DonateToSignalAction()
+  data object CancelSubscription : DonateToSignalAction()
   data class UpdateSubscription(val inAppPayment: InAppPaymentTable.InAppPayment, val isLongRunning: Boolean) : DonateToSignalAction()
 }
