879c794324b04f510f39f1bec61bd2a8ea106243
mtang-signal <mtang@signal.org>
2024-08-22 11:50:42 -0700

    Bump version to 7.14.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,8 +21,8 @@ plugins {
 
 apply(from = "static-ips.gradle.kts")
 
-val canonicalVersionCode = 1445
-val canonicalVersionName = "7.14.1"
+val canonicalVersionCode = 1446
+val canonicalVersionName = "7.14.2"
 val currentHotfixVersion = 0
 val maxHotfixVersions = 100
 
