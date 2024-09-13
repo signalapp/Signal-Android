6188502cb10e46f1741af9a8da737715b9fd6e22
Cody Henthorne <cody@signal.org>
2024-09-13 13:52:40 -0400

    Bump version to 7.17.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,8 +21,8 @@ plugins {
 
 apply(from = "static-ips.gradle.kts")
 
-val canonicalVersionCode = 1457
-val canonicalVersionName = "7.17.0"
+val canonicalVersionCode = 1458
+val canonicalVersionName = "7.17.1"
 val currentHotfixVersion = 0
 val maxHotfixVersions = 100
 
