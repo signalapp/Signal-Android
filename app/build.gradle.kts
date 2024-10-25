4e87422575516c70d0b66081d70c919220fe70b6
Greyson Parrelli <greyson@signal.org>
2024-10-25 16:21:26 -0400

    Bump version to 7.22.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,8 +21,8 @@ plugins {
 
 apply(from = "static-ips.gradle.kts")
 
-val canonicalVersionCode = 1477
-val canonicalVersionName = "7.22.0"
+val canonicalVersionCode = 1478
+val canonicalVersionName = "7.22.1"
 val currentHotfixVersion = 0
 val maxHotfixVersions = 100
 
