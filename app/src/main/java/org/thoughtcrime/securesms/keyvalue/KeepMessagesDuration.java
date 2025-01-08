97525a1ff3add7553abed49a92b6b7c7bc94e95d
Rainer Matischek <rainer.matischek@gmx.net>
2025-01-08 18:13:44 +0100

    Usability improvement of 1-YEAR KeepMessagesDuration: Simply adding a few buffer-days to 365 increases the usability wrt annually messages (e.g. good wishes for birthday, New Year, etc). In this way, users are still able to cross-check, what exact wording they have written to a person 1 year before (e.g. to avoid writing always the same).

--- app/src/main/java/org/thoughtcrime/securesms/keyvalue/KeepMessagesDuration.java
+++ app/src/main/java/org/thoughtcrime/securesms/keyvalue/KeepMessagesDuration.java
@@ -9,7 +9,8 @@ import java.util.concurrent.TimeUnit;
 
 public enum KeepMessagesDuration {
   FOREVER(0, R.string.preferences_storage__forever, Long.MAX_VALUE),
-  ONE_YEAR(1, R.string.preferences_storage__one_year, TimeUnit.DAYS.toMillis(365)),
+  //Remark: Simply adding a few "buffer-days" to 365 increases the usability wrt annually messages (good wishes for birthday, New Year, etc), so that users are still able to cross-check, what exact wording they have written to a person 1 year before (e.g. to avoid writing always the same)
+  ONE_YEAR(1, R.string.preferences_storage__one_year, TimeUnit.DAYS.toMillis(365+3)),
   SIX_MONTHS(2, R.string.preferences_storage__six_months, TimeUnit.DAYS.toMillis(183)),
   THIRTY_DAYS(3, R.string.preferences_storage__thirty_days, TimeUnit.DAYS.toMillis(30));
 
