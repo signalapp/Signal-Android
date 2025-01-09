7611b2421d811f17d22b5d9d2cbabe8af17b59a7
Greyson Parrelli <greyson@signal.org>
2025-01-09 13:06:49 -0500

    Fix test compilation issues.

--- spinner/app/src/main/java/org/signal/spinnertest/SpinnerTestSqliteOpenHelper.kt
+++ spinner/app/src/main/java/org/signal/spinnertest/SpinnerTestSqliteOpenHelper.kt
@@ -13,8 +13,8 @@ import androidx.sqlite.db.SupportSQLiteQuery
 import androidx.sqlite.db.SupportSQLiteStatement
 import java.util.Locale
 
-class SpinnerTestSqliteOpenHelper(context: Context?) :
-  SQLiteOpenHelper(context, "test", null, 2), SupportSQLiteDatabase {
+class SpinnerTestSqliteOpenHelper(context: Context) : SQLiteOpenHelper(context, "test", null, 2), SupportSQLiteDatabase {
+
   override fun onCreate(db: SQLiteDatabase) {
     db.execSQL("CREATE TABLE test (id INTEGER PRIMARY KEY, col1 TEXT, col2 TEXT)")
   }
@@ -25,7 +25,25 @@ class SpinnerTestSqliteOpenHelper(context: Context?) :
     }
   }
 
-  override fun compileStatement(sql: String?): SupportSQLiteStatement {
+  override var version: Int
+    get() = readableDatabase.version
+    set(value) {
+      writableDatabase.version = value
+    }
+
+  override val maximumSize: Long
+    get() = readableDatabase.maximumSize
+
+  override val isDbLockedByCurrentThread: Boolean
+    get() = readableDatabase.isDbLockedByCurrentThread
+
+  override var pageSize: Long
+    get() = readableDatabase.pageSize
+    set(value) {
+      writableDatabase.pageSize = value
+    }
+
+  override fun compileStatement(sql: String): SupportSQLiteStatement {
     TODO("Not yet implemented")
   }
 
@@ -37,11 +55,11 @@ class SpinnerTestSqliteOpenHelper(context: Context?) :
     writableDatabase.beginTransactionNonExclusive()
   }
 
-  override fun beginTransactionWithListener(transactionListener: SQLiteTransactionListener?) {
+  override fun beginTransactionWithListener(transactionListener: SQLiteTransactionListener) {
     writableDatabase.beginTransactionWithListener(transactionListener)
   }
 
-  override fun beginTransactionWithListenerNonExclusive(transactionListener: SQLiteTransactionListener?) {
+  override fun beginTransactionWithListenerNonExclusive(transactionListener: SQLiteTransactionListener) {
     writableDatabase.beginTransactionWithListenerNonExclusive(transactionListener)
   }
 
@@ -57,10 +75,6 @@ class SpinnerTestSqliteOpenHelper(context: Context?) :
     return writableDatabase.inTransaction()
   }
 
-  override fun isDbLockedByCurrentThread(): Boolean {
-    return writableDatabase.isDbLockedByCurrentThread
-  }
-
   override fun yieldIfContendedSafely(): Boolean {
     return writableDatabase.yieldIfContendedSafely()
   }
@@ -69,84 +83,61 @@ class SpinnerTestSqliteOpenHelper(context: Context?) :
     return writableDatabase.yieldIfContendedSafely(sleepAfterYieldDelay)
   }
 
-  override fun getVersion(): Int {
-    return writableDatabase.version
-  }
-
-  override fun setVersion(version: Int) {
-    writableDatabase.version = version
-  }
-
-  override fun getMaximumSize(): Long {
-    return writableDatabase.maximumSize
-  }
-
   override fun setMaximumSize(numBytes: Long): Long {
     writableDatabase.maximumSize = numBytes
     return writableDatabase.maximumSize
   }
 
-  override fun getPageSize(): Long {
-    return writableDatabase.pageSize
-  }
-
-  override fun setPageSize(numBytes: Long) {
-    writableDatabase.pageSize = numBytes
-  }
-
-  override fun query(query: String?): Cursor {
+  override fun query(query: String): Cursor {
     return readableDatabase.rawQuery(query, null)
   }
 
-  override fun query(query: String?, bindArgs: Array<out Any>?): Cursor {
-    return readableDatabase.rawQuery(query, bindArgs?.map { it.toString() }?.toTypedArray())
+  override fun query(query: String, bindArgs: Array<out Any?>): Cursor {
+    return readableDatabase.rawQuery(query, bindArgs.map { it.toString() }?.toTypedArray())
   }
 
-  override fun query(query: SupportSQLiteQuery?): Cursor {
+  override fun query(query: SupportSQLiteQuery): Cursor {
     TODO("Not yet implemented")
   }
 
-  override fun query(query: SupportSQLiteQuery?, cancellationSignal: CancellationSignal?): Cursor {
+  override fun query(query: SupportSQLiteQuery, cancellationSignal: CancellationSignal?): Cursor {
     TODO("Not yet implemented")
   }
 
-  override fun insert(table: String?, conflictAlgorithm: Int, values: ContentValues?): Long {
+  override fun insert(table: String, conflictAlgorithm: Int, values: ContentValues): Long {
     return writableDatabase.insertWithOnConflict(table, null, values, conflictAlgorithm)
   }
 
-  override fun delete(table: String?, whereClause: String?, whereArgs: Array<out Any>?): Int {
+  override fun delete(table: String, whereClause: String?, whereArgs: Array<out Any?>?): Int {
     return writableDatabase.delete(table, whereClause, whereArgs?.map { it.toString() }?.toTypedArray())
   }
 
-  override fun update(table: String?, conflictAlgorithm: Int, values: ContentValues?, whereClause: String?, whereArgs: Array<out Any>?): Int {
+  override fun update(table: String, conflictAlgorithm: Int, values: ContentValues, whereClause: String?, whereArgs: Array<out Any?>?): Int {
     return writableDatabase.updateWithOnConflict(table, values, whereClause, whereArgs?.map { it.toString() }?.toTypedArray(), conflictAlgorithm)
   }
 
-  override fun execSQL(sql: String?) {
+  override fun execSQL(sql: String) {
     writableDatabase.execSQL(sql)
   }
 
-  override fun execSQL(sql: String?, bindArgs: Array<out Any>?) {
-    writableDatabase.execSQL(sql, bindArgs?.map { it.toString() }?.toTypedArray())
+  override fun execSQL(sql: String, bindArgs: Array<out Any?>) {
+    writableDatabase.execSQL(sql, bindArgs.map { it.toString() }.toTypedArray())
   }
 
-  override fun isReadOnly(): Boolean {
-    return readableDatabase.isReadOnly
-  }
+  override val isReadOnly: Boolean
+    get() = readableDatabase.isReadOnly
 
-  override fun isOpen(): Boolean {
-    return readableDatabase.isOpen
-  }
+  override val isOpen: Boolean
+    get() = readableDatabase.isOpen
 
   override fun needUpgrade(newVersion: Int): Boolean {
     return readableDatabase.needUpgrade(newVersion)
   }
 
-  override fun getPath(): String {
-    return readableDatabase.path
-  }
+  override val path: String
+    get() = readableDatabase.path
 
-  override fun setLocale(locale: Locale?) {
+  override fun setLocale(locale: Locale) {
     writableDatabase.setLocale(locale)
   }
 
@@ -166,15 +157,12 @@ class SpinnerTestSqliteOpenHelper(context: Context?) :
     writableDatabase.disableWriteAheadLogging()
   }
 
-  override fun isWriteAheadLoggingEnabled(): Boolean {
-    return readableDatabase.isWriteAheadLoggingEnabled
-  }
+  override val isWriteAheadLoggingEnabled: Boolean
+    get() = readableDatabase.isWriteAheadLoggingEnabled
 
-  override fun getAttachedDbs(): MutableList<Pair<String, String>> {
-    return readableDatabase.attachedDbs
-  }
+  override val attachedDbs: List<Pair<String, String>>?
+    get() = readableDatabase.attachedDbs
 
-  override fun isDatabaseIntegrityOk(): Boolean {
-    return readableDatabase.isDatabaseIntegrityOk
-  }
+  override val isDatabaseIntegrityOk: Boolean
+    get() = readableDatabase.isDatabaseIntegrityOk
 }
