signalapp/Signal-Android

commit ff09d2267bb5ba41791b39fad8e0eba1dc3ad3ae

Don't send edits if the message text wasn't changed.

app/src/main/java/org/thoughtcrime/securesms/conversation/v2/ConversationFragment.kt
@@ -1594,6 +1594,12 @@ class ConversationFragment :
       return
     }
 
+    if (editMessage.body == composeText.editableText.toString()) {
+      Log.d(TAG, "Updated message matches original, exiting edit mode")
+      inputPanel.exitEditMessageMode()
+      return
+    }
+
     sendMessage()
   }
 

commit d3be5807b52ac4938f679a54da7015fbac35317f

Fix ability to remove quoted message without changing message text.
Fixes a bug introduced in ff09d2267bb5ba41791b39fad8e0eba1dc3ad3ae that prevented removing a quoted message when the message text is unchanged.

app/src/main/java/org/thoughtcrime/securesms/conversation/v2/ConversationFragment.kt
@@ -330,6 +330,7 @@ import org.thoughtcrime.securesms.util.atUTC
 import org.thoughtcrime.securesms.util.createActivityViewModel
 import org.thoughtcrime.securesms.util.doAfterNextLayout
 import org.thoughtcrime.securesms.util.fragments.requireListener
+import org.thoughtcrime.securesms.util.getQuote
 import org.thoughtcrime.securesms.util.getRecordQuoteType
 import org.thoughtcrime.securesms.util.hasAudio
 import org.thoughtcrime.securesms.util.hasGiftBadge
@@ -1594,7 +1595,9 @@ class ConversationFragment :
       return
     }
 
-    if (editMessage.body == composeText.editableText.toString()) {
+    if (editMessage.body == composeText.editableText.toString() &&
+      editMessage.getQuote()?.displayText?.toString() == inputPanel.quote.map { it.text }.orNull()
+    ) {
       Log.d(TAG, "Updated message matches original, exiting edit mode")
       inputPanel.exitEditMessageMode()
       return