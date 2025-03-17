6bbd8995070f9fe6c0317a63e2be147543668654
Alex Hart <alex@signal.org>
2025-03-17 16:29:17 -0300

    Bump version to 7.38.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,8 +20,8 @@ plugins {
 
 apply(from = "static-ips.gradle.kts")
 
-val canonicalVersionCode = 1525
-val canonicalVersionName = "7.38.2"
+val canonicalVersionCode = 1526
+val canonicalVersionName = "7.38.3"
 val currentHotfixVersion = 0
 val maxHotfixVersions = 100
 
