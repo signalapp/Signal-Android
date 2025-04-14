f172f5d47ef8fb16d51465208331ef40f5d84db0
Cody Henthorne <cody@signal.org>
2025-04-14 19:59:32 -0400

    Convert change number back to REST.

--- libsignal-service/src/main/java/org/whispersystems/signalservice/api/registration/RegistrationApi.kt
+++ libsignal-service/src/main/java/org/whispersystems/signalservice/api/registration/RegistrationApi.kt
@@ -7,6 +7,7 @@ package org.whispersystems.signalservice.api.registration
 
 import org.whispersystems.signalservice.api.NetworkResult
 import org.whispersystems.signalservice.api.account.AccountAttributes
+import org.whispersystems.signalservice.api.account.ChangePhoneNumberRequest
 import org.whispersystems.signalservice.api.account.PreKeyCollection
 import org.whispersystems.signalservice.api.provisioning.RestoreMethod
 import org.whispersystems.signalservice.internal.push.BackupV2AuthCheckResponse
@@ -131,6 +132,17 @@ class RegistrationApi(
   fun setRestoreMethod(token: String, method: RestoreMethod): NetworkResult<Unit> {
     return NetworkResult.fromFetch {
       pushServiceSocket.setRestoreMethodChosen(token, RestoreMethodBody(method = method))
+    }
+  }
+
+  /**
+   * Changes the phone number that an account is associated with.
+   *
+   * `PUT /v2/accounts/number`
+   */
+  fun changeNumber(requestBody: ChangePhoneNumberRequest): NetworkResult<VerifyAccountResponse> {
+    return NetworkResult.fromFetch {
+      pushServiceSocket.changeNumber(requestBody)
     }
   }
 }
