f255160c4a0f281a9355efacc636ab1497b726b9
Greyson Parrelli <greyson@signal.org>
2025-08-07 13:57:34 -0400

    Default the number of JobRunners to 8.

--- app/src/main/java/org/thoughtcrime/securesms/jobmanager/JobManager.java
+++ app/src/main/java/org/thoughtcrime/securesms/jobmanager/JobManager.java
@@ -655,7 +655,7 @@ public class JobManager implements ConstraintObserver.Notifier {
     public static class Builder {
 
       private ExecutorFactory                 executorFactory     = new DefaultExecutorFactory();
-      private int                             jobThreadCount      = Math.max(2, Math.min(Runtime.getRuntime().availableProcessors() - 1, 4));
+      private int                             jobThreadCount      = 8;
       private Map<String, Job.Factory>        jobFactories        = new HashMap<>();
       private Map<String, Constraint.Factory> constraintFactories = new HashMap<>();
       private List<ConstraintObserver>        constraintObservers = new ArrayList<>();
