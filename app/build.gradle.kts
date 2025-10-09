0cc5a81ef9e25b47da06fa7c673b9dc040dad40d
Alex Hart <alex@signal.org>
2025-10-09 16:56:27 -0300

    Bump version to 7.59.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,8 +23,8 @@ plugins {
 apply(from = "static-ips.gradle.kts")
 
 val canonicalVersionCode = 1596
-val canonicalVersionName = "7.59.1"
-val currentHotfixVersion = 0
+val canonicalVersionName = "7.59.2"
+val currentHotfixVersion = 1
 val maxHotfixVersions = 100
 
 val keystores: Map<String, Properties?> = mapOf("debug" to loadKeystoreProperties("keystore.debug.properties"))
