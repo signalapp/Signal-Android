9c06b07262979b7f2f443711c0e6c685d9f7b75d
Alex Hart <alex@signal.org>
2025-12-19 16:52:18 -0400

    Update translations and other static files.

--- app/static-ips.gradle.kts
+++ app/static-ips.gradle.kts
@@ -1,6 +1,6 @@
 rootProject.extra["service_ips"] = """new String[]{"13.248.212.111","76.223.92.165"}"""
 rootProject.extra["storage_ips"] = """new String[]{"142.250.69.147"}"""
-rootProject.extra["cdn_ips"] = """new String[]{"18.239.6.104","18.239.6.107","18.239.6.81","18.239.6.89"}"""
+rootProject.extra["cdn_ips"] = """new String[]{"13.225.196.60","13.225.196.76","13.225.196.77","13.225.196.9"}"""
 rootProject.extra["cdn2_ips"] = """new String[]{"104.18.10.47","104.18.11.47"}"""
 rootProject.extra["cdn3_ips"] = """new String[]{"104.18.10.47","104.18.11.47"}"""
 rootProject.extra["sfu_ips"] = """new String[]{"34.117.136.13"}"""
