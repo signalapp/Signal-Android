8908754dccb225fdcbe75ecba0f5978eff6675ef
Alex Hart <alex@signal.org>
2026-01-23 12:43:50 -0400

    Bump version to 7.70.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -30,8 +30,8 @@ plugins {
 apply(from = "static-ips.gradle.kts")
 
 val canonicalVersionCode = 1638
-val canonicalVersionName = "7.70.1"
-val currentHotfixVersion = 0
+val canonicalVersionName = "7.70.2"
+val currentHotfixVersion = 1
 val maxHotfixVersions = 100
 
 // We don't want versions to ever end in 0 so that they don't conflict with nightly versions
