5e221901e67bbac1ce4511de9fc7dce51a5cb6bd
Greyson Parrelli <greyson@signal.org>
2026-02-06 22:47:46 -0500

    Update video demo app to minSdk 26.

--- demo/video/build.gradle.kts
+++ demo/video/build.gradle.kts
@@ -14,7 +14,7 @@ android {
 
   defaultConfig {
     applicationId = "org.thoughtcrime.video.app"
-    minSdk = 23
+    minSdk = 26
     targetSdk = libs.versions.targetSdk.get().toInt()
     versionCode = 1
     versionName = "1.0"
