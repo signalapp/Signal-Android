316b071c813a162427d253efb965c54aa2b42e2f
Greyson Parrelli <greyson@signal.org>
2026-03-06 16:34:51 -0500

    Bump version to 8.2.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,8 +24,8 @@ plugins {
 
 apply(from = "static-ips.gradle.kts")
 
-val canonicalVersionCode = 1661
-val canonicalVersionName = "8.2.0"
+val canonicalVersionCode = 1662
+val canonicalVersionName = "8.2.1"
 val currentHotfixVersion = 0
 val maxHotfixVersions = 100
 
