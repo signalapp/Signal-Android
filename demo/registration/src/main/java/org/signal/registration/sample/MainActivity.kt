33c3c48bd5699f439bdddd079197bf35ad0faa0d
Greyson Parrelli <greyson@signal.org>
2026-03-10 16:32:59 -0400

    Make regV5 edge-to-edge.

--- demo/registration/src/main/java/org/signal/registration/sample/MainActivity.kt
+++ demo/registration/src/main/java/org/signal/registration/sample/MainActivity.kt
@@ -10,6 +10,7 @@ package org.signal.registration.sample
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.compose.animation.EnterTransition
 import androidx.compose.animation.ExitTransition
 import androidx.compose.animation.core.tween
@@ -23,7 +24,10 @@ import androidx.compose.animation.slideOutHorizontally
 import androidx.compose.animation.slideOutVertically
 import androidx.compose.animation.togetherWith
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.safeDrawing
+import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
@@ -80,6 +84,7 @@ sealed interface SampleRoute : NavKey {
 class MainActivity : ComponentActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
+    enableEdgeToEdge()
 
     setContent {
       SignalTheme {
@@ -99,7 +104,9 @@ fun AppScreen(registrationDependencies: RegistrationDependencies) {
   val backStack = rememberNavBackStack(SampleRoute.Main)
 
   Box(
-    modifier = Modifier.fillMaxSize()
+    modifier = Modifier
+      .fillMaxSize()
+      .windowInsetsPadding(WindowInsets.safeDrawing)
   ) {
     SampleNavHost(
       backStack = backStack,
