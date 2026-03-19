c7ec3ab837b3c149d5579840317b1dc6cd4629f3
Michelle Tang <mtang@signal.org>
2026-03-19 17:09:55 -0400

    Bump version to 8.4.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,8 +24,8 @@ plugins {
 
 apply(from = "static-ips.gradle.kts")
 
-val canonicalVersionCode = 1669
-val canonicalVersionName = "8.4.0"
+val canonicalVersionCode = 1670
+val canonicalVersionName = "8.4.1"
 val currentHotfixVersion = 0
 val maxHotfixVersions = 100
 
