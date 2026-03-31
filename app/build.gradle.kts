430a55f89f0c378cacc5017101ffafdf3ff325ad
Alex Hart <alex@signal.org>
2026-03-31 16:49:29 -0300

    Bump version to 8.6.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,8 +24,8 @@ plugins {
 
 apply(from = "static-ips.gradle.kts")
 
-val canonicalVersionCode = 1672
-val canonicalVersionName = "8.5.1"
+val canonicalVersionCode = 1673
+val canonicalVersionName = "8.6.0"
 val currentHotfixVersion = 0
 val maxHotfixVersions = 100
 
