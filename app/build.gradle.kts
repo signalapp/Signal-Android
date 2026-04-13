b054a7eb766f04d248feed658a18e1130d26d327
jeffrey-signal <jeffrey@signal.org>
2026-04-13 16:15:41 -0400

    Bump version to 8.7.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,8 +24,8 @@ plugins {
 
 apply(from = "static-ips.gradle.kts")
 
-val canonicalVersionCode = 1678
-val canonicalVersionName = "8.7.2"
+val canonicalVersionCode = 1679
+val canonicalVersionName = "8.7.3"
 val currentHotfixVersion = 0
 val maxHotfixVersions = 100
 
