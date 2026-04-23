db6efeaf3d58da2290a41af47c96b928e5b70148
Alex Hart <alex@signal.org>
2026-04-23 16:20:57 -0300

    Revert "Migrate VerifyScanFragment to compose."
    This reverts commit 9fa587b7e459a122d39585a5877d60f5e054adba.

--- app/src/main/java/org/thoughtcrime/securesms/verify/VerifyScanFragment.kt
+++ app/src/main/java/org/thoughtcrime/securesms/verify/VerifyScanFragment.kt
@@ -1,35 +1,56 @@
 package org.thoughtcrime.securesms.verify
 
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.getValue
-import androidx.lifecycle.viewmodel.compose.viewModel
-import org.signal.camera.CameraScreenViewModel
-import org.signal.core.ui.compose.ComposeFragment
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.core.view.OneShotPreDrawListener
+import androidx.fragment.app.Fragment
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
+import org.signal.core.util.concurrent.LifecycleDisposable
+import org.signal.qr.QrScannerView
 import org.signal.qr.kitkat.ScanListener
+import org.thoughtcrime.securesms.R
+import org.thoughtcrime.securesms.components.ShapeScrim
+import org.thoughtcrime.securesms.mediasend.camerax.CameraXRemoteConfig
+import org.thoughtcrime.securesms.util.ViewUtil
 import org.thoughtcrime.securesms.util.fragments.findListener
 
 /**
  * QR Scanner for identity verification
  */
-class VerifyScanFragment : ComposeFragment() {
-  @Composable
-  override fun FragmentContent() {
-    val viewModel = viewModel {
-      CameraScreenViewModel()
+class VerifyScanFragment : Fragment() {
+  private val lifecycleDisposable = LifecycleDisposable()
+
+  private lateinit var cameraView: QrScannerView
+  private lateinit var cameraScrim: ShapeScrim
+  private lateinit var cameraMarks: ImageView
+
+  override fun onCreateView(inflater: LayoutInflater, viewGroup: ViewGroup?, bundle: Bundle?): View? {
+    return ViewUtil.inflate(inflater, viewGroup!!, R.layout.verify_scan_fragment)
+  }
+
+  override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+    cameraView = view.findViewById(R.id.scanner)
+    cameraScrim = view.findViewById(R.id.camera_scrim)
+    cameraMarks = view.findViewById(R.id.camera_marks)
+    OneShotPreDrawListener.add(cameraScrim) {
+      val width = cameraScrim.scrimWidth
+      val height = cameraScrim.scrimHeight
+      ViewUtil.updateLayoutParams(cameraMarks, width, height)
     }
 
-    val state by viewModel.state
+    cameraView.start(viewLifecycleOwner, CameraXRemoteConfig.isBlocklisted())
 
-    LaunchedEffect(viewModel) {
-      viewModel.qrCodeDetected.collect {
-        findListener<ScanListener>()?.onQrDataFound(it)
+    lifecycleDisposable.bindTo(viewLifecycleOwner)
+
+    lifecycleDisposable += cameraView
+      .qrData
+      .distinctUntilChanged()
+      .observeOn(AndroidSchedulers.mainThread())
+      .subscribe { qrData: String ->
+        findListener<ScanListener>()?.onQrDataFound(qrData)
       }
-    }
-
-    VerifyScanScreen(
-      state = state,
-      emitter = viewModel::onEvent
-    )
   }
 }
