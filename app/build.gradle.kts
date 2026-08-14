b3a099b85f9483400a5d1bd95041d6651637f3f5
Greyson Parrelli <greyson@signal.org>
2026-08-14 16:48:05 -0400

    Bump version to 8.23.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -29,8 +29,8 @@ plugins {
 val staticIps = Properties().apply { file("static-ips.properties").reader().use { load(it) } }
 staticIps.stringPropertyNames().forEach { rootProject.extra[it] = staticIps.getProperty(it) }
 
-val canonicalVersionCode = 1734
-val canonicalVersionName = "8.23.0"
+val canonicalVersionCode = 1735
+val canonicalVersionName = "8.23.1"
 val currentHotfixVersion = 0
 val maxHotfixVersions = 100
 
